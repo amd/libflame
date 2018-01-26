cc13a544a84e0b4e373489730a4894296dda21b2
Field G. Van Zee <field@cs.utexas.edu>
2018-01-26 17:47:34 -0600

    Continuing towards disabling lapack2flamec's xblas.
    Details:
    - Turns out 5151689 did not completely disable xblas-related code within
      lapack2flamec layer as previously thought. This is because I only
      disabled the *definitions* of the xblas code and not the *invocations*
      of that code in other f2c'ed netlib LAPACK functions. This commit
      provides a second attempt. Thanks for Serguei Sokol for his continued
      support on this issue.
    - Added a script, disable_xblas.sh, which is now called by regen_files.sh
      in src/map/lapack2flamec/f2c/c when a fresh version of netlib LAPACK is
      imported into libflame.
    - Minor usability updates to build/find-dependents.sh script.

--- src/map/lapack2flamec/f2c/c/cla_syrfsx_extended.c
+++ src/map/lapack2flamec/f2c/c/cla_syrfsx_extended.c
@@ -1,6 +1,6 @@
+#ifdef FLA_ENABLE_XBLAS
 /* ../netlib/cla_syrfsx_extended.f -- translated by f2c (version 20100827). You must link the resulting object file with libf2c: on Microsoft Windows system, link with libf2c.lib;
  on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
-#ifdef FLA_ENABLE_XBLAS
 #include "FLA_f2c.h" /* Table of constant values */
 static integer c__1 = 1;
 static scomplex c_b14 = {-1.f, 0.f};
@@ -844,6 +844,5 @@ void cla_syrfsx_extended_(integer *prec_type__, char *uplo, integer *n, integer 
     AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
     return;
 }
-#endif
 /* cla_syrfsx_extended__ */
 #endif

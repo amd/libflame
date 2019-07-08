2db37b9eaa4aea1ba2318d819fd74d5a380fae43
varajago <Vasanthakumar.Rajagopal@amd.com>
2019-07-08 17:15:52 +0530

    Modifications for thread safety (LAPACK_getrf in FLA_getrf.c)
    Change-Id: Ia71fe5300e8f62bf05dde76eb6c41a0c1a559bd2

--- src/base/flamec/control/init/blas/1/flash/FLASH_Axpyt_cntl_init.c
+++ src/base/flamec/control/init/blas/1/flash/FLASH_Axpyt_cntl_init.c
@@ -10,11 +10,56 @@
 
 #include "FLAME.h"
 
-TLS_CLASS_SPEC fla_axpyt_t*       flash_axpyt_cntl_blas = NULL;
-TLS_CLASS_SPEC fla_axpyt_t*       flash_axpyt_cntl_tb;
-TLS_CLASS_SPEC fla_axpyt_t*       flash_axpyt_cntl_lr;
-TLS_CLASS_SPEC fla_axpyt_t*       flash_axpyt_cntl;
-TLS_CLASS_SPEC fla_blocksize_t*   flash_axpyt_bsize;
+#ifdef FLA_ENABLE_THREAD_SAFE_INTERFACES
+void FLASH_Axpyt_cntl_init_ts(FLA_Cntl_init_flash_s *FLA_cntl_flash_init_i)
+{
+	// Set blocksize for hierarchical storage.
+	FLA_cntl_flash_init_i->flash_axpyt_bsize     = FLA_Blocksize_create( 1, 1, 1, 1 );
+
+	// Create a control tree that assumes A and B are small.
+	FLA_cntl_flash_init_i->flash_axpyt_cntl_blas = FLA_Cntl_axpyt_obj_create( FLA_HIER,
+	                                                   FLA_SUBPROBLEM,
+	                                                   NULL,
+	                                                   NULL );
+
+	// Create a control tree that marches through A and B vertically.
+	FLA_cntl_flash_init_i->flash_axpyt_cntl_tb   = FLA_Cntl_axpyt_obj_create( FLA_HIER,
+	                                                   FLA_BLOCKED_VARIANT1,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_bsize,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_cntl_blas );
+
+	// Create a control tree that marches through A and B horizontally.
+	FLA_cntl_flash_init_i->flash_axpyt_cntl_lr   = FLA_Cntl_axpyt_obj_create( FLA_HIER,
+	                                                   FLA_BLOCKED_VARIANT3,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_bsize,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_cntl_blas );
+
+	// Create a control tree that marches through A and B horizontally, then
+	// vertically.
+	FLA_cntl_flash_init_i->flash_axpyt_cntl      = FLA_Cntl_axpyt_obj_create( FLA_HIER,
+	                                                   FLA_BLOCKED_VARIANT3,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_bsize,
+	                                                   FLA_cntl_flash_init_i->flash_axpyt_cntl_tb );
+}
+
+void FLASH_Axpyt_cntl_finalize_ts(FLA_Cntl_init_flash_s *FLA_cntl_flash_init_i)
+{
+	FLA_Cntl_obj_free( FLA_cntl_flash_init_i->flash_axpyt_cntl_blas );
+
+	FLA_Cntl_obj_free( FLA_cntl_flash_init_i->flash_axpyt_cntl_tb );
+	FLA_Cntl_obj_free( FLA_cntl_flash_init_i->flash_axpyt_cntl_lr );
+	FLA_Cntl_obj_free( FLA_cntl_flash_init_i->flash_axpyt_cntl );
+
+	FLA_Blocksize_free( FLA_cntl_flash_init_i->flash_axpyt_bsize );
+}
+
+#endif
+
+fla_axpyt_t*       flash_axpyt_cntl_blas = NULL;
+fla_axpyt_t*       flash_axpyt_cntl_tb;
+fla_axpyt_t*       flash_axpyt_cntl_lr;
+fla_axpyt_t*       flash_axpyt_cntl;
+fla_blocksize_t*   flash_axpyt_bsize;
 
 void FLASH_Axpyt_cntl_init()
 {
--- src/base/flamec/control/init/lapack/flash/FLASH_CAQR_UT_inc_cntl_init.c
+++ src/base/flamec/control/init/lapack/flash/FLASH_CAQR_UT_inc_cntl_init.c
@@ -10,8 +10,31 @@
 
 #include "FLAME.h"
 
-extern TLS_CLASS_SPEC fla_caqr2ut_t*  flash_caqr2ut_cntl;
-extern TLS_CLASS_SPEC fla_apcaq2ut_t* flash_apcaq2ut_cntl;
+#ifdef FLA_ENABLE_THREAD_SAFE_INTERFACES
+void FLASH_CAQR_UT_inc_cntl_init_ts(FLA_Cntl_init_flash_s *FLA_cntl_flash_init_i)
+{
+	// Set blocksizes for hierarchical storage.
+	FLA_cntl_flash_init_i->flash_caqrutinc_var1_bsize = FLA_Blocksize_create( 1, 1, 1, 1 );
+
+	// Create a control tree to invoke variant 1.
+	FLA_cntl_flash_init_i->flash_caqrutinc_cntl = FLA_Cntl_caqrutinc_obj_create( FLA_HIER,
+	                                                      FLA_BLOCKED_VARIANT1, 
+	                                                      FLA_cntl_flash_init_i->flash_caqrutinc_var1_bsize,
+	                                                      FLA_cntl_flash_init_i->flash_caqr2ut_cntl,
+	                                                      FLA_cntl_flash_init_i->flash_apcaq2ut_cntl );
+}
+
+void FLASH_CAQR_UT_inc_cntl_finalize_ts(FLA_Cntl_init_flash_s *FLA_cntl_flash_init_i)
+{
+	FLA_Cntl_obj_free( FLA_cntl_flash_init_i->flash_caqrutinc_cntl );
+
+	FLA_Blocksize_free( FLA_cntl_flash_init_i->flash_caqrutinc_var1_bsize );
+}
+
+#endif
+
+extern fla_caqr2ut_t*  flash_caqr2ut_cntl;
+extern fla_apcaq2ut_t* flash_apcaq2ut_cntl;
 
 TLS_CLASS_SPEC fla_caqrutinc_t*     flash_caqrutinc_cntl = NULL;
 TLS_CLASS_SPEC fla_blocksize_t*     flash_caqrutinc_var1_bsize = NULL;

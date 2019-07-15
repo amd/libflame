4d24f3586e4f279e1c39b100b499bd3e52a4cbec
varajago <Vasanthakumar.Rajagopal@amd.com>
2019-07-15 17:21:22 +0530

    modified test files to reflect TLS changes
    Change-Id: I8d54f1619f1aac297fa8401248f0756942cea9f7

--- test/legacyflame/src/test_symm.c
+++ test/legacyflame/src/test_symm.c
@@ -314,9 +314,9 @@ void libfla_test_symm_experiment( test_params_t params,
 
 
 
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_scal_t* fla_scal_cntl_blas;
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_gemm_t* fla_gemm_cntl_blas;
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_symm_t* fla_symm_cntl_blas;
+extern __thread fla_scal_t* fla_scal_cntl_blas;
+extern __thread fla_gemm_t* fla_gemm_cntl_blas;
+extern __thread fla_symm_t* fla_symm_cntl_blas;
 
 void libfla_test_symm_cntl_create( unsigned int var,
                                    fla_dim_t        b_alg_flat )

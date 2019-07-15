ffdc7580eedb78f4558627275610b1db94b1185f
varajago <Vasanthakumar.Rajagopal@amd.com>
2019-07-15 17:21:22 +0530

    modified test files to reflect TLS changes
    Change-Id: I8d54f1619f1aac297fa8401248f0756942cea9f7

--- test/legacyflame/src/test_trmm.c
+++ test/legacyflame/src/test_trmm.c
@@ -311,9 +311,9 @@ void libfla_test_trmm_experiment( test_params_t params,
 
 
 
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_scal_t* fla_scal_cntl_blas;
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_gemm_t* fla_gemm_cntl_blas;
-extern LIBFLAME_IMPORT TLS_CLASS_SPEC fla_trmm_t* fla_trmm_cntl_blas;
+extern __thread fla_scal_t* fla_scal_cntl_blas;
+extern __thread fla_gemm_t* fla_gemm_cntl_blas;
+extern __thread fla_trmm_t* fla_trmm_cntl_blas;
 
 void libfla_test_trmm_cntl_create( unsigned int var,
                                    fla_dim_t        b_alg_flat )

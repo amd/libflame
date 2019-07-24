38dd2960daacb8dbaa069831355c25705fa22e56
Vasanthakumar.Rajagopal <Vasanthakumar.Rajagopal@amd.com>
2019-07-24 12:55:08 +0530

    Macro enabled TLS
    Change-Id: I18ca76a9e9f43a1e3a1bd04c561689b6926cf36f

--- test/legacyflame/src/test_trinv.c
+++ test/legacyflame/src/test_trinv.c
@@ -249,9 +249,9 @@ void libfla_test_trinv_experiment( test_params_t params,
 
 
 
-extern __thread fla_trmm_t* fla_trmm_cntl_blas;
-extern __thread fla_trsm_t* fla_trsm_cntl_blas;
-extern __thread fla_gemm_t* fla_gemm_cntl_blas;
+extern TLS_CLASS_SPEC fla_trmm_t* fla_trmm_cntl_blas;
+extern TLS_CLASS_SPEC fla_trsm_t* fla_trsm_cntl_blas;
+extern TLS_CLASS_SPEC fla_gemm_t* fla_gemm_cntl_blas;
 
 void libfla_test_trinv_cntl_create( unsigned int var,
                                     fla_dim_t        b_alg_flat )

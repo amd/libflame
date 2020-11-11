dcc7a79925f11a4c68b0de43f2341a40ef431fd1
Pradeep Rao <pradeep.rao@amd.com>
2020-11-11 16:19:10 +0530

    New configure option to enable f2c convention DOTC function
    New configure option to enable the application to invoke
    Fortran-to-C based calling convention for complex datatype dotc
    function of BLAS. If this option is enabled libflame calls
    cdotc/zdotc function with output as first parameter of the function
    instead of as return type.
    
    Change-Id: I64ef37c2daea8db2c71258c16d40f202cbc21527

--- src/base/flamec/include/FLA_f2c.h
+++ src/base/flamec/include/FLA_f2c.h
@@ -19,6 +19,7 @@
 #include <complex.h>
 #undef complex
 #include "aocldtl.h"
+#include "FLA_config.h"
 
 #ifndef F2C_INCLUDE
 #define F2C_INCLUDE
@@ -30,9 +31,14 @@
 #define cdotc_f2c_ cdotc_
 #define zdotc_f2c_ zdotc_
 
-#define cdotu_f2c_ cdotu_
-#define zdotu_f2c_ zdotu_
+/* Call F2C convention for Complex DOTC functions */
+#ifdef FLA_ENABLE_F2C_DOTC
+#define cdotc_f2c_ cdotc_
+#define zdotc_f2c_ zdotc_
 #endif
+
+/* typedef long int integer; */
+typedef int integer; 
 
 /* DTL purpose */
 #ifdef _WIN32
--- src/map/lapack2flamec/f2c/install/static/dotc.c
+++ src/map/lapack2flamec/f2c/install/static/dotc.c
@@ -1,5 +1,9 @@
 #include "FLA_f2c.h"
 
+#ifndef FLA_ENABLE_F2C_DOTC
+
+extern
+complex cdotc_(integer *n, complex *cx, integer *incx, complex *cy, integer *incy);
 
 void aocl_lapack_cdotc_f2c(scomplex *r, aocl_int64_t *n, scomplex *cx, aocl_int64_t *incx, scomplex *cy, aocl_int64_t *incy)
 {
@@ -11,4 +15,4 @@ void aocl_lapack_zdotc_f2c(dcomplex *r, aocl_int64_t *n, dcomplex *cx, aocl_int6
 {
     aocl_blas_zdotc(r, n, cx, incx, cy, incy);
 }
-
+#endif

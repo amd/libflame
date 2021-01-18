4751770a2a6705ee4ea3c1d19dabc1ee1a7f316a
swhirema <SwatiG.Hiremath@amd.com>
2021-01-18 15:35:56 +0530

    libFlame : Added DTL code to getrf, getrfnp, getrfnpi, potrf, geqrf
    Change-Id: Ib6179dcaf0a21f0459a9322f25ae8c3d45d24501

--- src/map/lapack2flamec/check/zgeqrf_check.c
+++ src/map/lapack2flamec/check/zgeqrf_check.c
@@ -12,7 +12,11 @@ int zgeqrf_check(aocl_int64_t *m, aocl_int64_t *n, dcomplex *a, aocl_int64_t *ld
     aocl_int64_t k, nb;
     aocl_int64_t lwkopt;
     logical lquery;
-
+#if AOCL_DTL_LOG_ENABLE
+    char buffer[256];
+    sprintf(buffer, "zgeqrf inputs: m %d, n %d, lda %d\n", *m, *n, *lda);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
+#endif
     /* Parameter adjustments */
     a_dim1 = *lda;
     a_offset = 1 + a_dim1;

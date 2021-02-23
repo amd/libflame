5b94d239c0b4cd00a762e76932d61c3042834c60
“dnikku” <Deepika.Nikku@amd.com>
2021-02-23 05:10:44 +0000

    libflame: Added DTL code to srscl.c zdrscl.c dzsum1.c files
    Added code changes to enable logging and tracing in srscl(),
    zdrscl(), dzsum1() functions
    
    Change-Id: I01613e74ec99c8e6d8b6e1427dc7eb059ec775e5

--- src/map/lapack2flamec/f2c/c/srscl.c
+++ src/map/lapack2flamec/f2c/c/srscl.c
@@ -120,13 +120,20 @@ void aocl_lapack_srscl(aocl_int64_t *n, real *sa, real *sx, aocl_int64_t *incx)
     /* .. */
     /* .. Executable Statements .. */
     /* Quick return if possible */
+    /* Logging and tracing code */
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+    #if AOCL_DTL_LOG_ENABLE
+      char buffer[256];
+      sprintf(buffer, "srscl inputs: n %d, incx %d \n", *n, *incx);
+      AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
+    #endif
     /* Parameter adjustments */
     --sx;
     /* Function Body */
     if(*n <= 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Get machine parameters */
     smlnum = slamch_("S");
@@ -163,8 +170,8 @@ L10:
     {
         goto L10;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SRSCL */
 }
 /* srscl_ */

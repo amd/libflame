e387c2283c7490247f4e5e3fda8c188839d09207
varajago <Vasanthakumar.Rajagopal@amd.com>
2021-03-16 19:28:51 +0530

    LibFLAME: Super Matrix based multi threading for LAPACK interface of LU factorization.
    Multi threading enabled for LAPACK interface of LU factorization with partial pivoting
    through usage of FLAME Scalable Hierarchical (FLASH) interface. FLASH interface uses
    libFLAME's Super Matrix method to parallelize computations.
    This feature can be enabled through --enable-multithreading option during configure step.
    
    Signed-off-by: Vasanthakumar R <varajago@amd.com>
    AMD-Internal: CPUPL-1473
    Change-Id: I5e174cb71a034147337b77cc7359e808144c511f

--- src/base/flamec/include/FLA_macro_defs.h
+++ src/base/flamec/include/FLA_macro_defs.h
@@ -9,7 +9,8 @@
 */
 
 /*
-    Modifications Copyright (c) 2021-2026 Advanced Micro Devices, Inc.  All rights reserved.
+    Copyright (c) 2021 Advanced Micro Devices, Inc.  All rights reserved.
+    Mar 16, 2021
 */
 
 

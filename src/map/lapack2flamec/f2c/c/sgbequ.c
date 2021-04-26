cc999992e1752351a4f027b5f925939b906962d8
dnikku <Deepika.Nikku@amd.com>
2021-04-26 12:55:38 +0530

    libflame: Added DTL code to sbdsqr.c scombssq.c scsum1.c sgbcon.c sgbequ.c sgbrfs.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I4166b0c2b90e897594e214f4c1deedcf59ad0c3d

--- src/map/lapack2flamec/f2c/c/sgbequ.c
+++ src/map/lapack2flamec/f2c/c/sgbequ.c
@@ -152,31 +152,12 @@ void sgbequ_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, real 
              aocl_int_t *ldab, real *r__, real *c__, real *rowcnd, real *colcnd, real *amax,
              aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_sgbequ(m, n, kl, ku, ab, ldab, r__, c__, rowcnd, colcnd, amax, info);
-#else
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t kl_64 = *kl;
-    aocl_int64_t ku_64 = *ku;
-    aocl_int64_t ldab_64 = *ldab;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_sgbequ(&m_64, &n_64, &kl_64, &ku_64, ab, &ldab_64, r__, c__, rowcnd, colcnd, amax,
-                       &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"sgbequ inputs: m %d, n %d, kl %d, ku %d, ldab %d",*m, *n, *kl, *ku, *ldab);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl_int64_t *ku,
-                        real *ab, aocl_int64_t *ldab, real *r__, real *c__, real *rowcnd,
-                        real *colcnd, real *amax, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("sgbequ inputs: m %" FLA_IS ", n %" FLA_IS ", kl %" FLA_IS ", ku %" FLA_IS
-                      ", ldab %" FLA_IS "",
-                      *m, *n, *kl, *ku, *ldab);
     /* System generated locals */
     aocl_int64_t ab_dim1, ab_offset, i__1, i__2, i__3, i__4;
     real r__1, r__2, r__3;
@@ -237,9 +218,9 @@ void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("SGBEQU", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SGBEQU", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*m == 0 || *n == 0)
@@ -247,8 +228,8 @@ void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl
         *rowcnd = 1.f;
         *colcnd = 1.f;
         *amax = 0.f;
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Get machine constants. */
     smlnum = slamch_("S");
@@ -306,8 +287,8 @@ void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl
             if(r__[i__] == 0.f)
             {
                 *info = i__;
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L50: */
         }
@@ -381,8 +362,8 @@ void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl
             if(c__[j] == 0.f)
             {
                 *info = *m + j;
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L110: */
         }
@@ -403,8 +384,8 @@ void aocl_lapack_sgbequ(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *kl, aocl
         /* Compute COLCND = fla_min(C(J)) / fla_max(C(J)) */
         *colcnd = fla_max(rcmin, smlnum) / fla_min(rcmax, bignum);
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SGBEQU */
 }
 /* sgbequ_ */

ea5084fcfbb583d71eb69df6b88753e5980457f6
dnikku <Deepika.Nikku@amd.com>
2021-05-05 12:41:28 +0530

    libflame: Added DTL code to slasr.c slasrt.c slassq.c slasv2.c slaswlq.c slaswp.c slasyf.c slasyf_aa.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: If9b9b70c8ff0aa17e1f4d8ada4b000d34318fded

--- src/map/lapack2flamec/f2c/c/slasr.c
+++ src/map/lapack2flamec/f2c/c/slasr.c
@@ -198,24 +198,12 @@
 void slasr_(char *side, char *pivot, char *direct, aocl_int_t *m, aocl_int_t *n, real *c__, real *s,
             real *a, aocl_int_t *lda)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_slasr(side, pivot, direct, m, n, c__, s, a, lda);
-#else
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-
-    aocl_lapack_slasr(side, pivot, direct, &m_64, &n_64, c__, s, a, &lda_64);
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"slasr inputs: side %c, pivot %c, direct %c, m %d, n %d, lda %d",*side, *pivot, *direct, *m, *n, *lda);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_slasr(char *side, char *pivot, char *direct, aocl_int64_t *m, aocl_int64_t *n,
-                       real *c__, real *s, real *a, aocl_int64_t *lda)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("slasr inputs: side %c, pivot %c, direct %c, m %" FLA_IS ", n %" FLA_IS
-                      ", lda %" FLA_IS "",
-                      *side, *pivot, *direct, *m, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1, i__2;
     /* Local variables */
@@ -278,15 +266,15 @@ void aocl_lapack_slasr(char *side, char *pivot, char *direct, aocl_int64_t *m, a
     }
     if(info != 0)
     {
-        aocl_blas_xerbla("SLASR ", &info, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SLASR ", &info);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*m == 0 || *n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(lsame_(side, "L", 1, 1))
     {
@@ -560,8 +548,8 @@ void aocl_lapack_slasr(char *side, char *pivot, char *direct, aocl_int64_t *m, a
             }
         }
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SLASR */
 }
 /* slasr_ */

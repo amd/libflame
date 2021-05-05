cd91f8a9257c535c17253e3cb0b9681285200451
dnikku <Deepika.Nikku@amd.com>
2021-05-05 12:15:13 +0530

    libflame: Added DTL code to slarz.c slarzb.c slarzt.c slas2.c slascl.c slasd4.c slasd6.c slasd7.c slasd8.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I3611065a71d976a21f817f8f7f52a95d98e95283

--- src/map/lapack2flamec/f2c/c/slarzb.c
+++ src/map/lapack2flamec/f2c/c/slarzb.c
@@ -188,34 +188,12 @@ void slarzb_(char *side, char *trans, char *direct, char *storev, aocl_int_t *m,
              aocl_int_t *k, aocl_int_t *l, real *v, aocl_int_t *ldv, real *t, aocl_int_t *ldt,
              real *c__, aocl_int_t *ldc, real *work, aocl_int_t *ldwork)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_slarzb(side, trans, direct, storev, m, n, k, l, v, ldv, t, ldt, c__, ldc, work,
-                       ldwork);
-#else
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t k_64 = *k;
-    aocl_int64_t l_64 = *l;
-    aocl_int64_t ldv_64 = *ldv;
-    aocl_int64_t ldt_64 = *ldt;
-    aocl_int64_t ldc_64 = *ldc;
-    aocl_int64_t ldwork_64 = *ldwork;
-
-    aocl_lapack_slarzb(side, trans, direct, storev, &m_64, &n_64, &k_64, &l_64, v, &ldv_64, t,
-                       &ldt_64, c__, &ldc_64, work, &ldwork_64);
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"slarzb inputs: side %c, trans %c, direct %c, storev %c, m %d, n %d, k %d, l %d, ldv %d, ldt %d, ldc %d",*side, *trans, *direct, *storev, *m, *n, *k, *l, *ldv, *ldt, *ldc);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_slarzb(char *side, char *trans, char *direct, char *storev, aocl_int64_t *m,
-                        aocl_int64_t *n, aocl_int64_t *k, aocl_int64_t *l, real *v,
-                        aocl_int64_t *ldv, real *t, aocl_int64_t *ldt, real *c__, aocl_int64_t *ldc,
-                        real *work, aocl_int64_t *ldwork)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("slarzb inputs: side %c, trans %c, direct %c, storev %c, m %" FLA_IS
-                      ", n %" FLA_IS ", k %" FLA_IS ", l %" FLA_IS ", ldv "
-                      "%" FLA_IS ", ldt %" FLA_IS ", ldc %" FLA_IS "",
-                      *side, *trans, *direct, *storev, *m, *n, *k, *l, *ldv, *ldt, *ldc);
     /* System generated locals */
     aocl_int64_t c_dim1, c_offset, t_dim1, t_offset, v_dim1, v_offset, work_dim1, work_offset, i__1,
         i__2;
@@ -258,8 +236,8 @@ void aocl_lapack_slarzb(char *side, char *trans, char *direct, char *storev, aoc
     /* Function Body */
     if(*m <= 0 || *n <= 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Check for currently supported options */
     info = 0;
@@ -274,9 +252,9 @@ void aocl_lapack_slarzb(char *side, char *trans, char *direct, char *storev, aoc
     if(info != 0)
     {
         i__1 = -info;
-        aocl_blas_xerbla("SLARZB", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SLARZB", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(lsame_(trans, "N", 1, 1))
     {
@@ -368,8 +346,8 @@ void aocl_lapack_slarzb(char *side, char *trans, char *direct, char *storev, aoc
                             ldc);
         }
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SLARZB */
 }
 /* slarzb_ */

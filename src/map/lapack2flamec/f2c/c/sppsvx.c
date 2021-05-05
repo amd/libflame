0db0886d2b51eb831db9e6af215f352cb37c933d
dnikku <Deepika.Nikku@amd.com>
2021-05-05 16:42:14 +0530

    libflame: Added DTL code to sppcon.c sppequ.c spprfs.c sppsv.c sppsvx.c spptrf.c spptri.c spptrs.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: Ib23be0128ef473578a9d835eeacb613c1bfce507

--- src/map/lapack2flamec/f2c/c/sppsvx.c
+++ src/map/lapack2flamec/f2c/c/sppsvx.c
@@ -319,32 +319,12 @@ void sppsvx_(char *fact, char *uplo, aocl_int_t *n, aocl_int_t *nrhs, real *ap, 
              char *equed, real *s, real *b, aocl_int_t *ldb, real *x, aocl_int_t *ldx, real *rcond,
              real *ferr, real *berr, real *work, aocl_int_t *iwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_sppsvx(fact, uplo, n, nrhs, ap, afp, equed, s, b, ldb, x, ldx, rcond, ferr, berr,
-                       work, iwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t ldx_64 = *ldx;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_sppsvx(fact, uplo, &n_64, &nrhs_64, ap, afp, equed, s, b, &ldb_64, x, &ldx_64,
-                       rcond, ferr, berr, work, iwork, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"sppsvx inputs: fact %c, uplo %c, n %d, nrhs %d, ldb %d, ldx %d",*fact, *uplo, *n, *nrhs, *ldb, *ldx);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_sppsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, real *ap,
-                        real *afp, char *equed, real *s, real *b, aocl_int64_t *ldb, real *x,
-                        aocl_int64_t *ldx, real *rcond, real *ferr, real *berr, real *work,
-                        aocl_int_t *iwork, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("sppsvx inputs: fact %c, uplo %c, n %" FLA_IS ", nrhs %" FLA_IS
-                      ", ldb %" FLA_IS ", ldx %" FLA_IS "",
-                      *fact, *uplo, *n, *nrhs, *ldb, *ldx);
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, x_dim1, x_offset, i__1, i__2;
     real r__1, r__2;
@@ -478,9 +458,9 @@ void aocl_lapack_sppsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("SPPSVX", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SPPSVX", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(equil)
     {
@@ -518,8 +498,8 @@ void aocl_lapack_sppsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
         if(*info > 0)
         {
             *rcond = 0.f;
-            AOCL_DTL_TRACE_LOG_EXIT
-            return;
+            AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+            return 0;
         }
     }
     /* Compute the norm of the matrix A. */
@@ -560,8 +540,8 @@ void aocl_lapack_sppsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
     {
         *info = *n + 1;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SPPSVX */
 }
 /* sppsvx_ */

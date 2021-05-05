a1db7db8039434eff5a2279387e2d31fa45a67e0
dnikku <Deepika.Nikku@amd.com>
2021-05-05 16:29:32 +0530

    libflame: Added DTL code to sormrq.c sormrz.c spbcon.c spdequ.c spbrfs.c spbstf.c spbsv.c spbsvx.c spbtf2.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I4a68aea03ec722ed08b7f3e8345a1fccaf180d4e

--- src/map/lapack2flamec/f2c/c/spbtf2.c
+++ src/map/lapack2flamec/f2c/c/spbtf2.c
@@ -146,26 +146,12 @@ static aocl_int64_t c__1 = 1;
 void spbtf2_(char *uplo, aocl_int_t *n, aocl_int_t *kd, real *ab, aocl_int_t *ldab,
              aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_spbtf2(uplo, n, kd, ab, ldab, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t kd_64 = *kd;
-    aocl_int64_t ldab_64 = *ldab;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_spbtf2(uplo, &n_64, &kd_64, ab, &ldab_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"spbtf2 inputs: uplo %c, n %d, kd %d, ldab %d",*uplo, *n, *kd, *ldab);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_spbtf2(char *uplo, aocl_int64_t *n, aocl_int64_t *kd, real *ab, aocl_int64_t *ldab,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("spbtf2 inputs: uplo %c, n %" FLA_IS ", kd %" FLA_IS ", ldab %" FLA_IS "",
-                      *uplo, *n, *kd, *ldab);
     /* System generated locals */
     aocl_int64_t ab_dim1, ab_offset, i__1, i__2, i__3;
     real r__1;
@@ -224,15 +210,15 @@ void aocl_lapack_spbtf2(char *uplo, aocl_int64_t *n, aocl_int64_t *kd, real *ab,
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("SPBTF2", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SPBTF2", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Computing MAX */
     i__1 = 1;
@@ -298,12 +284,12 @@ void aocl_lapack_spbtf2(char *uplo, aocl_int64_t *n, aocl_int64_t *kd, real *ab,
             /* L20: */
         }
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
 L30:
     *info = j;
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SPBTF2 */
 }
 /* spbtf2_ */

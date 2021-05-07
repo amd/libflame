89f8b7bf3752ae06bdcfc86a252921c0faf051f4
dnikku <Deepika.Nikku@amd.com>
2021-05-07 13:12:40 +0530

    libflame: Added DTL code to ssysv.c ssysv_aa.c ssysv_aa_2stage.c ssysv_rk.c ssysv_rook.c ssysvx.c ssyswapr.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: Iae2119cbcf9264ad610bda4e5d60d116446131c4

--- src/map/lapack2flamec/f2c/c/ssysv.c
+++ src/map/lapack2flamec/f2c/c/ssysv.c
@@ -173,31 +173,12 @@ the routine */
 void ssysv_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, real *a, aocl_int_t *lda, aocl_int_t *ipiv,
             real *b, aocl_int_t *ldb, real *work, aocl_int_t *lwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_ssysv(uplo, n, nrhs, a, lda, ipiv, b, ldb, work, lwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t lwork_64 = *lwork;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_ssysv(uplo, &n_64, &nrhs_64, a, &lda_64, ipiv, b, &ldb_64, work, &lwork_64,
-                      &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"ssysv inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *lda, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_ssysv(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, real *a, aocl_int64_t *lda,
-                       aocl_int_t *ipiv, real *b, aocl_int64_t *ldb, real *work,
-                       aocl_int64_t *lwork, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("ssysv inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS
-                      ", ipiv %" FLA_IS ", ldb %" FLA_IS "",
-                      *uplo, *n, *nrhs, *lda, *ipiv, *ldb);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     /* Local variables */
@@ -274,14 +255,14 @@ void aocl_lapack_ssysv(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, real *a,
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("SSYSV ", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SSYSV ", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     else if(lquery)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Compute the factorization A = U*D*U**T or A = L*D*L**T. */
     aocl_lapack_ssytrf(uplo, n, &a[a_offset], lda, &ipiv[1], &work[1], lwork, info);
@@ -300,9 +281,9 @@ void aocl_lapack_ssysv(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, real *a,
                                 &work[1], info);
         }
     }
-    work[1] = aocl_lapack_sroundup_lwork(&lwkopt);
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    work[1] = (real) lwkopt;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SSYSV */
 }
 /* ssysv_ */
--- src/map/lapack2flamec/f2c/c/ssysv_rook.c
+++ src/map/lapack2flamec/f2c/c/ssysv_rook.c
@@ -206,31 +206,12 @@ void ssysv_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, real *a, aocl_int_
                  aocl_int_t *ipiv, real *b, aocl_int_t *ldb, real *work, aocl_int_t *lwork,
                  aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_ssysv_rook(uplo, n, nrhs, a, lda, ipiv, b, ldb, work, lwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t lwork_64 = *lwork;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_ssysv_rook(uplo, &n_64, &nrhs_64, a, &lda_64, ipiv, b, &ldb_64, work, &lwork_64,
-                           &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"ssysv_rook inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *lda, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_ssysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, real *a,
-                            aocl_int64_t *lda, aocl_int_t *ipiv, real *b, aocl_int64_t *ldb,
-                            real *work, aocl_int64_t *lwork, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("ssysv inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS
-                      ", ldb %" FLA_IS "",
-                      *uplo, *n, *nrhs, *lda, *ldb);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     /* Local variables */
@@ -307,14 +288,14 @@ void aocl_lapack_ssysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, rea
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("SSYSV_ROOK", &i__1, (ftnlen)10);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("SSYSV_ROOK ", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     else if(lquery)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Compute the factorization A = U*D*U**T or A = L*D*L**T. */
     aocl_lapack_ssytrf_rook(uplo, n, &a[a_offset], lda, &ipiv[1], &work[1], lwork, info);
@@ -325,9 +306,9 @@ void aocl_lapack_ssysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, rea
         aocl_lapack_ssytrs_rook(uplo, n, nrhs, &a[a_offset], lda, &ipiv[1], &b[b_offset], ldb,
                                 info);
     }
-    work[1] = aocl_lapack_sroundup_lwork(&lwkopt);
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    work[1] = (real) lwkopt;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of SSYSV_ROOK */
 }
 /* ssysv_rook__ */

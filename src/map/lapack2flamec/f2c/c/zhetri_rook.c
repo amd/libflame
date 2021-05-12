1ceaa66fd948644c41906fb66f77b8ac089cd2e1
dnikku <Deepika.Nikku@amd.com>
2021-05-12 19:38:36 +0530

    libflame: Added DTL code to zhetri.c zhetri2.c zhetri2x.c zhetri_3.c zhetri_3x.c zhetri_rook.c zhetrs2.c zhetrs_3.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I9e0c9ccb5ec9b4c305fb4a48ab6e073e2ea56652

--- src/map/lapack2flamec/f2c/c/zhetri_rook.c
+++ src/map/lapack2flamec/f2c/c/zhetri_rook.c
@@ -133,25 +133,12 @@ the matrix is singular and its */
 void zhetri_rook_(char *uplo, aocl_int_t *n, dcomplex *a, aocl_int_t *lda, aocl_int_t *ipiv,
                   dcomplex *work, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zhetri_rook(uplo, n, a, lda, ipiv, work, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zhetri_rook(uplo, &n_64, a, &lda_64, ipiv, work, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"zhetri inputs: uplo %c, n %d, lda %d",*uplo, *n, *lda);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zhetri_rook(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int64_t *lda,
-                             aocl_int_t *ipiv, dcomplex *work, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zhetri_rook inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS "%" FLA_IS "",
-                      *uplo, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1, i__2, i__3;
     doublereal d__1;
@@ -214,15 +201,15 @@ void aocl_lapack_zhetri_rook(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int6
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZHETRI_ROOK", &i__1, (ftnlen)11);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
-    }
+        xerbla_("ZHETRI_ROOK", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
+        }
     /* Quick return if possible */
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Check that the diagonal matrix D is nonsingular. */
     if(upper)
@@ -233,8 +220,8 @@ void aocl_lapack_zhetri_rook(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int6
             i__1 = *info + *info * a_dim1;
             if(ipiv[*info] > 0 && (a[i__1].real == 0. && a[i__1].imag == 0.))
             {
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L10: */
         }
@@ -248,8 +235,8 @@ void aocl_lapack_zhetri_rook(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int6
             i__2 = *info + *info * a_dim1;
             if(ipiv[*info] > 0 && (a[i__2].real == 0. && a[i__2].imag == 0.))
             {
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L20: */
         }
@@ -778,8 +765,8 @@ void aocl_lapack_zhetri_rook(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int6
         goto L80;
     L120:;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZHETRI_ROOK */
 }
 /* zhetri_rook__ */

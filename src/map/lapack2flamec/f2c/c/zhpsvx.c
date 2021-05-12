75a3eae0589eb98663a48009c6a95ea95ca0d9c3
dnikku <Deepika.Nikku@amd.com>
2021-05-12 19:41:46 +0530

    libflame: Added DTL code to zhetrs.c zhetrs_aa.c zhetrs_aa_2stage.c zhetrs_rook.c zhpcon.c zhprfs.c zhpsv.c zhpsvx.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: Ic42acbed795a23622a36d548db0ef6d3403a8a02

--- src/map/lapack2flamec/f2c/c/zhpsvx.c
+++ src/map/lapack2flamec/f2c/c/zhpsvx.c
@@ -281,33 +281,12 @@ void zhpsvx_(char *fact, char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *
              dcomplex *x, aocl_int_t *ldx, doublereal *rcond, doublereal *ferr,
              doublereal *berr, dcomplex *work, doublereal *rwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zhpsvx(fact, uplo, n, nrhs, ap, afp, ipiv, b, ldb, x, ldx, rcond, ferr, berr, work,
-                       rwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t ldx_64 = *ldx;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zhpsvx(fact, uplo, &n_64, &nrhs_64, ap, afp, ipiv, b, &ldb_64, x, &ldx_64, rcond,
-                       ferr, berr, work, rwork, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE
+    char buffer[256];
+    snprintf(buffer, 256,"zhpsvx inputs: fact %c, uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS ", ldx %" FLA_IS "",*fact, *uplo, *n, *nrhs, *ldb, *ldx);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zhpsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs,
-                        dcomplex *ap, dcomplex *afp, aocl_int_t *ipiv, dcomplex *b,
-                        aocl_int64_t *ldb, dcomplex *x, aocl_int64_t *ldx, doublereal *rcond,
-                        doublereal *ferr, doublereal *berr, dcomplex *work, doublereal *rwork,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zhpsvx inputs: fact %c, uplo %c, n %" FLA_IS ", nrhs %" FLA_IS
-                      ", ldb %" FLA_IS ", ldx %" FLA_IS "",
-                      *fact, *uplo, *n, *nrhs, *ldb, *ldx);
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, x_dim1, x_offset, i__1;
     /* Local variables */
@@ -380,9 +359,9 @@ void aocl_lapack_zhpsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZHPSVX", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZHPSVX", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(nofact)
     {
@@ -394,8 +373,8 @@ void aocl_lapack_zhpsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
         if(*info > 0)
         {
             *rcond = 0.;
-            AOCL_DTL_TRACE_LOG_EXIT
-            return;
+            AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+            return 0;
         }
     }
     /* Compute the norm of the matrix A. */
@@ -414,8 +393,8 @@ void aocl_lapack_zhpsvx(char *fact, char *uplo, aocl_int64_t *n, aocl_int64_t *n
     {
         *info = *n + 1;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZHPSVX */
 }
 /* zhpsvx_ */

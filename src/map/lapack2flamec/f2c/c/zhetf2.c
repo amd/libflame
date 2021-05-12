0b0a7220183fe1fb332bc67a210cb51defc2ed75
dnikku <Deepika.Nikku@amd.com>
2021-05-12 19:35:41 +0530

    libflame: Added DTL code to zhetf2.c zhetf2_rk.c zhetf2_rook.c zhetrf.c zhetrf_aa.c zhetrf_aa_2stage.c zhetrf_rk.c zhetrf_rook.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I2c52c562567d6c30c7020d7f6e141f738a27c8c6

--- src/map/lapack2flamec/f2c/c/zhetf2.c
+++ src/map/lapack2flamec/f2c/c/zhetf2.c
@@ -191,24 +191,12 @@ static aocl_int64_t c__1 = 1;
 void zhetf2_(char *uplo, aocl_int_t *n, dcomplex *a, aocl_int_t *lda, aocl_int_t *ipiv,
              aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zhetf2(uplo, n, a, lda, ipiv, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zhetf2(uplo, &n_64, a, &lda_64, ipiv, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE
+    char buffer[256];
+    snprintf(buffer, 256,"zhetf2 inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS "",*uplo, *n, *lda);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zhetf2(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int64_t *lda,
-                        aocl_int_t *ipiv, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zhetf2 inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS "", *uplo, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1, i__2, i__3, i__4, i__5, i__6;
     doublereal d__1, d__2, d__3, d__4;
@@ -287,9 +275,9 @@ void aocl_lapack_zhetf2(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int64_t *
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZHETF2", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZHETF2", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Initialize ALPHA for use in choosing pivot block size. */
     alpha = (sqrt(17.) + 1.) / 8.;
@@ -883,8 +871,8 @@ void aocl_lapack_zhetf2(char *uplo, aocl_int64_t *n, dcomplex *a, aocl_int64_t *
         goto L50;
     }
 L90:
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZHETF2 */
 }
 /* zhetf2_ */

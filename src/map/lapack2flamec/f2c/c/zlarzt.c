e2aee4728df2b47a8d34143aa68a1dfc82421802
dnikku <Deepika.Nikku@amd.com>
2021-05-14 16:13:17 +0530

    libflame: Added DTL code to zlarf.c zlarfb.c zlarfg.c zlarfgp.c zlarft.c zlarnv.c zlartg.c zlarz.c zlarzb.c zlarzt.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I4c289d6aa269d0f11346b73afa2430ab7e19e77b

--- src/map/lapack2flamec/f2c/c/zlarzt.c
+++ src/map/lapack2flamec/f2c/c/zlarzt.c
@@ -190,27 +190,12 @@ the corresponding */
 void zlarzt_(char *direct, char *storev, aocl_int_t *n, aocl_int_t *k, dcomplex *v,
              aocl_int_t *ldv, dcomplex *tau, dcomplex *t, aocl_int_t *ldt)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zlarzt(direct, storev, n, k, v, ldv, tau, t, ldt);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t k_64 = *k;
-    aocl_int64_t ldv_64 = *ldv;
-    aocl_int64_t ldt_64 = *ldt;
-
-    aocl_lapack_zlarzt(direct, storev, &n_64, &k_64, v, &ldv_64, tau, t, &ldt_64);
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"zlarzt inputs: direct %c, storev %c, n %d, k %d, ldv %d, ldt %d",*direct, *storev, *n, *k, *ldv, *ldt);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zlarzt(char *direct, char *storev, aocl_int64_t *n, aocl_int64_t *k,
-                        dcomplex *v, aocl_int64_t *ldv, dcomplex *tau, dcomplex *t,
-                        aocl_int64_t *ldt)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zlarzt inputs: direct %c, storev %c, n %" FLA_IS ", k %" FLA_IS
-                      ", ldv %" FLA_IS ", ldt %" FLA_IS "",
-                      *direct, *storev, *n, *k, *ldv, *ldt);
-
     /* System generated locals */
     aocl_int64_t t_dim1, t_offset, v_dim1, v_offset, i__1, i__2;
     dcomplex z__1;
@@ -257,9 +242,9 @@ void aocl_lapack_zlarzt(char *direct, char *storev, aocl_int64_t *n, aocl_int64_
     if(info != 0)
     {
         i__1 = -info;
-        aocl_blas_xerbla("ZLARZT", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZLARZT", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     for(i__ = *k; i__ >= 1; --i__)
     {
@@ -303,8 +288,8 @@ void aocl_lapack_zlarzt(char *direct, char *storev, aocl_int64_t *n, aocl_int64_
         }
         /* L20: */
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZLARZT */
 }
 /* zlarzt_ */

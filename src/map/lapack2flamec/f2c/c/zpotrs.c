d069cf7ebde3d74664190ea384981b0dab0d47d0
dnikku <Deepika.Nikku@amd.com>
2021-05-15 16:20:43 +0530

    libflame: Added DTL code to zpotrs.c zppcon.c zppequ.c zpprfs.c zppsv.c zppsvx.c zpptrf.c zpptri.c zpptrs.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I8fe775268c41fc98e0b54ee301da037ba851b653

--- src/map/lapack2flamec/f2c/c/zpotrs.c
+++ src/map/lapack2flamec/f2c/c/zpotrs.c
@@ -112,29 +112,12 @@ static dcomplex c_b1 = {1., 0.};
 void zpotrs_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *a, aocl_int_t *lda,
              dcomplex *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zpotrs(uplo, n, nrhs, a, lda, b, ldb, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zpotrs(uplo, &n_64, &nrhs_64, a, &lda_64, b, &ldb_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"zpotrs inputs: uplo %c, n %d, nrhs %d, lda %d, ldb %d",*uplo, *n, *nrhs, *lda, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zpotrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dcomplex *a,
-                        aocl_int64_t *lda, dcomplex *b, aocl_int64_t *ldb, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zpotrs inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS
-                      ", ldb %" FLA_IS "",
-                      *uplo, *n, *nrhs, *lda, *ldb);
-
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     /* Local variables */
@@ -194,15 +177,15 @@ void aocl_lapack_zpotrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dcomple
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZPOTRS", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZPOTRS", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0 || *nrhs == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(upper)
     {
@@ -224,8 +207,8 @@ void aocl_lapack_zpotrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dcomple
         aocl_blas_ztrsm("Left", "Lower", "Conjugate transpose", "Non-unit", n, nrhs, &c_b1,
                         &a[a_offset], lda, &b[b_offset], ldb);
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZPOTRS */
 }
 /* zpotrs_ */

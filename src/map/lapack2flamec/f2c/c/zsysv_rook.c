920c5b18769787cc65f9d9bca9e335e9b82a9f05
dnikku <Deepika.Nikku@amd.com>
2021-05-15 17:25:39 +0530

    libflame: Added DTL code to zsysv.c zsysv_aa.c zsysv_aa_2stage.c zsysv_rk.c zsysv_rook.c zsysvx.c zsyswapr.c zsytf2.c files
    Enabled DTL logging and tracing in the specified files
    
    Change-Id: I5856fad3998c5f7296952c4f03827b0c154fdc7d

--- src/map/lapack2flamec/f2c/c/zsysv_rook.c
+++ src/map/lapack2flamec/f2c/c/zsysv_rook.c
@@ -207,32 +207,12 @@ void zsysv_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *a, aocl_
                  aocl_int_t *ipiv, dcomplex *b, aocl_int_t *ldb, dcomplex *work,
                  aocl_int_t *lwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zsysv_rook(uplo, n, nrhs, a, lda, ipiv, b, ldb, work, lwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t lwork_64 = *lwork;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zsysv_rook(uplo, &n_64, &nrhs_64, a, &lda_64, ipiv, b, &ldb_64, work, &lwork_64,
-                           &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"zsysv inputs: uplo %c, n %d, nrhs %d, lda %d, ldb %d",*uplo, *n, *nrhs, *lda, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_zsysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dcomplex *a,
-                            aocl_int64_t *lda, aocl_int_t *ipiv, dcomplex *b,
-                            aocl_int64_t *ldb, dcomplex *work, aocl_int64_t *lwork,
-                            aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zsysv_rook inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS
-                      ", ldb %" FLA_IS ", lwork %" FLA_IS "",
-                      *uplo, *n, *nrhs, *lda, *ldb, *lwork);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     /* Local variables */
@@ -311,14 +291,14 @@ void aocl_lapack_zsysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dco
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZSYSV_ROOK ", &i__1, (ftnlen)11);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZSYSV_ROOK ", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     else if(lquery)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Compute the factorization A = U*D*U**T or A = L*D*L**T. */
     aocl_lapack_zsytrf_rook(uplo, n, &a[a_offset], lda, &ipiv[1], &work[1], lwork, info);
@@ -329,10 +309,10 @@ void aocl_lapack_zsysv_rook(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, dco
         aocl_lapack_zsytrs_rook(uplo, n, nrhs, &a[a_offset], lda, &ipiv[1], &b[b_offset], ldb,
                                 info);
     }
-    work[1].real = (doublereal)lwkopt;
-    work[1].imag = 0.; // , expr subst
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    work[1].r = (doublereal) lwkopt;
+    work[1].i = 0.; // , expr subst
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of ZSYSV_ROOK */
 }
 /* zsysv_rook__ */

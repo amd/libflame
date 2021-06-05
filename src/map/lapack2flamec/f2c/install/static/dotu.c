f4dfa8601b2b907304d45bc74a34ae96a3444335
Pradeep Rao <pradeep.rao@amd.com>
2021-06-05 20:07:07 +0530

    Enable f2c calling convention for DOTU funtion
    Now, if --enable-f2c-dotc configure option is enabled, libflame
    invokes cdotu/zdotu function with output as first parameter of
    the function instead of as return type. This is similar to
    similar existing support for cdotc/zdotu.
    
    Change-Id: I111f677eb2a3f409ef128809f8b1f5dfe7797ecc

--- src/map/lapack2flamec/f2c/install/static/dotu.c
+++ src/map/lapack2flamec/f2c/install/static/dotu.c
@@ -1,6 +1,10 @@
 #include "FLA_f2c.h"
 
-void aocl_lapack_cdotu_f2c(scomplex *r, aocl_int64_t *n, scomplex *cx, aocl_int64_t *incx, scomplex *cy, aocl_int64_t *incy)
+#ifndef FLA_ENABLE_F2C_DOTC
+
+extern
+complex cdotu_(integer *n, complex *cx, integer *incx, complex *cy, integer *incy);
+VOID cdotu_f2c_(complex *r, integer *n, complex *cx, integer *incx, complex *cy, integer *incy)
 {
     aocl_blas_cdotu(r, n, cx, incx, cy, incy);
 }
@@ -10,3 +14,5 @@ void aocl_lapack_zdotu_f2c(dcomplex *r, aocl_int64_t *n, dcomplex *cx, aocl_int6
 {
     aocl_blas_zdotu(r, n, cx, incx, cy, incy);
 }
+
+#endif

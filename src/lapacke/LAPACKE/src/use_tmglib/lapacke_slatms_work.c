a94c5d059102655ef98e12db8998d89f1dc4a05d
Pradeep Rao <pradeep.rao@amd.com>
2021-06-18 17:47:07 +0530

    Exclude extended LAPACKE function build
    Exclude extended LAPACKE functions that need XBLAS and TMGLIB
    while building libFLAME
    
    Change-Id: Id2f41f7f8717b83803ff8243086f23293b90d690

--- src/lapacke/LAPACKE/src/use_tmglib/lapacke_slatms_work.c
+++ src/lapacke/LAPACKE/src/use_tmglib/lapacke_slatms_work.c
@@ -28,6 +28,7 @@
 *****************************************************************************
 * Contents: Native middle-level C interface to LAPACK function slatms
 * Author: Intel Corporation
+* Generated April, 2011
 *****************************************************************************/
 
 #include "lapacke_utils.h"
--- src/lapacke/LAPACKE/src/use_xblas/lapacke_dposvxx.c
+++ src/lapacke/LAPACKE/src/use_xblas/lapacke_dposvxx.c
@@ -28,6 +28,7 @@
 *****************************************************************************
 * Contents: Native high-level C interface to LAPACK function dposvxx
 * Author: Intel Corporation
+* Generated November 2015
 *****************************************************************************/
 
 #include "lapacke_utils.h"

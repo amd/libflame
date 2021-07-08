d9ca9f3f8ea9c1083ec8450a4a1adb793454b28c
Pradeep Rao <pradeep.rao@amd.com>
2021-07-08 11:58:20 +0530

    Enable complex return types for cladiv and zladiv functions
    cladiv and zladiv had only void return signature. This commit
    includes support for complex return type as well, which will be
    the default. In order to enable void return type for these
    functions, the use configure option --enable-void-return-complex
    while compiling.
    
    Change-Id: I12609c7072b499d2a707c4089c660ab164cf9200

--- src/map/lapack2flamec/f2c/c/clatbs.c
+++ src/map/lapack2flamec/f2c/c/clatbs.c
@@ -724,8 +724,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                     }
                     i__3 = j;
                     cladiv_f2c_(&q__1, &x[j], &tjjs);
-                    x[i__3].real = q__1.real;
-                    x[i__3].imag = q__1.imag; // , expr subst
+                    x[i__3].r = q__1.r;
+                    x[i__3].i = q__1.i; // , expr subst
                     i__3 = j;
                     xj = (r__1 = x[i__3].r, f2c_abs(r__1)) + (r__2 = r_imag(&x[j]) , f2c_abs(r__2));
                 }
@@ -749,8 +749,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                     }
                     i__3 = j;
                     cladiv_f2c_(&q__1, &x[j], &tjjs);
-                    x[i__3].real = q__1.real;
-                    x[i__3].imag = q__1.imag; // , expr subst
+                    x[i__3].r = q__1.r;
+                    x[i__3].i = q__1.i; // , expr subst
                     i__3 = j;
                     xj = (r__1 = x[i__3].r, f2c_abs(r__1)) + (r__2 = r_imag(&x[j]) , f2c_abs(r__2));
                 }
@@ -881,10 +881,10 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         /* Computing MIN */
                         r__1 = 1.f;
                         r__2 = rec * tjj; // , expr subst
-                        rec = fla_min(r__1, r__2);
+                        rec = min(r__1,r__2);
                         cladiv_f2c_(&q__1, &uscal, &tjjs);
-                        uscal.real = q__1.real;
-                        uscal.imag = q__1.imag; // , expr subst
+                        uscal.r = q__1.r;
+                        uscal.i = q__1.i; // , expr subst
                     }
                     if(rec < 1.f)
                     {
@@ -1021,8 +1021,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         }
                         i__3 = j;
                         cladiv_f2c_(&q__1, &x[j], &tjjs);
-                        x[i__3].real = q__1.real;
-                        x[i__3].imag = q__1.imag; // , expr subst
+                        x[i__3].r = q__1.r;
+                        x[i__3].i = q__1.i; // , expr subst
                     }
                     else if(tjj > 0.f)
                     {
@@ -1037,8 +1037,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         }
                         i__3 = j;
                         cladiv_f2c_(&q__1, &x[j], &tjjs);
-                        x[i__3].real = q__1.real;
-                        x[i__3].imag = q__1.imag; // , expr subst
+                        x[i__3].r = q__1.r;
+                        x[i__3].i = q__1.i; // , expr subst
                     }
                     else
                     {
@@ -1066,10 +1066,10 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                     /* product has already been divided by 1/A(j,j). */
                     i__3 = j;
                     cladiv_f2c_(&q__2, &x[j], &tjjs);
-                    q__1.real = q__2.real - csumj.real;
-                    q__1.imag = q__2.imag - csumj.imag; // , expr subst
-                    x[i__3].real = q__1.real;
-                    x[i__3].imag = q__1.imag; // , expr subst
+                    q__1.r = q__2.r - csumj.r;
+                    q__1.i = q__2.i - csumj.i; // , expr subst
+                    x[i__3].r = q__1.r;
+                    x[i__3].i = q__1.i; // , expr subst
                 }
                 /* Computing MAX */
                 i__3 = j;
@@ -1117,10 +1117,10 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         /* Computing MIN */
                         r__1 = 1.f;
                         r__2 = rec * tjj; // , expr subst
-                        rec = fla_min(r__1, r__2);
+                        rec = min(r__1,r__2);
                         cladiv_f2c_(&q__1, &uscal, &tjjs);
-                        uscal.real = q__1.real;
-                        uscal.imag = q__1.imag; // , expr subst
+                        uscal.r = q__1.r;
+                        uscal.i = q__1.i; // , expr subst
                     }
                     if(rec < 1.f)
                     {
@@ -1257,8 +1257,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         }
                         i__3 = j;
                         cladiv_f2c_(&q__1, &x[j], &tjjs);
-                        x[i__3].real = q__1.real;
-                        x[i__3].imag = q__1.imag; // , expr subst
+                        x[i__3].r = q__1.r;
+                        x[i__3].i = q__1.i; // , expr subst
                     }
                     else if(tjj > 0.f)
                     {
@@ -1273,8 +1273,8 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                         }
                         i__3 = j;
                         cladiv_f2c_(&q__1, &x[j], &tjjs);
-                        x[i__3].real = q__1.real;
-                        x[i__3].imag = q__1.imag; // , expr subst
+                        x[i__3].r = q__1.r;
+                        x[i__3].i = q__1.i; // , expr subst
                     }
                     else
                     {
@@ -1302,10 +1302,10 @@ void aocl_lapack_clatbs(char *uplo, char *trans, char *diag, char *normin, aocl_
                     /* product has already been divided by 1/A(j,j). */
                     i__3 = j;
                     cladiv_f2c_(&q__2, &x[j], &tjjs);
-                    q__1.real = q__2.real - csumj.real;
-                    q__1.imag = q__2.imag - csumj.imag; // , expr subst
-                    x[i__3].real = q__1.real;
-                    x[i__3].imag = q__1.imag; // , expr subst
+                    q__1.r = q__2.r - csumj.r;
+                    q__1.i = q__2.i - csumj.i; // , expr subst
+                    x[i__3].r = q__1.r;
+                    x[i__3].i = q__1.i; // , expr subst
                 }
                 /* Computing MAX */
                 i__3 = j;

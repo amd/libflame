acc877025ac9401265635c459de79cf4b91f78b5
varajago <varajago@amd.com>
2021-07-11 20:52:45 +0530

    LibFLAME: Optimization of DGETRF for small matrices.
        A simple unblocked variant to get better performance
    for very small matrices.
    
        Signed-off-by: Vasanthakumar R <varajago@amd.com>
        AMD-Internal: CPUPL-1454
    
    Change-Id: Ia7f8821ca9a9b92fcdbf4b983a5e4b91f73aa048

--- src/lapack/dec/lu/piv/front/flamec/FLA_LU_piv.h
+++ src/lapack/dec/lu/piv/front/flamec/FLA_LU_piv.h
@@ -15,32 +15,17 @@
 #include "FLA_LU_piv_vars.h"
 
 FLA_Error FLA_LU_piv_internal( FLA_Obj A, FLA_Obj p, fla_lu_t* cntl );
-fla_dim_t   FLA_LU_piv_small_s_var0( fla_dim_t *m, fla_dim_t *n, real *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-fla_dim_t   FLA_LU_piv_small_s_var1( fla_dim_t *m, fla_dim_t *n, real *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-fla_dim_t   FLA_LU_piv_small_d_var0( fla_dim_t *m, fla_dim_t *n, doublereal *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-fla_dim_t   FLA_LU_piv_small_d_var1( fla_dim_t *m, fla_dim_t *n, doublereal *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-fla_dim_t   FLA_LU_piv_small_d_var2( fla_dim_t *m, fla_dim_t *n, doublereal *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-int   FLA_LU_piv_small_z_var0( fla_dim_t *m, fla_dim_t *n, dcomplex *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info);
-int   FLA_LU_piv_z_var0(fla_dim_t *m, fla_dim_t *n, dcomplex *a, fla_dim_t *lda, aocl_int_t *ipiv, fla_dim_t *info);
-int   FLA_LU_piv_z_parallel( fla_dim_t *m, fla_dim_t *n, dcomplex *a, fla_dim_t *lda, aocl_int_t *ipiv, fla_dim_t *info);
-int   FLA_LU_piv_z_var1_parallel(fla_dim_t* m, fla_dim_t* n, dcomplex* a, fla_dim_t* lda, aocl_int_t* ipiv, fla_dim_t* info);
-fla_dim_t   FLA_LU_piv_small_s_var0( fla_dim_t *m, fla_dim_t *n, real *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-fla_dim_t   FLA_LU_piv_small_s_var1( fla_dim_t *m, fla_dim_t *n, real *a, fla_dim_t *lda,
-                                   aocl_int_t *ipiv, fla_dim_t *info );
-int FLA_LU_piv_d_parallel( fla_dim_t *m, fla_dim_t *n, doublereal *a, fla_dim_t *lda, aocl_int_t *ipiv, fla_dim_t *info);
-
-int FLA_LU_piv_s_parallel( fla_dim_t *m, fla_dim_t *n, real *a, fla_dim_t *lda, aocl_int_t *ipiv, fla_dim_t *info);
+integer   FLA_LU_piv_small_d_var0( integer *m, integer *n, doublereal *a, integer *lda,
+                                   integer *ipiv, integer *info );
+integer   FLA_LU_piv_small_d_var1( integer *m, integer *n, doublereal *a, integer *lda,
+                                   integer *ipiv, integer *info );
+integer   FLA_LU_piv_small_d_var2( integer *m, integer *n, doublereal *a, integer *lda,
+                                   integer *ipiv, integer *info );
 
 FLA_Error FLA_LU_piv_solve( FLA_Obj A, FLA_Obj p, FLA_Obj B, FLA_Obj X );
 
 FLA_Error FLASH_LU_piv_solve( FLA_Obj A, FLA_Obj p, FLA_Obj B, FLA_Obj X );
+
 integer lapack_cgetf2(integer *m, integer *n, complex *a, integer *lda,
 	 integer *ipiv, integer *info);
 integer lapack_cgetrf(integer *m, integer *n, complex *a, integer *lda,
@@ -56,4 +41,4 @@ integer lapack_sgetrf(integer *m, integer *n, real *a, integer *lda,
 integer lapack_zgetf2(integer *m, integer *n, doublecomplex *a,
 	integer *lda, integer *ipiv, integer *info);
 integer lapack_zgetrf(integer *m, integer *n, doublecomplex *a,
-	integer *lda, integer *ipiv, integer *info);	
+	integer *lda, integer *ipiv, integer *info);

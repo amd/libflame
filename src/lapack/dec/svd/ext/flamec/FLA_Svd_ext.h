9e6dd868608b676e5de2d68a69c1a7bdfb7c1450
“swajoshi” <Swapnil.Joshi@amd.com>
2021-08-19 17:10:22 +0530

    DGESVD optimization
    Optimization of dgesvd by using native lapack algorithm involving bidiagonal reduction converted to c
    
    Signed-off-by: Swapnil Joshi <swapnil.joshi@amd.com>
    AMD-Internal: CPUPL-1756
    Change-Id: I1a01f29b0c96f4ff0fc8d7dbea63036c875b732a

--- src/lapack/dec/svd/ext/flamec/FLA_Svd_ext.h
+++ src/lapack/dec/svd/ext/flamec/FLA_Svd_ext.h
@@ -14,64 +14,47 @@
 FLA_Error FLA_Svd_ext_u_unb_var1( FLA_Svd_type jobu, FLA_Svd_type jobv, 
                                   fla_dim_t n_iter_max,
                                   FLA_Obj A, FLA_Obj s, FLA_Obj V, FLA_Obj U,
-                                  fla_dim_t k_accum,
-                                  fla_dim_t b_alg );
-int lapack_dbdsqr(char *uplo, aocl_int64_t *n, aocl_int64_t *ncvt, aocl_int64_t *
-	          nru, aocl_int64_t *ncc, doublereal *d__, doublereal *e, doublereal *vt, 
-	          aocl_int64_t *ldvt, doublereal *u, aocl_int64_t *ldu, doublereal *c__, aocl_int64_t *
-	          ldc, doublereal *work, aocl_int64_t *info);
-int lapack_dbdsqr_small(char *uplo, aocl_int64_t *n, aocl_int64_t *ncvt, aocl_int64_t *nru,
-                                doublereal *d__, doublereal *e,
-                                doublereal *vt, aocl_int64_t *ldvt,
-                                doublereal *u, aocl_int64_t *ldu,
-                                aocl_int64_t *info);
-int lapack_dgebd2(aocl_int64_t *m, aocl_int64_t *n, doublereal *a, aocl_int64_t *
+                                  dim_t k_accum,
+                                  dim_t b_alg );
+int lapack_dbdsqr(char *uplo, integer *n, integer *ncvt, integer *
+	          nru, integer *ncc, doublereal *d__, doublereal *e, doublereal *vt, 
+	          integer *ldvt, doublereal *u, integer *ldu, doublereal *c__, integer *
+	          ldc, doublereal *work, integer *info);
+int lapack_dgebd2(integer *m, integer *n, doublereal *a, integer *
 	          lda, doublereal *d__, doublereal *e, doublereal *tauq, doublereal *
-	          taup, doublereal *work, aocl_int64_t *info);
-int lapack_dgebrd(aocl_int64_t *m, aocl_int64_t *n, doublereal *a, aocl_int64_t *
+	          taup, doublereal *work, integer *info);
+int lapack_dgebrd(integer *m, integer *n, doublereal *a, integer *
 	          lda, doublereal *d__, doublereal *e, doublereal *tauq, doublereal *
-	          taup, doublereal *work, aocl_int64_t *lwork, aocl_int64_t *info);
-int lapack_dgelqf(aocl_int64_t *m, aocl_int64_t *n, doublereal *a, aocl_int64_t *
-	          lda, doublereal *tau, doublereal *work, aocl_int64_t *lwork, aocl_int64_t *info);
-int lapack_dgelq2(aocl_int64_t *m, aocl_int64_t *n, doublereal *a, aocl_int64_t * lda, 
-                  doublereal *tau, doublereal *work, aocl_int64_t *info);
-int lapack_dgesvd(char *jobu, char *jobvt, aocl_int64_t *m, aocl_int64_t *n, 
-	          doublereal *a, aocl_int64_t *lda, doublereal *s, doublereal *u, aocl_int64_t *
-	          ldu, doublereal *vt, aocl_int64_t *ldvt, doublereal *work, aocl_int64_t *lwork, 
-	          aocl_int64_t *info);
-int lapack_dorg2r(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal *
-	          a, aocl_int64_t *lda, doublereal *tau, doublereal *work, aocl_int64_t *info);
-int lapack_dorgbr(char *vect, aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, 
-	          doublereal *a, aocl_int64_t *lda, doublereal *tau, doublereal *work, 
-	          aocl_int64_t *lwork, aocl_int64_t *info);
-int lapack_dorgl2(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal *
-	          a, aocl_int64_t *lda, doublereal *tau, doublereal *work, aocl_int64_t *info);
-int lapack_dorglq(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal *
-	          a, aocl_int64_t *lda, doublereal *tau, doublereal *work, aocl_int64_t *lwork, 
-	          aocl_int64_t *info);
-int lapack_dorgqr(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal *
-	          a, aocl_int64_t *lda, doublereal *tau, doublereal *work, aocl_int64_t *lwork, 
-	          aocl_int64_t *info);
-int lapack_dorm2r(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, 
-                  doublereal *a, aocl_int64_t *lda, doublereal *tau, doublereal * c__, 
-                  aocl_int64_t *ldc, doublereal *work, aocl_int64_t *info);
-int lapack_dormbr(char *vect, char *side, char *trans, aocl_int64_t *m, 
-	          aocl_int64_t *n, aocl_int64_t *k, doublereal *a, aocl_int64_t *lda, doublereal *tau, 
-	          doublereal *c__, aocl_int64_t *ldc, doublereal *work, aocl_int64_t *lwork, 
-	          aocl_int64_t *info);
-int lapack_dormlq(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *n, 
-                  aocl_int64_t *k, doublereal *a, aocl_int64_t *lda, doublereal *tau, doublereal *
-	          c__, aocl_int64_t *ldc, doublereal *work, aocl_int64_t *lwork, aocl_int64_t *info);
-int lapack_dorml2(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, 
-                  doublereal *a, aocl_int64_t *lda, doublereal *tau, doublereal * c__, 
-                  aocl_int64_t *ldc, doublereal *work, aocl_int64_t *info); 
-int lapack_dormqr(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *n, 
-	          aocl_int64_t *k, doublereal *a, aocl_int64_t *lda, doublereal *tau, doublereal *
-	          c__, aocl_int64_t *ldc, doublereal *work, aocl_int64_t *lwork, aocl_int64_t *info);
-int  dgesvd2x2(   char *jobu, char *jobvt, aocl_int64_t *m, aocl_int64_t *n,
-                  doublereal *a, aocl_int64_t *lda, doublereal *s, doublereal *u, aocl_int64_t *
-                  ldu, doublereal *vt, aocl_int64_t *ldvt, doublereal *work, aocl_int64_t *lwork,
-                  aocl_int64_t *info);
+	          taup, doublereal *work, integer *lwork, integer *info);
+int lapack_dgelqf(integer *m, integer *n, doublereal *a, integer *
+	          lda, doublereal *tau, doublereal *work, integer *lwork, integer *info);
+int lapack_dgesvd(char *jobu, char *jobvt, integer *m, integer *n, 
+	          doublereal *a, integer *lda, doublereal *s, doublereal *u, integer *
+	          ldu, doublereal *vt, integer *ldvt, doublereal *work, integer *lwork, 
+	          integer *info);
+int lapack_dorg2r(integer *m, integer *n, integer *k, doublereal *
+	          a, integer *lda, doublereal *tau, doublereal *work, integer *info);
+int lapack_dorgbr(char *vect, integer *m, integer *n, integer *k, 
+	          doublereal *a, integer *lda, doublereal *tau, doublereal *work, 
+	          integer *lwork, integer *info);
+int lapack_dorgl2(integer *m, integer *n, integer *k, doublereal *
+	          a, integer *lda, doublereal *tau, doublereal *work, integer *info);
+int lapack_dorglq(integer *m, integer *n, integer *k, doublereal *
+	          a, integer *lda, doublereal *tau, doublereal *work, integer *lwork, 
+	          integer *info);
+int lapack_dorgqr(integer *m, integer *n, integer *k, doublereal *
+	          a, integer *lda, doublereal *tau, doublereal *work, integer *lwork, 
+	          integer *info);
+int lapack_dormbr(char *vect, char *side, char *trans, integer *m, 
+	          integer *n, integer *k, doublereal *a, integer *lda, doublereal *tau, 
+	          doublereal *c__, integer *ldc, doublereal *work, integer *lwork, 
+	          integer *info);
+int lapack_dormlq(char *side, char *trans, integer *m, integer *n, 
+                  integer *k, doublereal *a, integer *lda, doublereal *tau, doublereal *
+	          c__, integer *ldc, doublereal *work, integer *lwork, integer *info);
+int lapack_dormqr(char *side, char *trans, integer *m, integer *n, 
+	          integer *k, doublereal *a, integer *lda, doublereal *tau, doublereal *
+	          c__, integer *ldc, doublereal *work, integer *lwork, integer *info);
 
 
 

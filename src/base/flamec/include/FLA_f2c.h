f8d38c2ac5612f59f5d4786b332ca39670398772
Saitharun <Katteboina.Saitharun@amd.com>
2021-08-30 12:48:39 +0530

    Adding ENABLE_WRAPPER CMAKE OPTION
    details: Wrapper code will be enabled when selecting the cmake option
    ENABLE_WRAPPER.
    
    AMD-Internal: [CPUPL-1848]
    
    Change-Id: Idfb36567b8c550c4ead1f4ae61f4a6b74163ae84

--- src/base/flamec/include/FLA_f2c.h
+++ src/base/flamec/include/FLA_f2c.h
@@ -394,9 +394,13 @@ typedef struct Namelist Namelist;
 #endif
 #endif
 
+#ifdef FLA_ENABLE_EXTRA_SYMBOLS
 #define lsame_ lsame_lf
 #define xerbla_ xerbla_lf
-/*#if (defined(_WIN32) || defined(_WIN64))
+#endif
+
+#ifndef FLA_ENABLE_EXTRA_SYMBOLS
+#if (defined(_WIN32) || defined(_WIN64))
 #ifdef UPPER
 #define  isamax_   ISAMAX     
 #define  idamax_   IDAMAX     
@@ -4683,4 +4687,4 @@ typedef struct Namelist Namelist;
 #define zgetrfnpi_               zgetrfnpi
 #endif
 #endif 
-*/
+#endif

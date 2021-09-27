98ef19f7d1c37740d26428e06de52de78928ffb6
dnikku <Deepika.Nikku@amd.com>
2021-09-27 17:48:57 +0530

    libflame: Added ILP64 changes to DTL code in slasyf_aa.c, zhecon_3.c,zhecon_rook.c, zhesv_aa.c, zhesv_aa_2stage.c, zhesv_rk.c, zhesv_rook.c
    Enabled ILP64 changes in DTL code in specified files
    
    Change-Id: I90c1833534aa2d6d142a41fad6c4ec5bf50bcdc0

--- src/map/lapack2flamec/f2c/c/zhesv_rook.c
+++ src/map/lapack2flamec/f2c/c/zhesv_rook.c
@@ -210,9 +210,9 @@ void zhesv_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *a, aocl_
                  aocl_int_t *lwork, aocl_int_t *info)
 {
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE 
-    char buffer[256]; 
-    snprintf(buffer, 256,"zhesv inputs: uplo %c, n %d, nrhs %d, lda %d, ldb %d",*uplo, *n, *nrhs, *lda, *ldb);
+#if AOCL_DTL_LOG_ENABLE
+    char buffer[256];
+    snprintf(buffer, 256,"zhesv_rook inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS ", lwork %" FLA_IS "",*uplo, *n, *nrhs, *lda, *ldb, *lwork);
     AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
     /* System generated locals */

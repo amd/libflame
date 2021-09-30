02310dfe039503973f35c535083a771ef374ca9d
Jintu Das <Jintu.Das@amd.com>
2021-09-30 17:47:20 +0530

    Updated postprocess script (f2c)
    Added replacement of 'ladiv_(' with 'ladiv_f2c_('
    Added: 'ladiv_('    'ladiv_f2c_(' \
    updated the replacement of 'abs_(', 'dotc_' and 'dotu_' with the following statements-
    'dotc_('    'dotc_f2c_(' \
    'dotu_('    'dotu_f2c_(' \
    ' abs('     ' f2c_abs(' \
    
    Signed-off-by: Jintu Das <Jintu.Das@amd.com>
    AMD-Internal: CPUPL-1891
    Change-Id: Id510a0d784ecd01a6366faed06a606bdff398273

--- src/map/lapack2flamec/f2c/c/postprocess.sh
+++ src/map/lapack2flamec/f2c/c/postprocess.sh
@@ -18,14 +18,11 @@ main()
         'f90_cycle__()'             'continue' \
         'f90_exit__()'              'break' \
         'include \"f2c.h\"'         'include \"FLA_f2c.h\"' \
-        'dotc_'                     'dotc_f2c_' \
-        'dotu_'                     'dotu_f2c_' \
-        'abs('                      'f2c_abs' \
+        'dotc_('                    'dotc_f2c_(' \
+        'dotu_('                    'dotu_f2c_(' \
+        ' abs('                     ' f2c_abs(' \
         '__('                       '_(' \
-	    'cladiv_('                   'cladiv_f2c_(' \
-        'zladiv_('                   'zladiv_f2c_(' \
-        ' max('                      ' fla_max('\
-        ' min('                      ' fla_min('\
+	'ladiv_('                   'ladiv_f2c_(' \
         )
 
     # '\\#include \"blaswrap.h\"' ' ' \

40da84e21ec5eb75dba3d901229e7dace1b2e74b
prangana <pradeep.rao@amd.com>
2021-10-20 01:47:40 -0400

    Update supported LAPACK version to 3.10.0
    Update supported LAPACK version to 3.10.0 in ilaver.c
    
    Change-Id: Ib137641d25fccfc4f57fd13eac883fa0c35df261

--- src/map/lapack2flamec/f2c/c/ilaver.c
+++ src/map/lapack2flamec/f2c/c/ilaver.c
@@ -35,7 +35,6 @@
 /* > \author Univ. of California Berkeley */
 /* > \author Univ. of Colorado Denver */
 /* > \author NAG Ltd. */
-/* > \date November 2019 */
 /* > \ingroup auxOTHERauxiliary */
 /* ===================================================================== */
 /* Subroutine */
@@ -44,13 +43,13 @@ void ilaver_(aocl_int_t *vers_major__, aocl_int_t *vers_minor__, aocl_int_t *ver
 {
     /* Logging and tracing code */
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-    /* -- LAPACK computational routine (version 3.5.0) -- */
+    /* -- LAPACK computational routine-- */
     /* -- LAPACK is a software package provided by Univ. of Tennessee, -- */
     /* -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..-- */
     /* ===================================================================== */
     /* ===================================================================== */
     *vers_major__ = 3;
-    *vers_minor__ = 9;
+    *vers_minor__ = 10;
     *vers_patch__ = 0;
     /* ===================================================================== */
     AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);

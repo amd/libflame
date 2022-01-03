997ec0e907710085cc27c90bd1f0b6bd001845e5
swhirema <SwatiG.Hiremath@amd.com>
2022-01-03 21:21:46 +0530

    Added DTL feature to following functions: dorbdb5.c dorbdb6.c dorbdb.c dorg2l.c dorgql.c dorgr2.c dorgrq.c dorgtsqr.c dorhr_col.c dorm22.c dorm2l.c dormql.c
    Change-Id: Ice0f59307ea4e1c6fd49260ec4c5314b3b1c46b0

--- src/map/lapack2flamec/f2c/c/dorbdb6.c
+++ src/map/lapack2flamec/f2c/c/dorbdb6.c
@@ -162,36 +162,12 @@ void dorbdb6_(aocl_int_t *m1, aocl_int_t *m2, aocl_int_t *n, doublereal *x1, aoc
               doublereal *x2, aocl_int_t *incx2, doublereal *q1, aocl_int_t *ldq1, doublereal *q2,
               aocl_int_t *ldq2, doublereal *work, aocl_int_t *lwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dorbdb6(m1, m2, n, x1, incx1, x2, incx2, q1, ldq1, q2, ldq2, work, lwork, info);
-#else
-    aocl_int64_t m1_64 = *m1;
-    aocl_int64_t m2_64 = *m2;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t incx1_64 = *incx1;
-    aocl_int64_t incx2_64 = *incx2;
-    aocl_int64_t ldq1_64 = *ldq1;
-    aocl_int64_t ldq2_64 = *ldq2;
-    aocl_int64_t lwork_64 = *lwork;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dorbdb6(&m1_64, &m2_64, &n_64, x1, &incx1_64, x2, &incx2_64, q1, &ldq1_64, q2,
-                        &ldq2_64, work, &lwork_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dorbdb6 inputs: m1 %" FLA_IS ", m2 %" FLA_IS ", n %" FLA_IS ", incx1 %" FLA_IS ", incx2 %" FLA_IS ", ldq1 %" FLA_IS ", ldq2 %" FLA_IS ", lwork %" FLA_IS "",*m1, *m2, *n, *incx1, *incx2, *ldq1, *ldq2, *lwork);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dorbdb6(aocl_int64_t *m1, aocl_int64_t *m2, aocl_int64_t *n, doublereal *x1,
-                         aocl_int64_t *incx1, doublereal *x2, aocl_int64_t *incx2, doublereal *q1,
-                         aocl_int64_t *ldq1, doublereal *q2, aocl_int64_t *ldq2, doublereal *work,
-                         aocl_int64_t *lwork, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dorbdb6 inputs: m1 %" FLA_IS ", m2 %" FLA_IS ", n %" FLA_IS
-                      ", incx1 %" FLA_IS ", incx2 %" FLA_IS ", ldq1 %" FLA_IS ", ldq2 %" FLA_IS
-                      ", lwork %" FLA_IS "",
-                      *m1, *m2, *n, *incx1, *incx2, *ldq1, *ldq2, *lwork);
     /* System generated locals */
     aocl_int64_t q1_dim1, q1_offset, q2_dim1, q2_offset, i__1, i__2;
     /* Builtin functions */
@@ -268,9 +244,9 @@ void aocl_lapack_dorbdb6(aocl_int64_t *m1, aocl_int64_t *m2, aocl_int64_t *n, do
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DORBDB6", &i__1, (ftnlen)7);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DORBDB6", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     eps = dlamch_("Precision");
     /* Compute the Euclidean norm of X */
@@ -309,25 +285,13 @@ void aocl_lapack_dorbdb6(aocl_int64_t *m1, aocl_int64_t *m2, aocl_int64_t *n, do
     /* Otherwise, project again. */
     if(norm_new__ >= norm * .83)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(norm_new__ <= *n * eps * norm)
     {
-        i__1 = (*m1 - 1) * *incx1 + 1;
-        i__2 = *incx1;
-        for(ix = 1; i__2 < 0 ? ix >= i__1 : ix <= i__1; ix += i__2)
-        {
-            x1[ix] = 0.;
-        }
-        i__2 = (*m2 - 1) * *incx2 + 1;
-        i__1 = *incx2;
-        for(ix = 1; i__1 < 0 ? ix >= i__2 : ix <= i__2; ix += i__1)
-        {
-            x2[ix] = 0.;
-        }
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     norm = norm_new__;
     i__1 = *n;
@@ -376,8 +340,8 @@ void aocl_lapack_dorbdb6(aocl_int64_t *m1, aocl_int64_t *m2, aocl_int64_t *n, do
             x2[ix] = 0.;
         }
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DORBDB6 */
 }
 /* dorbdb6_ */

79e652db241cc8cde91c3a7f8e5b9342b5a2b2e9
swhirema <SwatiG.Hiremath@amd.com>
2022-01-03 21:13:15 +0530

    Added DTL feature to following functions: dlasy2.c dla_syamv.c dlasyf_aa.c dlasyf.c dlasyf_rk.c dlasyf_rook.c dla_syrcond.c dla_syrfsx_extended.c dla_syrpvgrw.c dlat2s.c dla_wwaddw.c
    Change-Id: I96aca38219c0b5796708578355f6d43be75445e7

--- src/map/lapack2flamec/f2c/c/dlat2s.c
+++ src/map/lapack2flamec/f2c/c/dlat2s.c
@@ -111,26 +111,12 @@ if INFO>0, the content of */
 void dlat2s_(char *uplo, aocl_int_t *n, doublereal *a, aocl_int_t *lda, real *sa, aocl_int_t *ldsa,
              aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dlat2s(uplo, n, a, lda, sa, ldsa, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldsa_64 = *ldsa;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dlat2s(uplo, &n_64, a, &lda_64, sa, &ldsa_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dlat2s inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS ", ldsa %" FLA_IS "",*uplo, *n, *lda, *ldsa);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dlat2s(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t *lda, real *sa,
-                        aocl_int64_t *ldsa, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dlat2s inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS ", ldsa %" FLA_IS "",
-                      *uplo, *n, *lda, *ldsa);
     /* System generated locals */
     aocl_int64_t sa_dim1, sa_offset, a_dim1, a_offset, i__1, i__2;
     /* Local variables */
@@ -203,8 +189,8 @@ void aocl_lapack_dlat2s(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t
         }
     }
 L50:
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DLAT2S */
 }
 /* dlat2s_ */

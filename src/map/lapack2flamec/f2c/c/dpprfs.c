bab6a39016272b71f218dd47c3c7b53674d608b4
swhirema <SwatiG.Hiremath@amd.com>
2022-01-04 15:51:18 +0530

    Added DTL feature to following functions: dposvxx.c dpotrf2.c dpotrs.c dppcon.c dppequ.c dpprfs.c dppsv.c dppsvx.c dpptrf.c dpptri.c dpptrs.c
    Change-Id: I356075872e0b1354dcc6ef372b5d90367e622a74

--- src/map/lapack2flamec/f2c/c/dpprfs.c
+++ src/map/lapack2flamec/f2c/c/dpprfs.c
@@ -175,31 +175,12 @@ void dpprfs_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, doublereal *ap, double
              doublereal *b, aocl_int_t *ldb, doublereal *x, aocl_int_t *ldx, doublereal *ferr,
              doublereal *berr, doublereal *work, aocl_int_t *iwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dpprfs(uplo, n, nrhs, ap, afp, b, ldb, x, ldx, ferr, berr, work, iwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t ldx_64 = *ldx;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dpprfs(uplo, &n_64, &nrhs_64, ap, afp, b, &ldb_64, x, &ldx_64, ferr, berr, work,
-                       iwork, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dpprfs inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS ", ldx %" FLA_IS "",*uplo, *n, *nrhs, *ldb, *ldx);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dpprfs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doublereal *ap,
-                        doublereal *afp, doublereal *b, aocl_int64_t *ldb, doublereal *x,
-                        aocl_int64_t *ldx, doublereal *ferr, doublereal *berr, doublereal *work,
-                        aocl_int_t *iwork, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dpprfs inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS
-                      ", ldx %" FLA_IS "",
-                      *uplo, *n, *nrhs, *ldb, *ldx);
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, x_dim1, x_offset, i__1, i__2, i__3;
     doublereal d__1, d__2, d__3;
@@ -281,9 +262,9 @@ void aocl_lapack_dpprfs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doubler
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DPPRFS", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DPPRFS", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0 || *nrhs == 0)
@@ -295,8 +276,8 @@ void aocl_lapack_dpprfs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doubler
             berr[j] = 0.;
             /* L10: */
         }
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* NZ = maximum number of nonzero elements in each row of A, plus 1 */
     nz = *n + 1;
@@ -484,8 +465,8 @@ void aocl_lapack_dpprfs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doubler
         }
         /* L140: */
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DPPRFS */
 }
 /* dpprfs_ */

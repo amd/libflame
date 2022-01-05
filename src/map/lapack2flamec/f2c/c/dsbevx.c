56b568a6eef597263509de78168f9c85fc609a47
swhirema <SwatiG.Hiremath@amd.com>
2022-01-05 18:26:07 +0530

    Added DTL feature to following functions: dsbev.c dsbevd_2stage.c dsbevd.c dsbevx_2stage.c dsbevx.c dsbgst.c dsbgv.c dsbgvd.c dsbgvx.c dsbtrd.c dsfrk.c dsgesv.c dspcon.c
    Change-Id: I1dfc3b3c81b949ffa8301e6665fb57ac14db10dc

--- src/map/lapack2flamec/f2c/c/dsbevx.c
+++ src/map/lapack2flamec/f2c/c/dsbevx.c
@@ -270,40 +270,12 @@ void dsbevx_(char *jobz, char *range, char *uplo, aocl_int_t *n, aocl_int_t *kd,
              doublereal *z__, aocl_int_t *ldz, doublereal *work, aocl_int_t *iwork,
              aocl_int_t *ifail, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dsbevx(jobz, range, uplo, n, kd, ab, ldab, q, ldq, vl, vu, il, iu, abstol, m, w,
-                       z__, ldz, work, iwork, ifail, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t kd_64 = *kd;
-    aocl_int64_t ldab_64 = *ldab;
-    aocl_int64_t ldq_64 = *ldq;
-    aocl_int64_t il_64 = *il;
-    aocl_int64_t iu_64 = *iu;
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t ldz_64 = *ldz;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dsbevx(jobz, range, uplo, &n_64, &kd_64, ab, &ldab_64, q, &ldq_64, vl, vu, &il_64,
-                       &iu_64, abstol, &m_64, w, z__, &ldz_64, work, iwork, ifail, &info_64);
-
-    *m = (aocl_int_t)m_64;
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dsbevx inputs: jobz %c, range %c, uplo %c, n %" FLA_IS ", kd %" FLA_IS ", ldab %" FLA_IS ", ldq %" FLA_IS ", il %" FLA_IS ", iu %" FLA_IS ", ldz %" FLA_IS "",*jobz, *range, *uplo, *n, *kd, *ldab, *ldq, *il, *iu, *ldz);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dsbevx(char *jobz, char *range, char *uplo, aocl_int64_t *n, aocl_int64_t *kd,
-                        doublereal *ab, aocl_int64_t *ldab, doublereal *q, aocl_int64_t *ldq,
-                        doublereal *vl, doublereal *vu, aocl_int64_t *il, aocl_int64_t *iu,
-                        doublereal *abstol, aocl_int64_t *m, doublereal *w, doublereal *z__,
-                        aocl_int64_t *ldz, doublereal *work, aocl_int_t *iwork, aocl_int_t *ifail,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dsbevx inputs: jobz %c, range %c, uplo %c, n %" FLA_IS ", kd %" FLA_IS
-                      ", ldab %" FLA_IS ", ldq %" FLA_IS ", il %" FLA_IS ", iu %" FLA_IS
-                      ", ldz %" FLA_IS "",
-                      *jobz, *range, *uplo, *n, *kd, *ldab, *ldq, *il, *iu, *ldz);
     /* System generated locals */
     aocl_int64_t ab_dim1, ab_offset, q_dim1, q_offset, z_dim1, z_offset, i__1, i__2;
     doublereal d__1, d__2;
@@ -435,16 +407,16 @@ void aocl_lapack_dsbevx(char *jobz, char *range, char *uplo, aocl_int64_t *n, ao
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DSBEVX", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DSBEVX", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     *m = 0;
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(*n == 1)
     {
@@ -472,8 +444,8 @@ void aocl_lapack_dsbevx(char *jobz, char *range, char *uplo, aocl_int64_t *n, ao
                 z__[z_dim1 + 1] = 1.;
             }
         }
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Get machine constants. */
     safmin = dlamch_("Safe minimum");
@@ -662,8 +634,8 @@ L30:
             /* L50: */
         }
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DSBEVX */
 }
 /* dsbevx_ */

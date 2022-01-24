1bbf59f98991bce1fe1c1377ac4ef83ca0a57e91
swhirema <SwatiG.Hiremath@amd.com>
2022-01-24 14:37:11 +0530

    Added DTL feature to following functions: dsptrd.c dsptrf.c dsptri.c dsptrs.c dstebz.c dstedc.c dstegr.c dstein.c dstemr.c dsteqr.c dsterf.c dstev.c
    Change-Id: Id6db66dade0fbcbfe6234295b42b2dbf5f6b3368

--- src/map/lapack2flamec/f2c/c/dsptrs.c
+++ src/map/lapack2flamec/f2c/c/dsptrs.c
@@ -119,26 +119,12 @@ static doublereal c_b19 = 1.;
 void dsptrs_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, doublereal *ap, aocl_int_t *ipiv,
              doublereal *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dsptrs(uplo, n, nrhs, ap, ipiv, b, ldb, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dsptrs(uplo, &n_64, &nrhs_64, ap, ipiv, b, &ldb_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dsptrs inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ipiv %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *ipiv, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dsptrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doublereal *ap,
-                        aocl_int_t *ipiv, doublereal *b, aocl_int64_t *ldb, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dsptrs inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS "",
-                      *uplo, *n, *nrhs, *ldb);
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, i__1;
     doublereal d__1;
@@ -199,15 +185,15 @@ void aocl_lapack_dsptrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doubler
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DSPTRS", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DSPTRS", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0 || *nrhs == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     if(upper)
     {
@@ -457,8 +443,8 @@ void aocl_lapack_dsptrs(char *uplo, aocl_int64_t *n, aocl_int64_t *nrhs, doubler
         goto L90;
     L100:;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DSPTRS */
 }
 /* dsptrs_ */
--- src/map/lapack2flamec/f2c/c/dstegr.c
+++ src/map/lapack2flamec/f2c/c/dstegr.c
@@ -258,38 +258,12 @@ void dstegr_(char *jobz, char *range, aocl_int_t *n, doublereal *d__, doublereal
              doublereal *w, doublereal *z__, aocl_int_t *ldz, aocl_int_t *isuppz, doublereal *work,
              aocl_int_t *lwork, aocl_int_t *iwork, aocl_int_t *liwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dstegr(jobz, range, n, d__, e, vl, vu, il, iu, abstol, m, w, z__, ldz, isuppz, work,
-                       lwork, iwork, liwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t il_64 = *il;
-    aocl_int64_t iu_64 = *iu;
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t ldz_64 = *ldz;
-    aocl_int64_t lwork_64 = *lwork;
-    aocl_int64_t liwork_64 = *liwork;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dstegr(jobz, range, &n_64, d__, e, vl, vu, &il_64, &iu_64, abstol, &m_64, w, z__,
-                       &ldz_64, isuppz, work, &lwork_64, iwork, &liwork_64, &info_64);
-
-    *m = (aocl_int_t)m_64;
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dstegr inputs: jobz %c, range %c, n %" FLA_IS ", il %" FLA_IS ", iu %" FLA_IS ", ldz %" FLA_IS ", lwork %" FLA_IS ", liwork %" FLA_IS "",*jobz, *range, *n, *il, *iu, *ldz, *lwork, *liwork);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dstegr(char *jobz, char *range, aocl_int64_t *n, doublereal *d__, doublereal *e,
-                        doublereal *vl, doublereal *vu, aocl_int64_t *il, aocl_int64_t *iu,
-                        doublereal *abstol, aocl_int64_t *m, doublereal *w, doublereal *z__,
-                        aocl_int64_t *ldz, aocl_int_t *isuppz, doublereal *work,
-                        aocl_int64_t *lwork, aocl_int_t *iwork, aocl_int64_t *liwork,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dstegr inputs: jobz %c, range %c, n %" FLA_IS ", il %" FLA_IS ", iu %" FLA_IS
-                      ", ldz %" FLA_IS ", lwork %" FLA_IS ", liwork %" FLA_IS "",
-                      *jobz, *range, *n, *il, *iu, *ldz, *lwork, *liwork);
     /* System generated locals */
     aocl_int64_t z_dim1, z_offset;
     /* Local variables */
@@ -324,7 +298,7 @@ void aocl_lapack_dstegr(char *jobz, char *range, aocl_int64_t *n, doublereal *d_
     aocl_lapack_dstemr(jobz, range, n, &d__[1], &e[1], vl, vu, il, iu, m, &w[1], &z__[z_offset],
                        ldz, n, &isuppz[1], &tryrac, &work[1], lwork, &iwork[1], liwork, info);
     /* End of DSTEGR */
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
 }
 /* dstegr_ */

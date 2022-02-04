bd2b73ef805e80384e9a5ad02f9206b7de204d57
swhirema <SwatiG.Hiremath@amd.com>
2022-02-04 19:40:35 +0530

    Added DTL feature to following functions: dtprfb.c dtprfs.c dtptri.c dtptrs.c dtpttf.c dtpttr.c dtrcon.c dtrevc3.c dtrevc.c dtrexc.c dtrrfs.c dtrsen.c
    Change-Id: Icbfa0c11915a82f06784a9e6b8457614c6b91be2

--- src/map/lapack2flamec/f2c/c/dtptrs.c
+++ src/map/lapack2flamec/f2c/c/dtptrs.c
@@ -134,27 +134,12 @@ static aocl_int64_t c__1 = 1;
 void dtptrs_(char *uplo, char *trans, char *diag, aocl_int_t *n, aocl_int_t *nrhs, doublereal *ap,
              doublereal *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dtptrs(uplo, trans, diag, n, nrhs, ap, b, ldb, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dtptrs(uplo, trans, diag, &n_64, &nrhs_64, ap, b, &ldb_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dtptrs inputs: uplo %c, trans %c, diag %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS "",*uplo, *trans, *diag, *n, *nrhs, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dtptrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, aocl_int64_t *nrhs,
-                        doublereal *ap, doublereal *b, aocl_int64_t *ldb, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dtptrs inputs: uplo %c, trans %c, diag %c, n %" FLA_IS ", nrhs %" FLA_IS
-                      ", ldb %" FLA_IS "",
-                      *uplo, *trans, *diag, *n, *nrhs, *ldb);
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, i__1;
     /* Local variables */
@@ -219,15 +204,15 @@ void aocl_lapack_dtptrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DTPTRS", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DTPTRS", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Check for singularity. */
     if(nounit)
@@ -240,8 +225,8 @@ void aocl_lapack_dtptrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
             {
                 if(ap[jc + *info - 1] == 0.)
                 {
-                    AOCL_DTL_TRACE_LOG_EXIT
-                    return;
+                    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                    return 0;
                 }
                 jc += *info;
                 /* L10: */
@@ -255,8 +240,8 @@ void aocl_lapack_dtptrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
             {
                 if(ap[jc] == 0.)
                 {
-                    AOCL_DTL_TRACE_LOG_EXIT
-                    return;
+                    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                    return 0;
                 }
                 jc = jc + *n - *info + 1;
                 /* L20: */
@@ -271,8 +256,8 @@ void aocl_lapack_dtptrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
         aocl_blas_dtpsv(uplo, trans, diag, n, &ap[1], &b[j * b_dim1 + 1], &c__1);
         /* L30: */
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DTPTRS */
 }
 /* dtptrs_ */
--- src/map/lapack2flamec/f2c/c/dtrcon.c
+++ src/map/lapack2flamec/f2c/c/dtrcon.c
@@ -140,26 +140,12 @@ static aocl_int64_t c__1 = 1;
 void dtrcon_(char *norm, char *uplo, char *diag, aocl_int_t *n, doublereal *a, aocl_int_t *lda,
              doublereal *rcond, doublereal *work, aocl_int_t *iwork, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dtrcon(norm, uplo, diag, n, a, lda, rcond, work, iwork, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dtrcon(norm, uplo, diag, &n_64, a, &lda_64, rcond, work, iwork, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dtrcon inputs: norm %c, uplo %c, diag %c, n %" FLA_IS ", lda %" FLA_IS "",*norm, *uplo, *diag, *n, *lda);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dtrcon(char *norm, char *uplo, char *diag, aocl_int64_t *n, doublereal *a,
-                        aocl_int64_t *lda, doublereal *rcond, doublereal *work, aocl_int_t *iwork,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dtrcon inputs: norm %c, uplo %c, diag %c, n %" FLA_IS ", lda %" FLA_IS "",
-                      *norm, *uplo, *diag, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1;
     doublereal d__1;
@@ -234,16 +220,16 @@ void aocl_lapack_dtrcon(char *norm, char *uplo, char *diag, aocl_int64_t *n, dou
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DTRCON", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DTRCON", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0)
     {
         *rcond = 1.;
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     *rcond = 0.;
     smlnum = dlamch_("Safe minimum") * (doublereal)fla_max(1, *n);
@@ -301,8 +287,8 @@ void aocl_lapack_dtrcon(char *norm, char *uplo, char *diag, aocl_int64_t *n, dou
         }
     }
 L20:
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DTRCON */
 }
 /* dtrcon_ */

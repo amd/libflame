5416a9ddefc70512ba470b907913c1b4af7058af
swhirema <SwatiG.Hiremath@amd.com>
2022-02-04 19:49:24 +0530

    Added DTL feature to following functions: dtrsna.c dtrsyl.c dtrtrs.c dtrttf.c  dtrttp.c dgetsqrhrt.c dlaqz0.c dlaqz1.c dlaqz2.c dlaqz3.c dlaqz4.c dlarfb_gett.c dorgtsqr_row.c
    Change-Id: I21c1f66e1f2ecc9f2c1e99887acbe27e6c995c6e

--- src/map/lapack2flamec/f2c/c/dtrtrs.c
+++ src/map/lapack2flamec/f2c/c/dtrtrs.c
@@ -142,29 +142,12 @@ static doublereal c_b12 = 1.;
 void dtrtrs_(char *uplo, char *trans, char *diag, aocl_int_t *n, aocl_int_t *nrhs, doublereal *a,
              aocl_int_t *lda, doublereal *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dtrtrs(uplo, trans, diag, n, nrhs, a, lda, b, ldb, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t nrhs_64 = *nrhs;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldb_64 = *ldb;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dtrtrs(uplo, trans, diag, &n_64, &nrhs_64, a, &lda_64, b, &ldb_64, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dtrtrs inputs: uplo %c, trans %c, diag %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS "",*uplo, *trans, *diag, *n, *nrhs, *lda, *ldb);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dtrtrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, aocl_int64_t *nrhs,
-                        doublereal *a, aocl_int64_t *lda, doublereal *b, aocl_int64_t *ldb,
-                        aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dtrtrs inputs: uplo %c, trans %c, diag %c, n %" FLA_IS ", nrhs %" FLA_IS
-                      ", lda %" FLA_IS ", ldb %" FLA_IS "",
-                      *uplo, *trans, *diag, *n, *nrhs, *lda, *ldb);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     /* Local variables */
@@ -232,15 +215,15 @@ void aocl_lapack_dtrtrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DTRTRS", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DTRTRS", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Check for singularity. */
     if(nounit)
@@ -250,18 +233,17 @@ void aocl_lapack_dtrtrs(char *uplo, char *trans, char *diag, aocl_int64_t *n, ao
         {
             if(a[*info + *info * a_dim1] == 0.)
             {
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L10: */
         }
     }
     *info = 0;
     /* Solve A * x = b or A**T * x = b. */
-    aocl_blas_dtrsm("Left", uplo, trans, diag, n, nrhs, &c_b12, &a[a_offset], lda, &b[b_offset],
-                    ldb);
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    dtrsm_("Left", uplo, trans, diag, n, nrhs, &c_b12, &a[a_offset], lda, &b[ b_offset], ldb);
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DTRTRS */
 }
 /* dtrtrs_ */

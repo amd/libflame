0a516081723044141bef0db8f58e3994405295b9
swhirema <SwatiG.Hiremath@amd.com>
2022-02-04 16:27:46 +0530

    Added DTL feature to following functions: dsytri2.c dsytri2x.c dsytri_3.c dsytri_3x.c dsytri.c dsytri_rook.c dsytrs2.c dsytrs_3.c dsytrs_aa_2stage.c dsytrs_aa.c dsytrs.c
    Change-Id: Iba5d5191a9ad741316dd3ea3b555d4ac27aaf8cb

--- src/map/lapack2flamec/f2c/c/dsytri.c
+++ src/map/lapack2flamec/f2c/c/dsytri.c
@@ -120,24 +120,12 @@ the matrix is singular and its */
 void dsytri_(char *uplo, aocl_int_t *n, doublereal *a, aocl_int_t *lda, aocl_int_t *ipiv,
              doublereal *work, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_dsytri(uplo, n, a, lda, ipiv, work, info);
-#else
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_dsytri(uplo, &n_64, a, &lda_64, ipiv, work, &info_64);
-
-    *info = (aocl_int_t)info_64;
+    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
+#if AOCL_DTL_LOG_ENABLE 
+    char buffer[256]; 
+    snprintf(buffer, 256,"dsytri inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS ", ipiv %" FLA_IS "",*uplo, *n, *lda, *ipiv);
+    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
-}
-
-void aocl_lapack_dsytri(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t *lda,
-                        aocl_int_t *ipiv, doublereal *work, aocl_int64_t *info)
-{
-    AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("dsytri inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS "", *uplo, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1;
     doublereal d__1;
@@ -196,15 +184,15 @@ void aocl_lapack_dsytri(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("DSYTRI", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("DSYTRI", &i__1);
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Quick return if possible */
     if(*n == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        return 0;
     }
     /* Check that the diagonal matrix D is nonsingular. */
     if(upper)
@@ -214,8 +202,8 @@ void aocl_lapack_dsytri(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t
         {
             if(ipiv[*info] > 0 && a[*info + *info * a_dim1] == 0.)
             {
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L10: */
         }
@@ -228,8 +216,8 @@ void aocl_lapack_dsytri(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t
         {
             if(ipiv[*info] > 0 && a[*info + *info * a_dim1] == 0.)
             {
-                AOCL_DTL_TRACE_LOG_EXIT
-                return;
+                AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+                return 0;
             }
             /* L20: */
         }
@@ -419,8 +407,8 @@ void aocl_lapack_dsytri(char *uplo, aocl_int64_t *n, doublereal *a, aocl_int64_t
         goto L50;
     L60:;
     }
-    AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    return 0;
     /* End of DSYTRI */
 }
 /* dsytri_ */

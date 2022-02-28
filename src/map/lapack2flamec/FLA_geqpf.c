e6751eac0abcc8c3553ef4c70eec07f87a9af09f
prangana <pradeep.rao@amd.com>
2022-02-28 03:09:20 -0500

    Add Netlib LAPACK based implementation for real GEQP3 API
    Netlib LAPACK based implementation added or real GEQP3 API.
    This is used when FLA_AMD_OPT macro is enabled.
    
    Change-Id: Id2b56044b6fef73b95d07b87ac985087f57baf7a

--- src/map/lapack2flamec/FLA_geqpf.c
+++ src/map/lapack2flamec/FLA_geqpf.c
@@ -278,7 +278,7 @@ LAPACK_geqpf_complex(z)
                                integer* info )
 LAPACK_geqp3(s)
 {
-  
+#if !FLA_AMD_OPT
     {
         LAPACK_geqpf_body(s) fla_error = 0;
     }
@@ -295,12 +295,23 @@ LAPACK_geqp3(s)
     {
         LAPACK_geqpf_body(s)
     }
+#else
+    {
+        sgeqp3_fla( m, n,
+                    buff_A, ldim_A,
+                    buff_p,
+                    buff_t,
+                    buff_w, lwork,
+                    info ) ;
+	return 0;
+    }
 #endif
 }
 
 LAPACK_geqp3(d)
 {
-   {
+#if !FLA_AMD_OPT
+    {
         LAPACK_RETURN_CHECK( dgeqp3_check( m, n,
                                            buff_A, ldim_A,
                                            buff_p,
@@ -318,9 +329,18 @@ LAPACK_geqp3(d)
             return 0;
         }
     }
-
     {
         LAPACK_geqpf_body(d)
+    }
+#else
+    {
+        dgeqp3_fla( m, n,
+                    buff_A, ldim_A,
+                    buff_p,
+                    buff_t,
+                    buff_w, lwork,
+                    info ) ;
+	return 0;
     }
 #endif
 }

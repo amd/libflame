af2439bb65059b1b543bcc90325fbe1fe95ba632
bsinghpa <brijesh.singhpanwar@amd.com>
2022-04-29 12:19:10 +0530

    Logging Time for FLA_ file (Bug Fix)
    Bug mix- missed fla_error variable
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: Ic22a0a8c1f9f2694f6d5eadfad7e8520e436d796

--- src/map/lapack2flamec/FLA_potrf.c
+++ src/map/lapack2flamec/FLA_potrf.c
@@ -124,7 +124,9 @@ LAPACK_potrf(d)
     AOCL_DTL_TRACE_LOG_INIT
     AOCL_DTL_SNPRINTF("dpotrf inputs: uplo %c, n %" FLA_IS ", lda %" FLA_IS "", *uplo, *n, *ldim_A);
     {
-        LAPACK_RETURN_CHECK_VAR1(dpotrf_check(uplo, n, buff_A, ldim_A, info), fla_error)
+        LAPACK_RETURN_CHECK_VAR1( dpotrf_check( uplo, n,
+                                           buff_A, ldim_A,
+                                           info ),fla_error )
     }
     if (fla_error == LAPACK_SUCCESS)
     {

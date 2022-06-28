b40d2302f6123c9e6240f17750be7333675b2cd8
brijesh <brijesh.singhpanwar@amd.com>
2022-06-28 09:09:48 +0530

    Adding DTL to non FLA files(double precision )
    Adding Log and trace to double precision files (files starting with d)
    
    Functions:
    dlapy3,dlapy2,dlapmt,dlapmr,
    dlapll,dlaorhr_col_getrfnp2,
    dlaorhr_col_getrfnp,dlanv2,dlantr,
    dlantp,dlantb,dlansy,dlanst,dlansp,
    dlansf,dlansb,dlanhs,dlangt,dlange,dlangb.c 
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: Ibd56082dbb30c1e2a411beda591639c6ecf9ba9f

--- src/map/lapack2flamec/f2c/c/dlapmt.c
+++ src/map/lapack2flamec/f2c/c/dlapmt.c
@@ -103,12 +103,8 @@
 void dlapmt_(logical *forwrd, aocl_int_t *m, aocl_int_t *n, doublereal *x, aocl_int_t *ldx,
              aocl_int_t *k)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"dlapmt inputs: m %" FLA_IS ", n %" FLA_IS ", ldx %" FLA_IS ", k %" FLA_IS "",*m, *n, *ldx, *k);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("dlapmt inputs: forward %" FLA_IS ", m %" FLA_IS ", n %" FLA_IS ", ldx %" FLA_IS ", k %" FLA_IS "", *forwrd, * m, *n, *ldx, *k);
     /* System generated locals */
     aocl_int64_t x_dim1, x_offset, i__1, i__2;
     /* Local variables */
@@ -134,7 +130,7 @@ void dlapmt_(logical *forwrd, aocl_int_t *m, aocl_int_t *n, doublereal *x, aocl_
     /* Function Body */
     if(*n <= 1)
     {
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     i__1 = *n;
@@ -209,7 +205,7 @@ void dlapmt_(logical *forwrd, aocl_int_t *m, aocl_int_t *n, doublereal *x, aocl_
             ;
         }
     }
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
     /* End of DLAPMT */
 }

0c9cff3f4f557f6c308b9617dda0b25b12db5752
brijesh <brijesh.singhpanwar@amd.com>
2022-06-28 09:34:41 +0530

    Adding DTL to non FLA files(double precision )
    Adding Log and trace to double precision files (files starting with d)
    
    Functions: dsytri_3, dtgexc, dtgex2, dtgevc,
           dtfttr, dtfttp, dtftri, dtfsm, dtbtrs,
           dtbrfs, dtbcon, dsytrs2, dsytrs_rook,
           dsytrs_aa_2stage, dsytrs_aa, dsytrs_3, dsytrs,
           dsytri2x, dsytri_rook, dsytri_3x
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: I0a1ddff23880c6646f74f9cd6a75bbc236cdfc57

--- src/map/lapack2flamec/f2c/c/dsytrs_rook.c
+++ src/map/lapack2flamec/f2c/c/dsytrs_rook.c
@@ -138,12 +138,8 @@ static doublereal c_b19 = 1.;
 void dsytrs_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, doublereal *a, aocl_int_t *lda,
                   aocl_int_t *ipiv, doublereal *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE 
-    char buffer[256]; 
-    snprintf(buffer, 256,"dsytrs_rook inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ipiv %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *lda, *ipiv, *ldb);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("dsytrs_rook inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ipiv %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *lda, *ipiv, *ldb);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, b_dim1, b_offset, i__1;
     doublereal d__1;
@@ -211,13 +207,13 @@ void dsytrs_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, doublereal *a, ao
     {
         i__1 = -(*info);
         xerbla_("DSYTRS_ROOK", &i__1);
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     /* Quick return if possible */
     if(*n == 0 || *nrhs == 0)
     {
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     if(upper)
@@ -486,7 +482,7 @@ void dsytrs_rook_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, doublereal *a, ao
         goto L90;
     L100:;
     }
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
     /* End of DSYTRS_ROOK */
 }

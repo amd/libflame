a486413bba25ace46c322e8343f09b4336ef7d44
brijesh <brijesh.singhpanwar@amd.com>
2022-06-28 09:09:48 +0530

    Adding DTL to non FLA files(double precision )
    Adding Log and trace to double precision files (files starting with d)
    
    Functions:
    dlapy3,dlapy2,dlapmt,dlapmr,
    dlapll,dlaorhr_col_getrfnp2,
    dlaorhr_col_getrfnp,dlanv2,dlantr,
    dlantp,dlantb,dlansy,dlanst,dlansp,
    dlansf,dlansb,dlanhs,dlangt,dlange,dlangb.c 
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: Ibd56082dbb30c1e2a411beda591639c6ecf9ba9f

--- src/map/lapack2flamec/f2c/c/dlanst.c
+++ src/map/lapack2flamec/f2c/c/dlanst.c
@@ -100,12 +100,8 @@ static aocl_int64_t c__1 = 1;
 /** Generated wrapper function */
 doublereal dlanst_(char *norm, aocl_int_t *n, doublereal *d__, doublereal *e)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"dlanst inputs: norm %c, n %" FLA_IS "",*norm, *n);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("dlanst inputs: norm %c, n %" FLA_IS "",*norm, *n);
     /* System generated locals */
     aocl_int64_t i__1;
     doublereal ret_val, d__1, d__2, d__3;
@@ -208,7 +204,7 @@ doublereal dlanst_(char *norm, aocl_int_t *n, doublereal *d__, doublereal *e)
         anorm = scale * sqrt(sum);
     }
     ret_val = anorm;
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return ret_val;
     /* End of DLANST */
 }

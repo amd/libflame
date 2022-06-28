51edd0c1f5e045071ce9d11310efeab4df929a94
brijesh <brijesh.singhpanwar@amd.com>
2022-06-28 08:39:56 +0530

    Adding DTL to non FLA files(double precision )
    Adding Log and trace to double precision files (files starting with d)
    Functions: dlabrd, dlabad, dla_wwaddw, dla_syrpvgrw, 
    dla_syrfsx_extended, dla_syrcond,dla_syamv, dla_porpvgrw, 
    dla_porfsx_extended, dla_porcond, dla_lin_berr, dla_gerpvgrw, 
    dla_gerfsx_extended, dla_gercond,dla_geamv, dla_gbrpvgrw, 
    dla_gbrfsx_extended, dla_gbrcond,dla_gbamv, disnan
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: I08c604ae49437aa4c44c2024d64d5a6f5706c17f

--- src/map/lapack2flamec/f2c/c/dla_gbrfsx_extended.c
+++ src/map/lapack2flamec/f2c/c/dla_gbrfsx_extended.c
@@ -424,12 +424,8 @@ void dla_gbrfsx_extended_(integer *prec_type__, integer *trans_type__, integer *
                           doublereal *rthresh, doublereal *dz_ub__, logical *ignore_cwise__,
                           integer *info)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"dla_gbrfsx_extended inputs: prec_type__ %" FLA_IS ", trans_type__ %" FLA_IS ", n %" FLA_IS ", kl %" FLA_IS ", ku %" FLA_IS ", nrhs %" FLA_IS ", ldab %" FLA_IS ", ldafb %" FLA_IS ", ipiv %" FLA_IS ", ldb %" FLA_IS ", ldy %" FLA_IS ", n_norms__ %" FLA_IS ", ithresh %" FLA_IS "",*prec_type__, *trans_type__, *n, *kl, *ku, *nrhs, *ldab, *ldafb, *ipiv, *ldb, *ldy, *n_norms__, *ithresh);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("dla_gbrfsx_extended inputs: prec_type__ %" FLA_IS ", trans_type__ %" FLA_IS ", n %" FLA_IS ", kl %" FLA_IS ", ku %" FLA_IS ", nrhs %" FLA_IS ", ldab %" FLA_IS ", ldafb %" FLA_IS ", ipiv %" FLA_IS ", ldb %" FLA_IS ", ldy %" FLA_IS ", n_norms__ %" FLA_IS ", ithresh %" FLA_IS "",*prec_type__, *trans_type__, *n, *kl, *ku, *nrhs, *ldab, *ldafb, *ipiv, *ldb, *ldy, *n_norms__, *ithresh);
     /* System generated locals */
     integer ab_dim1, ab_offset, afb_dim1, afb_offset, b_dim1, b_offset, y_dim1, y_offset,
         err_bnds_norm_dim1, err_bnds_norm_offset, err_bnds_comp_dim1, err_bnds_comp_offset, i__1,
@@ -530,7 +526,7 @@ void dla_gbrfsx_extended_(integer *prec_type__, integer *trans_type__, integer *
     /* Function Body */
     if(*info != 0)
     {
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     chla_transtype_(ch__1, trans_type__);
@@ -806,7 +802,7 @@ void dla_gbrfsx_extended_(integer *prec_type__, integer *trans_type__, integer *
         dla_lin_berr_(n, n, &c__1, &res[1], &ayb[1], &berr_out__[j]);
         /* End of loop for each RHS */
     }
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
 }
 /* dla_gbrfsx_extended__ */

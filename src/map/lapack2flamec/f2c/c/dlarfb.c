e8bc81d4826e7da3d6c0edfacf5f3cd92104e877
brijesh <brijesh.singhpanwar@amd.com>
2022-06-28 09:19:27 +0530

    Adding DTL to non FLA files(double precision )
    Adding Log and trace to double precision files (files starting with d)
    
    Functions: dlar2v,dlarrr,dlarrk,dlarrj,dlarrf,
    dlarre,dlarrd,dlarrc,dlarrb,dlarra,
    dlarnv,dlargv,dlarfy,dlarfx,dlarft
    dlarfgp,dlarfg,dlarfb_gett,dlarfb,dlarf
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2015
    Change-Id: Ib3e77b36760457e87b1a787024c5fbaa87f3a51b

--- src/map/lapack2flamec/f2c/c/dlarfb.c
+++ src/map/lapack2flamec/f2c/c/dlarfb.c
@@ -202,12 +202,8 @@ void dlarfb_(char *side, char *trans, char *direct, char *storev, aocl_int_t *m,
              aocl_int_t *k, doublereal *v, aocl_int_t *ldv, doublereal *t, aocl_int_t *ldt,
              doublereal *c__, aocl_int_t *ldc, doublereal *work, aocl_int_t *ldwork)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"dlarfb inputs: side %c, trans %c, direct %c, storev %c, m %" FLA_IS ", n %" FLA_IS ", k %" FLA_IS ", ldv %" FLA_IS ", ldt %" FLA_IS ", ldc %" FLA_IS ", ldwork %" FLA_IS "",*side, *trans, *direct, *storev, *m, *n, *k, *ldv, *ldt, *ldc, *ldwork);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("dlarfb inputs: side %c, trans %c, direct %c, storev %c, m %" FLA_IS ", n %" FLA_IS ", k %" FLA_IS ", ldv %" FLA_IS ", ldt %" FLA_IS ", ldc %" FLA_IS ", ldwork %" FLA_IS "",*side, *trans, *direct, *storev, *m, *n, *k, *ldv, *ldt, *ldc, *ldwork);
     /* System generated locals */
     aocl_int64_t c_dim1, c_offset, t_dim1, t_offset, v_dim1, v_offset, work_dim1, work_offset, i__1,
         i__2;
@@ -250,7 +246,7 @@ void dlarfb_(char *side, char *trans, char *direct, char *storev, aocl_int_t *m,
     /* Function Body */
     if(*m <= 0 || *n <= 0)
     {
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     if(lsame_(trans, "N", 1, 1))
@@ -694,7 +690,7 @@ void dlarfb_(char *side, char *trans, char *direct, char *storev, aocl_int_t *m,
             }
         }
     }
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
     /* End of DLARFB */
 }

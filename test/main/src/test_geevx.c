eef73349e70551616af8bca69fc84143c9bcb0d6
dsherene <Deborah.Sherene@amd.com>
2022-07-12 02:37:54 +0530

    Added validation code for geev and geevx API
    Note: validation of reciprocal condition number for eigen values
    and eigen vector will be added later
    
    Signed-off-by: dsherene<deborah.sherene@amd.com>
    Change-Id: I4c89b10146130fd675dff9797f9f745e0d681663

--- test/main/src/test_geevx.c
+++ test/main/src/test_geevx.c
@@ -7,26 +7,27 @@
 #include "test_common.h"
 #include "test_prototype.h"
 
-// Local prototypes.
+/* Local prototypes.*/
 void fla_test_geevx_experiment(test_params_t *params, integer datatype, integer p_cur, integer  q_cur, integer pci,
                                     integer n_repeats, double* perf, double* t, double* residual);
 void prepare_geevx_run(char *balanc, char *jobvl, char *jobvr, char * sense, integer n, void *a, void *wr, void *wi, void *w,
-                            void *vl, integer ldvl, void *vr, integer ldvr, integer *ilo, integer * ihi, void *scale, void *abnrm, void *rconde,
-                            void *rcondv, integer datatype, integer n_repeats, double* time_min_);
-void invoke_geevx(integer datatype, char *balanc, char *jobvl, char *jobvr, char * sense, integer *n, void *a, integer *lda, void *wr, void *wi, void *w,
-                            void *vl, integer *ldvl, void *vr, integer *ldvr, integer *ilo, integer *ihi, void *scale, void *abnrm, void *rconde, void *rcondv,
-                            void* work, integer* lwork, void* rwork, integer* iwork, integer* info);
+                       void *vl, integer ldvl, void *vr, integer ldvr, integer *ilo, integer * ihi, void *scale, void *abnrm,
+		       void *rconde, void *rcondv, integer datatype, integer n_repeats, double* time_min_);
+void invoke_geevx(integer datatype, char *balanc, char *jobvl, char *jobvr, char * sense, integer *n, void *a, integer *lda,
+                  void *wr, void *wi, void *w, void *vl, integer *ldvl, void *vr, integer *ldvr, integer *ilo, integer *ihi,
+                  void *scale, void *abnrm, void *rconde, void *rcondv, void* work, integer* lwork, void* rwork, integer* iwork,
+		  integer* info);
+
 
 void fla_test_geevx(test_params_t *params)
 {
-    char* op_str = "Eigen value and Eigen vectors";
+    char* op_str = "Eigen Decomposition of non symmetric matrix";
     char* front_str = "GEEVX";
 
     fla_test_output_info("--- %s ---\n", op_str);
     fla_test_output_info("\n");
-    fla_test_op_driver(front_str, SQUARE_INPUT, params, EIG_SYM, fla_test_geevx_experiment);
+    fla_test_op_driver(front_str, SQUARE_INPUT,  params, EIG_NSYM, fla_test_geevx_experiment);
 }
-
 
 void fla_test_geevx_experiment(test_params_t *params,
     integer  datatype,
@@ -35,7 +36,7 @@ void fla_test_geevx_experiment(test_params_t *params,
     integer pci,
     integer n_repeats,
     double* perf,
-    double *t,
+    double *time_min,
     double* residual)
 {
     integer m, n, cs_A, ldvl, ldvr;
@@ -43,24 +44,35 @@ void fla_test_geevx_experiment(test_params_t *params,
     void *A = NULL, *wr = NULL, *wi = NULL, *w = NULL, *VL = NULL, *VR = NULL;
     void *scale = NULL, *abnrm = NULL, *rconde = NULL, *rcondv = NULL;
     void *A_test = NULL;
-    double time_min = 1e9;
+    char balanc, jobvl, jobvr, sense;
 
-
-    // Get input matrix dimensions.
+    /* Get input matrix dimensions.*/
     m = p_cur;
     n = p_cur;
     cs_A = m;
     ldvl = m;
     ldvr = m;
-
-    // Create input matrix parameters
+    
+    *residual =  params->eig_non_sym_paramslist[pci].GenNonSymEigProblem_threshold;
+    balanc = params->eig_non_sym_paramslist[pci].balance_ggevx;
+    jobvl = params->eig_non_sym_paramslist[pci].jobvsl;
+    jobvr = params->eig_non_sym_paramslist[pci].jobvsr;
+    sense = params->eig_non_sym_paramslist[pci].sense_ggevx;
+    if(sense == 'B' || sense == 'E')
+    {
+        jobvl = 'V';
+	jobvr = 'V';
+    }	
+    /* Create input matrix parameters */
     create_matrix(datatype, &A, m, n);
+
     create_matrix(datatype, &VL, ldvl, m);
     create_matrix(datatype, &VR, ldvr, m);
     create_realtype_vector(datatype, &scale, m);
     create_realtype_vector(datatype, &abnrm, 1);
     create_realtype_vector(datatype, &rconde, m);
     create_realtype_vector(datatype, &rcondv, n);
+
     if(datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
     {
         create_vector(datatype, &w, m);
@@ -70,29 +82,32 @@ void fla_test_geevx_experiment(test_params_t *params,
         create_vector(datatype, &wr, m);
         create_vector(datatype, &wi, m);
     }
-
-
-    // Initialize input matrix A with random numbers
+    
+    /* Initialize input matrix A with random numbers */
     rand_matrix(datatype, A, m, n, cs_A);
 
-    // Make a copy of input matrix A. This is required to validate the API functionality.
+    /* Make a copy of input matrix A. This is required to validate the API functionality. */
     create_matrix(datatype, &A_test, m, n);
     copy_matrix(datatype, "full", m, n, A, cs_A, A_test, cs_A);
 
-    //**prepare_gesdd_run(VECTORS_ALL, m, n, A_test, s, U, V, datatype, n_repeats, &time_min);
-    prepare_geevx_run("B", "N", "N", "N", m, A_test, wr, wi, w,  VL, ldvl, VR, ldvr, &ilo, &ihi, scale, abnrm, rconde, rcondv, datatype, n_repeats, &time_min);
+    prepare_geevx_run(&balanc, &jobvl, &jobvr, &sense, m, A_test, wr, wi, w,  VL, ldvl, VR, ldvr,
+                      &ilo, &ihi, scale, abnrm, rconde , rcondv, datatype, n_repeats, time_min);
 
-    // execution time
-    *t = time_min;
+    /* performance computation
+       4/3 m^3 flops if job = 'N'
+       8/3 m^3 + m^2 flops if job = 'V' */
+    if(jobvl == 'N' && jobvr == 'N')
+        *perf = (double)((4.0 / 3.0) * m * m * m) / *time_min / FLOPS_PER_UNIT_PERF;
+    else
+        *perf = (double)(((8.0 / 3.0) * m * m * m) + (m * m)) / *time_min / FLOPS_PER_UNIT_PERF;
+    if(datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
+        *perf *= 4.0;
 
-    // Todo
-    // performance computation will be added later
-    *perf = 0;
+    /* output validation */
+    validate_geevx(&jobvl, &jobvr, &sense, &balanc, m, A, A_test, VL, VR, w, wr, wi, scale,
+                   abnrm, rconde, rcondv, datatype, residual);
 
-    // Todo
-    // output validation
-
-    // Free up the buffers
+    /* Free up the buffers */
     free_matrix(A);
     free_matrix(A_test);
     free_matrix(VL);
@@ -113,8 +128,6 @@ void fla_test_geevx_experiment(test_params_t *params,
 
 }
 
-
-
 void prepare_geevx_run(char *balanc, char *jobvl, char *jobvr, char * sense,
                         integer m_A, void *A,
                         void *wr, void *wi, void *w,
@@ -125,13 +138,10 @@ void prepare_geevx_run(char *balanc, char *jobvl, char *jobvr, char * sense,
 {
     integer cs_A;
     void *A_save = NULL, *rwork = NULL, *iwork = NULL, *work = NULL;
-    void *w_test = NULL, *wi_test = NULL, *wr_test = NULL, *scale_test = NULL, *abnrm_test = NULL;
-    //void *VL_test = NULL, *VR_test = NULL, *rconde_test = NULL, *rcondv_test = NULL;
     integer lwork, liwork, lrwork;
     integer i;
     integer info = 0;
     double time_min = 1e9, exe_time;
-
 
     cs_A = m_A;
 
@@ -144,22 +154,29 @@ void prepare_geevx_run(char *balanc, char *jobvl, char *jobvr, char * sense,
     lrwork = 2 * m_A;
     liwork = 2 * m_A - 2;
 
-    // Make a workspace query the first time through. This will provide us with
-    // and ideal workspace size based on an internal block size.
+    /* Make a workspace query the first time through. This will provide us with
+     and ideal workspace size based on an internal block size.*/
     lwork = -1;
     create_vector(datatype, &work, 1);
-
-    // call to  geevx API
+    if ( datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
+    {
+        create_realtype_vector(datatype, &rwork, lrwork);
+    }
+    /* call to  geevx API */
     invoke_geevx(datatype, balanc, jobvl, jobvr, sense, &m_A, NULL, &cs_A,
                     NULL, NULL, NULL, NULL, &ldvl, NULL, &ldvr,
-                    ilo, ihi, NULL, NULL, NULL, NULL, work, &lwork, NULL, NULL, &info);
+                    ilo, ihi, NULL, NULL, NULL, NULL, work, &lwork, rwork, NULL, &info);
 
-    // Get work size
+    /* Get work size */
     lwork = get_work_value( datatype, work );
 
-    /* Output buffers will be freshly allocated for each iterations, free up 
-       the current output buffers.*/ 
+    /* Output buffers will be freshly allocated for each iterations, free up
+       the current output buffers.*/
     free_vector(work);
+    if ( datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
+    {
+        free_vector(rwork);
+    }
 
     for (i = 0; i < n_repeats; ++i)
     {
@@ -167,70 +184,42 @@ void prepare_geevx_run(char *balanc, char *jobvl, char *jobvr, char * sense,
            for each iteration*/
         copy_matrix(datatype, "full", m_A, m_A, A_save, m_A, A, m_A);
 
-        //create_matrix(datatype, &VL_test, ldvl, m_A);
-        //create_matrix(datatype, &VR_test, ldvr, m_A);
-        create_realtype_vector(datatype, &scale_test, m_A);
-        create_realtype_vector(datatype, &abnrm_test, 1);
-        //create_realtype_vector(datatype, &rconde_test, m_A);
-        //create_realtype_vector(datatype, &rcondv_test, m_A);
         create_vector(datatype, &work, lwork);
         if(datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
         {
-            create_vector(datatype, &w_test, m_A);
             create_realtype_vector(datatype, &rwork, lrwork);
         }
         else
         {
-            create_vector(datatype, &wr_test, m_A);
-            create_vector(datatype, &wi_test, m_A);
             create_vector(INTEGER, &iwork, liwork);
         }
 
         exe_time = fla_test_clock();
 
-        // call to API
-        /*invoke_geevx(datatype, balanc, jobvl, jobvr, sense, &m_A, A, &cs_A,
-                    wr_test, wi_test, w_test, VL_test, &ldvl, VR_test, &ldvr,
-                    ilo, ihi, scale_test, abnrm_test, rconde_test, rcondv_test, work, &lwork, rwork, iwork, &info);*/
-        invoke_geevx(datatype, balanc, jobvl, jobvr, sense, &m_A, A, &cs_A,
-                    wr_test, wi_test, w_test, NULL, &ldvl, NULL, &ldvr,
-                    ilo, ihi, scale_test, abnrm_test, NULL, NULL, work, &lwork, rwork, iwork, &info);
-        
+        /* call to geevx API */
+        invoke_geevx(datatype, balanc, jobvl, jobvr, sense, &m_A, A, &cs_A, wr, wi, w, VL, &ldvl, VR, &ldvr,
+                     ilo, ihi, scale, abnrm, rconde, rcondv, work, &lwork, rwork, iwork, &info);
+ 
         exe_time = fla_test_clock() - exe_time;
 
-        // Get the best execution time
+        /* Get the best execution time */
         time_min = min(time_min, exe_time);
 
-        // Todo
-        // Make a copy of the output buffers. This is required to validate the API functionality.
-
-
-        // Free up the output buffers
+        /* Free up the output buffers */
         free_vector(work);
         if ( datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
         {
-            free_vector(w_test);
             free_vector(rwork);
         }
         else
         {
-            free_vector(wr_test);
-            free_vector(wi_test);
             free_vector(iwork);
         }
-        //free_matrix(VL_test);
-        //free_matrix(VR_test);
-        free_vector(scale_test);
-        free_vector(abnrm_test);
-        //free_vector(rconde_test);
-        //free_vector(rcondv_test);
     }
-
     *time_min_ = time_min;
 
     free(A_save);
 }
-
 
 void invoke_geevx(integer datatype, char *balanc, char *jobvl, char *jobvr, char *sense,
                             integer *n, void *a, integer *lda, void *wr, void *wi, void *w,

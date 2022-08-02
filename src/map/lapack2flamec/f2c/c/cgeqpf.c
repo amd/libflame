cc63cb1a835657d5d142e0f54c4a00be65d97936
bsinghpa <brijesh.singhpanwar@amd.com>
2022-08-02 03:09:52 -0400

    Test faliures for dgeqp3 and zgeqp3
    Added code for the control to return when lwork=-1
    Segmentation fault issue when dtl gets enabled
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2354
    Change-Id: Ib498df84426eb3e24bbb5dea94c625d0c314cba1

--- src/map/lapack2flamec/f2c/c/cgeqpf.c
+++ src/map/lapack2flamec/f2c/c/cgeqpf.c
@@ -154,11 +154,7 @@ void cgeqpf_(aocl_int_t *m, aocl_int_t *n, scomplex *a, aocl_int_t *lda, aocl_in
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
 #if AOCL_DTL_LOG_ENABLE
     char buffer[256];
-#if FLA_ENABLE_ILP64
-    snprintf(buffer, 256,"cgeqpf inputs: m %lld, n %lld, lda %lld, jpvt %lld",*m, *n, *lda, *jpvt);
-#else
-    snprintf(buffer, 256,"cgeqpf inputs: m %d, n %d, lda %d, jpvt %d",*m, *n, *lda, *jpvt);
-#endif
+    snprintf(buffer, 256,"cgeqpf inputs: m %" FLA_IS ", n %" FLA_IS ", lda %" FLA_IS "",*m, *n, *lda);
     AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
     /* System generated locals */

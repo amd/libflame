fbef6b15a6349d1b640da013c8fd6a389ba6ac96
srigovin <sridhar.govindaswamy@amd.com>
2022-08-03 08:59:01 +0530

    Optimization of DGESDD
    1. Added lapack implementation of dgesdd.
    2. Modifed FLA_gesdd.c to choose lapack_dgesdd() path, when FLA_AMD_OPT is enabled.
    3. Parallelized dormlq() and dormqr() APIs.
    
    Signed-off-by: Sridhar Govindaswamy <Sridhar.Govindaswamy@amd.com>
    AMD-Internal: CPUPL-2380
    Change-Id: I8a9a6e86dbceccc8d87522970a973e0ae4342df9

--- src/base/flamec/include/FLA_Threads.h
+++ src/base/flamec/include/FLA_Threads.h
@@ -1,14 +1,9 @@
 /*
-    Copyright (C) 2022-2026, Advanced Micro Devices, Inc. All rights reserved.
+    Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
 */
-
-#ifdef FLA_OPENMP_MULTITHREADING
 
 #ifndef API_ID_DEFINED
 #define API_ID_DEFINED
-
-#include "FLA_type_defs.h"
-
 /* API ID */
 typedef enum
 {
@@ -19,7 +14,5 @@ typedef enum
 #endif
 
 
-void FLA_Thread_get_subrange( int thread_ID, int num_threads, fla_dim_t range, fla_dim_t *sub_range, fla_dim_t *index );
+void FLA_Thread_get_subrange( int thread_ID, int num_threads, integer range, integer *sub_range, integer *index );
 void FLA_Thread_optimum( API_ID family, int *actual_num_threads);
-
-#endif
--- src/base/flamec/include/FLA_f2c.h
+++ src/base/flamec/include/FLA_f2c.h
@@ -25,6 +25,7 @@
 #include "aocldtl.h"
 #include "FLA_config.h"
 #include "FLA_progress.h"
+#include "FLA_Threads.h"
 
 #ifndef F2C_INCLUDE
 #define F2C_INCLUDE

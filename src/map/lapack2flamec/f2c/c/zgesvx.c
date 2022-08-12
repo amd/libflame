daa5a19ab5a80365538c1fd1f25b4b2eca94388d
bsinghpa <brijesh.singhpanwar@amd.com>
2022-08-11 22:15:55 -0400

    Adding DTL to z* APIs (double precision Complex)
    Adding Log and trace with timming to double complex precision APIs (files starting with z)
    
    Functions:
    zgerfsx,zgerq2,zgerqf,zgesc2,
    zgesdd,zgesv,zgesvd,zgesvdq,
    zgesvdx,zgesvj,zgesvx,zgesvxx,
    zgetc2,zgetrf2,zgetri,zgetrs,
    zgetsls,zgetsqrhrt,zggbak,zggbal
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2387
    Change-Id: I5f9d8d5aa364b2b103be3cf14ec072b3c5b64770

--- src/map/lapack2flamec/f2c/c/zgesvx.c
+++ src/map/lapack2flamec/f2c/c/zgesvx.c
@@ -357,12 +357,9 @@ void zgesvx_(char *fact, char *trans, aocl_int_t *n, aocl_int_t *nrhs, dcomplex 
              aocl_int_t *ldx, doublereal *rcond, doublereal *ferr, doublereal *berr,
              dcomplex *work, doublereal *rwork, aocl_int_t *info)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"zgesvx inputs: fact %c, trans %c, n %d, nrhs %d, lda %d, ldaf %d, ldb %d, ldx %d",*fact, *trans, *n, *nrhs, *lda, *ldaf, *ldb, *ldx);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("zgesvx inputs: fact %c, trans %c, n %" FLA_IS ", nrhs %" FLA_IS ", lda %" FLA_IS ", ldaf %" FLA_IS ", ldb %" FLA_IS ", ldx %" FLA_IS "",*fact, *trans, *n, *nrhs, *lda, *ldaf, *ldb, *ldx);
+
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, af_dim1, af_offset, b_dim1, b_offset, x_dim1, x_offset, i__1,
         i__2, i__3, i__4, i__5;
@@ -557,7 +554,7 @@ void zgesvx_(char *fact, char *trans, aocl_int_t *n, aocl_int_t *nrhs, dcomplex 
     {
         i__1 = -(*info);
         xerbla_("ZGESVX", &i__1);
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     if(equil)
@@ -639,7 +636,7 @@ void zgesvx_(char *fact, char *trans, aocl_int_t *n, aocl_int_t *nrhs, dcomplex 
             }
             rwork[1] = rpvgrw;
             *rcond = 0.;
-            AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+            AOCL_DTL_TRACE_LOG_EXIT
             return 0;
         }
     }
@@ -736,7 +733,7 @@ void zgesvx_(char *fact, char *trans, aocl_int_t *n, aocl_int_t *nrhs, dcomplex 
         *info = *n + 1;
     }
     rwork[1] = rpvgrw;
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
     /* End of ZGESVX */
 }

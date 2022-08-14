a198a80a17cbb30fafcbeaf83f0e2083f1e95a6b
Parag Sharma <parsharm@amd.com>
2022-08-14 11:28:59 +0530

    Aocl_progress implementation for SGBTRF,DGBTRF,CGBTRF and ZGBTRF API
    Signed-off-by: Parag <parsharm@amd.com>
    AMD-Internal: [CPUPL-2211]
    Change-Id: I16b3ae34b3f9d47306df68fea0dfcd4fb28634bb

--- src/map/lapack2flamec/f2c/c/zgbtf2.c
+++ src/map/lapack2flamec/f2c/c/zgbtf2.c
@@ -191,9 +191,9 @@ void zgbtf2_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, dcomp
     ab_offset = 1 + ab_dim1;
     ab -= ab_offset;
     --ipiv;
-#if AOCL_FLA_PROGRESS_H
-    AOCL_FLA_PROGRESS_VAR;
-#endif
+    #if AOCL_FLA_PROGRESS_H
+        AOCL_FLA_PROGRESS_VAR;
+    #endif
 
     /* Function Body */
     kv = *ku + *kl;
@@ -232,13 +232,13 @@ void zgbtf2_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, dcomp
         AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
         return 0;
     }
-#if AOCL_FLA_PROGRESS_H
-    progress_step_count = 0;
-#ifndef FLA_ENABLE_WINDOWS_BUILD
-    if(!aocl_fla_progress_ptr)
-        aocl_fla_progress_ptr = aocl_fla_progress;
-#endif
-#endif
+    #if AOCL_FLA_PROGRESS_H
+        step_count =0;
+         #ifndef FLA_ENABLE_WINDOWS_BUILD
+                if(!aocl_fla_progress_ptr)
+                    aocl_fla_progress_ptr=aocl_fla_progress;
+         #endif
+    #endif
 
     /* Gaussian elimination with partial pivoting */
     /* Set fill-in elements in columns KU+2 to KV to zero. */
@@ -261,17 +261,14 @@ void zgbtf2_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, dcomp
     i__1 = fla_min(*m, *n);
     for(j = 1; j <= i__1; ++j)
     {
-#if AOCL_FLA_PROGRESS_H
-        if(aocl_fla_progress_ptr)
-        {
-            if(j % 32 == 0 || j == i__1)
-            {
-                progress_step_count = j;
-                AOCL_FLA_PROGRESS_FUNC_PTR("ZGBTF2", 6, &progress_step_count, &progress_thread_id,
-                                           &progress_total_threads);
+	#if AOCL_FLA_PROGRESS_H
+            if(aocl_fla_progress_ptr){
+                if(j%32==0 || j==i__1){
+                        step_count=j;
+                        AOCL_FLA_PROGRESS_FUNC_PTR("ZGBTF2",6,&step_count,&thread_id,&total_threads);
+                }
             }
-        }
-#endif
+        #endif
 
         /* Set fill-in elements in column J+KV to zero. */
         if(j + kv <= *n)

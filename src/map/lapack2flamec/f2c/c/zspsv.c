25df330a2ca715ab85693181a2969f7b052573ad
bsinghpa <brijesh.singhpanwar@amd.com>
2022-08-22 06:25:11 -0400

    Adding DTL to z* APIs (double precision Complex)
    Adding Log and trace with timing to double complex precision APIs (files starting with z)
    
    Functions:
    zptsvx,zpttrf,zpttrs,zptts2,
    zspcon,zspmv,zspr,zsprfs,zspsv,
    zspsvx,zsptrf,zsptri,zsptrs,zstedc,
    zstegr,zstein,zstemr,zsteqr,zsycon
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2422
    Change-Id: Id9e58e2b3638bee9139a68638a917e96a95b4784

--- src/map/lapack2flamec/f2c/c/zspsv.c
+++ src/map/lapack2flamec/f2c/c/zspsv.c
@@ -163,12 +163,9 @@
 void zspsv_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *ap, aocl_int_t *ipiv,
             dcomplex *b, aocl_int_t *ldb, aocl_int_t *info)
 {
-    AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
-#if AOCL_DTL_LOG_ENABLE
-    char buffer[256];
-    snprintf(buffer, 256,"zspsv inputs: uplo %c, n %d, nrhs %d, ldb %d",*uplo, *n, *nrhs, *ldb);
-    AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
-#endif
+    AOCL_DTL_TRACE_LOG_INIT
+    AOCL_DTL_SNPRINTF("zspsv inputs: uplo %c, n %" FLA_IS ", nrhs %" FLA_IS ", ldb %" FLA_IS "",*uplo, *n, *nrhs, *ldb);
+
     /* System generated locals */
     aocl_int64_t b_dim1, b_offset, i__1;
     /* Local variables */
@@ -218,7 +215,7 @@ void zspsv_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *ap, aocl_int_
     {
         i__1 = -(*info);
         xerbla_("ZSPSV ", &i__1);
-        AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+        AOCL_DTL_TRACE_LOG_EXIT
         return 0;
     }
     /* Compute the factorization A = U*D*U**T or A = L*D*L**T. */
@@ -228,7 +225,7 @@ void zspsv_(char *uplo, aocl_int_t *n, aocl_int_t *nrhs, dcomplex *ap, aocl_int_
         /* Solve the system A*X = B, overwriting B with X. */
         aocl_lapack_zsptrs(uplo, n, nrhs, &ap[1], &ipiv[1], &b[b_offset], ldb, info);
     }
-    AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
+    AOCL_DTL_TRACE_LOG_EXIT
     return 0;
     /* End of ZSPSV */
 }

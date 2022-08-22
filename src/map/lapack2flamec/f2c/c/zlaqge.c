8f260ce4386ad3cdf0384ad5f939564996c1b812
bsinghpa <brijesh.singhpanwar@amd.com>
2022-08-22 06:11:17 -0400

    Adding DTL to z* APIs (double precision Complex.)
    Adding Log and trace with timing to double complex precision APIs (files starting with z)
    
    Functions:
    zlapmr,zlapmt,zlaqgb,zlaqge,zlaqhb,
    zlaqhe,zlaqhp,zlaqp2,zlaqps,zlaqr0,
    zlaqr1,zlaqr2,zlaqr3,zlaqr4,zlaqr5,
    zlaqsb,zlaqsp,zlaqsy,zlaqz0,zlaqz1,
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2422
    Change-Id: I3c97cc1994d42ba371a677190896d07e74e45f02

--- src/map/lapack2flamec/f2c/c/zlaqge.c
+++ src/map/lapack2flamec/f2c/c/zlaqge.c
@@ -140,23 +140,8 @@
 void zlaqge_(aocl_int_t *m, aocl_int_t *n, dcomplex *a, aocl_int_t *lda, doublereal *r__,
              doublereal *c__, doublereal *rowcnd, doublereal *colcnd, doublereal *amax, char *equed)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zlaqge(m, n, a, lda, r__, c__, rowcnd, colcnd, amax, equed);
-#else
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t lda_64 = *lda;
-
-    aocl_lapack_zlaqge(&m_64, &n_64, a, &lda_64, r__, c__, rowcnd, colcnd, amax, equed);
-#endif
-}
-
-void aocl_lapack_zlaqge(aocl_int64_t *m, aocl_int64_t *n, dcomplex *a, aocl_int64_t *lda,
-                        doublereal *r__, doublereal *c__, doublereal *rowcnd, doublereal *colcnd,
-                        doublereal *amax, char *equed)
-{
     AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zlaqge inputs: m %" FLA_IS ", n %" FLA_IS ", lda %" FLA_IS "", *m, *n, *lda);
+    AOCL_DTL_SNPRINTF("zlaqge inputs: m %" FLA_IS ", n %" FLA_IS ", lda %" FLA_IS "",*m, *n, *lda);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, i__1, i__2, i__3, i__4, i__5;
     doublereal d__1;
@@ -192,8 +177,8 @@ void aocl_lapack_zlaqge(aocl_int64_t *m, aocl_int64_t *n, dcomplex *a, aocl_int6
     if(*m <= 0 || *n <= 0)
     {
         *(unsigned char *)equed = 'N';
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+    AOCL_DTL_TRACE_LOG_EXIT
+        return 0;
     }
     /* Initialize LARGE and SMALL. */
     small_val = dlamch_("Safe minimum") / dlamch_("Precision");
@@ -275,7 +260,7 @@ void aocl_lapack_zlaqge(aocl_int64_t *m, aocl_int64_t *n, dcomplex *a, aocl_int6
         *(unsigned char *)equed = 'B';
     }
     AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    return 0;
     /* End of ZLAQGE */
 }
 /* zlaqge_ */

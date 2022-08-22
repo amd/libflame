a77816a4c668f414f5b26f168ede1342317451c4
bsinghpa <brijesh.singhpanwar@amd.com>
2022-08-22 06:35:27 -0400

    Adding DTL to z* APIs (double precision Complex.)
    Adding Log and trace with timing to double complex precision APIs (files starting with z)
    
    Functions:
    zungtsqr_row,zunhr_col,zunm22,zunm2l,zunm2r,zunmbr,
    zunmhr,zunml2,zunmlq,zunmql,zunmqr,zunmr2,zunmr3,
    zunmrq,zunmrz,zunmtr,zupgtr,zupmtr
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2422
    Change-Id: I6810e898ac08432b5c1f40cfbb4dd43eff4e60db

--- src/map/lapack2flamec/f2c/c/zunmr2.c
+++ src/map/lapack2flamec/f2c/c/zunmr2.c
@@ -159,31 +159,8 @@ void zunmr2_(char *side, char *trans, aocl_int_t *m, aocl_int_t *n, aocl_int_t *
              aocl_int_t *lda, dcomplex *tau, dcomplex *c__, aocl_int_t *ldc,
              dcomplex *work, aocl_int_t *info)
 {
-#if FLA_ENABLE_ILP64
-    aocl_lapack_zunmr2(side, trans, m, n, k, a, lda, tau, c__, ldc, work, info);
-#else
-    aocl_int64_t m_64 = *m;
-    aocl_int64_t n_64 = *n;
-    aocl_int64_t k_64 = *k;
-    aocl_int64_t lda_64 = *lda;
-    aocl_int64_t ldc_64 = *ldc;
-    aocl_int64_t info_64 = *info;
-
-    aocl_lapack_zunmr2(side, trans, &m_64, &n_64, &k_64, a, &lda_64, tau, c__, &ldc_64, work,
-                       &info_64);
-
-    *info = (aocl_int_t)info_64;
-#endif
-}
-
-void aocl_lapack_zunmr2(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k,
-                        dcomplex *a, aocl_int64_t *lda, dcomplex *tau, dcomplex *c__,
-                        aocl_int64_t *ldc, dcomplex *work, aocl_int64_t *info)
-{
     AOCL_DTL_TRACE_LOG_INIT
-    AOCL_DTL_SNPRINTF("zunmr2 inputs: side %c, trans %c, m %" FLA_IS ", n %" FLA_IS ", k %" FLA_IS
-                      ", lda %" FLA_IS ", ldc %" FLA_IS "",
-                      *side, *trans, *m, *n, *k, *lda, *ldc);
+    AOCL_DTL_SNPRINTF("zunmr2 inputs: side %c, trans %c, m %" FLA_IS ", n %" FLA_IS ", k %" FLA_IS ", lda %" FLA_IS ", ldc %" FLA_IS "",*side, *trans, *m, *n, *k, *lda, *ldc);
     /* System generated locals */
     aocl_int64_t a_dim1, a_offset, c_dim1, c_offset, i__1, i__2, i__3;
     dcomplex z__1;
@@ -270,15 +247,15 @@ void aocl_lapack_zunmr2(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *
     if(*info != 0)
     {
         i__1 = -(*info);
-        aocl_blas_xerbla("ZUNMR2", &i__1, (ftnlen)6);
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+        xerbla_("ZUNMR2", &i__1);
+    AOCL_DTL_TRACE_LOG_EXIT
+        return 0;
     }
     /* Quick return if possible */
     if(*m == 0 || *n == 0 || *k == 0)
     {
-        AOCL_DTL_TRACE_LOG_EXIT
-        return;
+    AOCL_DTL_TRACE_LOG_EXIT
+        return 0;
     }
     if(left && !notran || !left && notran)
     {
@@ -345,7 +322,7 @@ void aocl_lapack_zunmr2(char *side, char *trans, aocl_int64_t *m, aocl_int64_t *
         /* L10: */
     }
     AOCL_DTL_TRACE_LOG_EXIT
-    return;
+    return 0;
     /* End of ZUNMR2 */
 }
 /* zunmr2_ */

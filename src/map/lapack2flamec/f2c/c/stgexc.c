7ae6abc19fe3e880c1175f6132c0eb9fa7d36b77
bsinghpa <brijesh.singhpanwar@amd.com>
2022-09-01 14:59:36 -0400

    Removing %% from logs
    Removing %% from logs which was causing issue in printng variable valuse
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2422
    Change-Id: Id93c9d868e135cabf9c3354c3aed3a80a5fdb47e

--- src/map/lapack2flamec/f2c/c/stgexc.c
+++ src/map/lapack2flamec/f2c/c/stgexc.c
@@ -229,7 +229,7 @@ void stgexc_(logical *wantq, logical *wantz, aocl_int_t *n, real *a, aocl_int_t 
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
 #if AOCL_DTL_LOG_ENABLE
     char buffer[256];
-    snprintf(buffer, 256,"stgexc inputs: n %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS ", ldq %%" FLA_IS ", ldz %" FLA_IS ", ifst %" FLA_IS ", ilst %" FLA_IS "",*n, *lda, *ldb, *ldq, *ldz, *ifst, *ilst);
+    snprintf(buffer, 256,"stgexc inputs: n %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS ", ldq %" FLA_IS ", ldz %" FLA_IS ", ifst %" FLA_IS ", ilst %" FLA_IS "",*n, *lda, *ldb, *ldq, *ldz, *ifst, *ilst);
     AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
     /* System generated locals */
--- src/map/lapack2flamec/f2c/c/zlacpy.c
+++ src/map/lapack2flamec/f2c/c/zlacpy.c
@@ -106,7 +106,7 @@ void zlacpy_(char *uplo, aocl_int_t *m, aocl_int_t *n, dcomplex *a, aocl_int_t *
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
 #if AOCL_DTL_LOG_ENABLE
     char buffer[256];
-    snprintf(buffer, 256,"zlacpy inputs: uplo %c, m %" FLA_IS ", n %" FLA_IS ", lda %%" FLA_IS ", ldb %" FLA_IS "",*uplo, *m, *n, *lda, *ldb);
+    snprintf(buffer, 256,"zlacpy inputs: uplo %c, m %" FLA_IS ", n %" FLA_IS ", lda %" FLA_IS ", ldb %" FLA_IS "",*uplo, *m, *n, *lda, *ldb);
     AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
     /* System generated locals */

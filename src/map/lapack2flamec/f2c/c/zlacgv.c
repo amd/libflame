8020ae5c82d813e27bfd82bfe213a9a97b20fb78
bsinghpa <brijesh.singhpanwar@amd.com>
2022-09-01 14:59:36 -0400

    Removing %% from logs
    Removing %% from logs which was causing issue in printng variable valuse
    
    Signed-off-by: bsinghpa<brijesh.singhpanwar@amd.com>
    AMD-Internal: CPUPL-2422
    Change-Id: Id93c9d868e135cabf9c3354c3aed3a80a5fdb47e

--- src/map/lapack2flamec/f2c/c/zlacgv.c
+++ src/map/lapack2flamec/f2c/c/zlacgv.c
@@ -75,7 +75,7 @@ void zlacgv_(aocl_int_t *n, dcomplex *x, aocl_int_t *incx)
     AOCL_DTL_TRACE_ENTRY(AOCL_DTL_LEVEL_TRACE_5);
 #if AOCL_DTL_LOG_ENABLE
     char buffer[256];
-    snprintf(buffer, 256,"zlacgv inputs: n %" FLA_IS ", incx %%" FLA_IS "",*n, *incx);
+    snprintf(buffer, 256,"zlacgv inputs: n %" FLA_IS ", incx %" FLA_IS "",*n, *incx);
     AOCL_DTL_LOG(AOCL_DTL_LEVEL_TRACE_5, buffer);
 #endif
     /* System generated locals */

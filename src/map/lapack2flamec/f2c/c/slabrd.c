d1a115e53fa67f0430cae599a3c187354e0c344b
prangana <pradeep.rao@amd.com>
2023-01-03 11:28:29 +0530

    libflame Framework upgrades to support context passing in all APIs
    - Context structure that includes global context information
    - Target CPU ISA and number of threads info included in context
    - aocl_fla_init() initialization call invoked within selected libflame
      APIs. More to be added in future commits
    
    Also, fixed few warnings in zgetrf files
    
    AMD-Internal: CPUPL-2881
    Change-Id: I5d622d8064bfdade650dcc058d0a280f7c5037ed

--- src/map/lapack2flamec/f2c/c/slabrd.c
+++ src/map/lapack2flamec/f2c/c/slabrd.c
@@ -1,8 +1,5 @@
-/* ../netlib/slabrd.f -- translated by f2c (version 20100827). You must link the resulting object
- file with libf2c: on Microsoft Windows system, link with libf2c.lib;
- on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a
- standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c
- -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+/* ../netlib/slabrd.f -- translated by f2c (version 20100827). You must link the resulting object file with libf2c: on Microsoft Windows system, link with libf2c.lib;
+ on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
 /*
  *  Copyright (c) 2022-2023 Advanced Micro Devices, Inc.  All rights reserved.
  */
@@ -258,6 +255,13 @@ void aocl_lapack_slabrd(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *nb, real
 void fla_slabrd(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *nb, real *a, aocl_int64_t *lda,
                 real *d__, real *e, real *tauq, real *taup, real *x, aocl_int64_t *ldx, real *y,
                 aocl_int64_t *ldy)
+{
+    extern int fla_slabrd(integer *m, integer *n, integer *nb, real *a, integer *lda, real *d__, real *e, real *tauq, real *taup, real *x, integer *ldx, real *y, integer *ldy);
+
+    return fla_slabrd(m, n, nb, a, lda, d__, e, tauq, taup, x, ldx, y, ldy);
+}
+
+int fla_slabrd(integer *m, integer *n, integer *nb, real *a, integer *lda, real *d__, real *e, real *tauq, real *taup, real *x, integer *ldx, real *y, integer *ldy)
 {
     /* System generated locals */
     integer a_dim1, a_offset, x_dim1, x_offset, y_dim1, y_offset, i__1, i__2, i__3, i__4, i__5;

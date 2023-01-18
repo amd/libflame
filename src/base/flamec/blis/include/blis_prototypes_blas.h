6db541a3519f036a71e40cd19a70d3112b025209
prangana <pradeep.rao@amd.com>
2023-01-18 04:41:04 -0500

    Header file modifications
    FLAME.h and other header files that it depends on, have declarations
    and definitions that are conflicting with BLIS header file, blis.h.
    This creates issues when an application includes both FLAME.h and
    blis.h. This commit modifies and cleanup of the existing header files
    to address this issue.
    
    AMD-Internal: CPUPL-2898
    Change-Id: I6ec8b1ed5c75ce4fd9f1955b3fc1d857201dac20

--- src/base/flamec/blis/include/blis_prototypes_blas.h
+++ src/base/flamec/blis/include/blis_prototypes_blas.h
@@ -127,113 +127,7 @@ extern "C" {
 #define F77_ctrsm  aocl_blas_ctrsm
 #define F77_ztrsm  aocl_blas_ztrsm  
 
-
-
 #ifdef BLIS1_FROM_LIBFLAME
-
-#define BL1_EXPORT_isamax F77_FUNC( isamax,  ISAMAX )
-#define BL1_EXPORT_idamax F77_FUNC( idamax,  IDAMAX )
-#define BL1_EXPORT_icamax F77_FUNC( icamax,  ICAMAX )
-#define BL1_EXPORT_izamax F77_FUNC( izamax,  IZAMAX )
-#define BL1_EXPORT_sasum  F77_FUNC( sasum,  SASUM )
-#define BL1_EXPORT_dasum  F77_FUNC( dasum,  DASUM )
-#define BL1_EXPORT_scasum F77_FUNC( scasum,  SCASUM )
-#define BL1_EXPORT_dzasum F77_FUNC( dzasum,  DZASUM )
-#define BL1_EXPORT_saxpy  F77_FUNC( saxpy,  SAXPY )
-#define BL1_EXPORT_daxpy  F77_FUNC( daxpy,  DAXPY )
-#define BL1_EXPORT_caxpy  F77_FUNC( caxpy,  CAXPY )
-#define BL1_EXPORT_zaxpy  F77_FUNC( zaxpy,  ZAXPY )
-#define BL1_EXPORT_scopy  F77_FUNC( scopy,  SCOPY )
-#define BL1_EXPORT_dcopy  F77_FUNC( dcopy,  DCOPY )
-#define BL1_EXPORT_ccopy  F77_FUNC( ccopy,  CCOPY )
-#define BL1_EXPORT_zcopy  F77_FUNC( zcopy,  ZCOPY )
-#define BL1_EXPORT_sdot   F77_FUNC( sdot,  SDOT )
-#define BL1_EXPORT_ddot   F77_FUNC( ddot,  DDOT )
-#define BL1_EXPORT_cdotu  F77_FUNC( cdotu,  CDOTU )
-#define BL1_EXPORT_cdotc  F77_FUNC( cdotc,  CDOTC )
-#define BL1_EXPORT_zdotu  F77_FUNC( zdotu,  ZDOTU )
-#define BL1_EXPORT_zdotc  F77_FUNC( zdotc,  ZDOTC )
-#define BL1_EXPORT_snrm2  F77_FUNC( snrm2,  SNRM2 )
-#define BL1_EXPORT_dnrm2  F77_FUNC( dnrm2,  DNRM2 )
-#define BL1_EXPORT_scnrm2 F77_FUNC( scnrm2,  SCNRM2 )
-#define BL1_EXPORT_dznrm2 F77_FUNC( dznrm2,  DZNRM2 )
-#define BL1_EXPORT_sscal  F77_FUNC( sscal,  SSCAL )
-#define BL1_EXPORT_dscal  F77_FUNC( dscal,  DSCAL )
-#define BL1_EXPORT_cscal  F77_FUNC( cscal,  CSCAL )
-#define BL1_EXPORT_csscal F77_FUNC( csscal,  CSSCAL )
-#define BL1_EXPORT_zscal  F77_FUNC( zscal,  ZSCAL )
-#define BL1_EXPORT_zdscal F77_FUNC( zdscal,  ZDSCAL )
-#define BL1_EXPORT_sswap  F77_FUNC( sswap,  SSWAP )
-#define BL1_EXPORT_dswap  F77_FUNC( dswap,  DSWAP )
-#define BL1_EXPORT_cswap  F77_FUNC( cswap,  CSWAP )
-#define BL1_EXPORT_zswap  F77_FUNC( zswap,  ZSWAP )  
-
-// --- Name-mangle level-2 BLAS routines ---------------------------
-
-#define BL1_EXPORT_sgemv  F77_FUNC( sgemv,  SGEMV )
-#define BL1_EXPORT_dgemv  F77_FUNC( dgemv,  DGEMV )
-#define BL1_EXPORT_cgemv  F77_FUNC( cgemv,  CGEMV )
-#define BL1_EXPORT_zgemv  F77_FUNC( zgemv,  ZGEMV )
-#define BL1_EXPORT_sger   F77_FUNC( sger,  SGER )
-#define BL1_EXPORT_dger   F77_FUNC( dger,  DGER )
-#define BL1_EXPORT_cgerc  F77_FUNC( cgerc,  CGERC )
-#define BL1_EXPORT_cgeru  F77_FUNC( cgeru,  CGERU )
-#define BL1_EXPORT_zgerc  F77_FUNC( zgerc,  ZGERC )
-#define BL1_EXPORT_zgeru  F77_FUNC( zgeru,  ZGERU )
-#define BL1_EXPORT_chemv  F77_FUNC( chemv,  CHEMV )
-#define BL1_EXPORT_zhemv  F77_FUNC( zhemv,  ZHEMV )
-#define BL1_EXPORT_cher   F77_FUNC( cher,  CHER )
-#define BL1_EXPORT_zher   F77_FUNC( zher,  ZHER )
-#define BL1_EXPORT_cher2  F77_FUNC( cher2,  CHER2 )
-#define BL1_EXPORT_zher2  F77_FUNC( zher2,  ZHER2 )
-#define BL1_EXPORT_ssymv  F77_FUNC( ssymv,  SSYMV )
-#define BL1_EXPORT_dsymv  F77_FUNC( dsymv,  DSYMV )
-#define BL1_EXPORT_ssyr   F77_FUNC( ssyr,  SSYR )
-#define BL1_EXPORT_dsyr   F77_FUNC( dsyr,  DSYR )
-#define BL1_EXPORT_ssyr2  F77_FUNC( ssyr2,  SSYR2 )
-#define BL1_EXPORT_dsyr2  F77_FUNC( dsyr2,  DSYR2 )
-#define BL1_EXPORT_strmv  F77_FUNC( strmv,  STRMV )
-#define BL1_EXPORT_dtrmv  F77_FUNC( dtrmv,  DTRMV )
-#define BL1_EXPORT_ctrmv  F77_FUNC( ctrmv,  CTRMV )
-#define BL1_EXPORT_ztrmv  F77_FUNC( ztrmv,  ZTRMV )
-#define BL1_EXPORT_strsv  F77_FUNC( strsv,  STRSV )
-#define BL1_EXPORT_dtrsv  F77_FUNC( dtrsv,  DTRSV )
-#define BL1_EXPORT_ctrsv  F77_FUNC( ctrsv,  CTRSV )
-#define BL1_EXPORT_ztrsv  F77_FUNC( ztrsv,  ZTRSV )
-
-// --- Name-mangle level-3 BLAS routines ---------------------------
-
-#define BL1_EXPORT_sgemm  F77_FUNC( sgemm,  SGEMM )
-#define BL1_EXPORT_dgemm  F77_FUNC( dgemm,  DGEMM )
-#define BL1_EXPORT_cgemm  F77_FUNC( cgemm,  CGEMM )
-#define BL1_EXPORT_zgemm  F77_FUNC( zgemm,  ZGEMM )
-#define BL1_EXPORT_chemm  F77_FUNC( chemm,  CHEMM )
-#define BL1_EXPORT_zhemm  F77_FUNC( zhemm,  ZHEMM )
-#define BL1_EXPORT_cherk  F77_FUNC( cherk,  CHERK )
-#define BL1_EXPORT_zherk  F77_FUNC( zherk,  ZHERK )
-#define BL1_EXPORT_cher2k F77_FUNC( cher2k,  CHER2K )
-#define BL1_EXPORT_zher2k F77_FUNC( zher2k,  ZHER2K )
-#define BL1_EXPORT_ssymm  F77_FUNC( ssymm,  SSYMM )
-#define BL1_EXPORT_dsymm  F77_FUNC( dsymm,  DSYMM )
-#define BL1_EXPORT_csymm  F77_FUNC( csymm,  CSYMM )
-#define BL1_EXPORT_zsymm  F77_FUNC( zsymm,  ZSYMM )
-#define BL1_EXPORT_ssyrk  F77_FUNC( ssyrk,  SSYRK )
-#define BL1_EXPORT_dsyrk  F77_FUNC( dsyrk,  DSYRK )
-#define BL1_EXPORT_csyrk  F77_FUNC( csyrk,  CSYRK )
-#define BL1_EXPORT_zsyrk  F77_FUNC( zsyrk,  ZSYRK )
-#define BL1_EXPORT_ssyr2k F77_FUNC( ssyr2k,  SSYR2K )
-#define BL1_EXPORT_dsyr2k F77_FUNC( dsyr2k,  DSYR2K )
-#define BL1_EXPORT_csyr2k F77_FUNC( csyr2k,  CSYR2K )
-#define BL1_EXPORT_zsyr2k F77_FUNC( zsyr2k,  ZSYR2K )
-#define BL1_EXPORT_strmm  F77_FUNC( strmm,  STRMM )
-#define BL1_EXPORT_dtrmm  F77_FUNC( dtrmm,  DTRMM )
-#define BL1_EXPORT_ctrmm  F77_FUNC( ctrmm,  CTRMM )
-#define BL1_EXPORT_ztrmm  F77_FUNC( ztrmm,  ZTRMM )
-#define BL1_EXPORT_strsm  F77_FUNC( strsm,  STRSM )
-#define BL1_EXPORT_dtrsm  F77_FUNC( dtrsm,  DTRSM )
-#define BL1_EXPORT_ctrsm  F77_FUNC( ctrsm,  CTRSM )
-#define BL1_EXPORT_ztrsm  F77_FUNC( ztrsm,  ZTRSM )  
-
 // --- Prototypes --------------------------------------------------------------
 
 // --- Level-1 BLAS prototypes -------------------
@@ -368,3 +262,4 @@ void     F77_dtrsm  ( char* side, char* uplo, char* transa, char* diag, integer*
 void     F77_ctrsm  ( char* side, char* uplo, char* transa, char* diag, integer* m, integer* n, scomplex* alpha, scomplex* a, integer* lda, scomplex* b, integer* ldb );
 void     F77_ztrsm  ( char* side, char* uplo, char* transa, char* diag, integer* m, integer* n, dcomplex* alpha, dcomplex* a, integer* lda, dcomplex* b, integer* ldb );
 
+#endif

d0206d62c3f15d05cf403d974fd06dbf08539aa0
prangana <pradeep.rao@amd.com>
2023-01-31 17:53:03 +0530

    Fix warnings in bunch of files - Part 2
    An earlier commit had fixed warnings in GETRF function. This commit
    fixes few more warnings. More in upcoming commits.
    
    Change-Id: I64207f6d1c035c047caae9a3ff923360aad9a73a

--- src/map/lapack2flamec/FLA_bdsqr.c
+++ src/map/lapack2flamec/FLA_bdsqr.c
@@ -130,7 +130,7 @@
                                                                         \
   *info = 0;                                                            \
                                                                         \
-  AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);                          \									
+  AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);                          \
   return 0;
 
 #define LAPACK_bdsqr_body(prefix)                                   \

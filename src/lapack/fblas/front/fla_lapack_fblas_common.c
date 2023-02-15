991890caabfdaed0e248be7bdd9f11ad62f5a2a7
Jintu Das <jintudas@amd.com>
2023-02-15 01:37:48 -0500

    LAPACKE Test Suite Failures Fix
    Modified ILAENV and DLAQP2 which were causing accuracy issues.
    
    Signed-off-by: Jintu Das <Jintu.Das@amd.com>
    AMD-Internal: CPUPL-3000
    Change-Id: I3e94026c8bb80d3c188dc1fec3b2c6ce3ec683de

--- src/lapack/fblas/front/fla_lapack_fblas_common.c
+++ src/lapack/fblas/front/fla_lapack_fblas_common.c
@@ -12,7 +12,7 @@
 #ifdef FLA_ENABLE_AMD_OPT
 
 /* IDAMAX for small sizes (<= 128)*/
-fla_dim_t fla_idamax(fla_dim_t *n, doublereal *dx, fla_dim_t *incx)
+integer fla_idamax(integer *n, doublereal *dx, integer *incx)
 {
     return fla_idamax_small(n, dx, incx);
 }

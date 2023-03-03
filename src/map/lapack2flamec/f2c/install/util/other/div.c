23cdf060f012bf3bb50ce21ea469a5954757ccdc
nphaniku <phani.kumar@amd.com>
2023-03-03 08:47:51 +0530

    LDL issues fix for non-finites values
       1. div.c moved to other folder
       2. c_div/z_div methods mapped to cladiv_f2c_/zladiv_f2c_ methods
    AMD Internal : [SWLCSG-1900]
    
    Change-Id: I6d77ebbaaeab1b18dedb51267f669d39760ba969

--- src/map/lapack2flamec/f2c/install/util/other/div.c
+++ src/map/lapack2flamec/f2c/install/util/other/div.c
@@ -5,29 +5,29 @@ extern "C" {
 #endif
 
 #ifdef _WIN32
-void c_div(scomplex *c, scomplex *a, scomplex *b)
-{
-    c->real = ((a->real * b->real) + (a->imag * b->imag)) / ((b->real * b->real) + (b->imag * b->imag));
-    c->imag = ((a->imag * b->real) - (a->real * b->imag)) / ((b->real * b->real) + (b->imag * b->imag));
-}
-void z_div(dcomplex *c, dcomplex *a, dcomplex *b)
-{
-    c->real = ((a->real * b->real) + (a->imag * b->imag)) / ((b->real * b->real) + (b->imag * b->imag));
-    c->imag = ((a->imag * b->real) - (a->real * b->imag)) / ((b->real * b->real) + (b->imag * b->imag));
-}
+    void c_div(complex *c, complex *a, complex *b)
+    {
+         c->r = ((a->r * b->r) + (a->i * b->i)) / ((b->r * b->r) + (b->i * b->i));
+         c->i = ((a->i * b->r) - (a->r * b->i)) / ((b->r * b->r) + (b->i * b->i));
+    }
+    void z_div(doublecomplex *c, doublecomplex *a, doublecomplex *b)
+    {
+        c->r = ((a->r * b->r) + (a->i * b->i)) / ((b->r * b->r) + (b->i * b->i));
+        c->i = ((a->i * b->r) - (a->r * b->i)) / ((b->r * b->r) + (b->i * b->i));
+    }
 #else
-void c_div(scomplex *c, scomplex *a, scomplex *b)
-{
-    double _Complex ret_val = (a->real + I * a->imag) / (b->real + I * b->imag);
-    c->real = creal(ret_val);
-    c->imag = cimag(ret_val);
-}
-void z_div(dcomplex *c, dcomplex *a, dcomplex *b)
-{
-    double _Complex ret_val = (a->real + I * a->imag) / (b->real + I * b->imag);
-    c->real = creal(ret_val);
-    c->imag = cimag(ret_val);
-}
+    void c_div(complex *c, complex *a, complex *b)
+    {
+        double _Complex ret_val = (a->r + I*a->i) / (b->r + I*b->i);
+        c->r = creal(ret_val);
+        c->i = cimag(ret_val);
+    }
+    void z_div(doublecomplex *c, doublecomplex *a, doublecomplex *b)
+    {
+        double _Complex ret_val = (a->r + I*a->i) / (b->r + I*b->i);
+        c->r = creal(ret_val);
+        c->i = cimag(ret_val);
+    }
 #endif
 
 #ifdef __cplusplus

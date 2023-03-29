82515a71233fb9c2924fa14f50c4a35e4ff00b6c
Vraj Shah <vraj.shah@amd.com>
2023-03-29 15:13:42 +0530

    Libflame: SGETRF Optimization
    -Added scalar code for smaller size matrixes
    -Added avx2 code for sscal and sger
    -Added different code flow path for various sizes.
    
    AMD-Internal: CPUPL-3168
    Change-Id: I990d517ad3264b36c7830f080aa039ac0242252d

--- src/lapack/x86/avx2/fla_sscal_ix1_avx2.c
+++ src/lapack/x86/avx2/fla_sscal_ix1_avx2.c
@@ -1,89 +1,135 @@
 /******************************************************************************
- * Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
- *******************************************************************************/
+* Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
+*******************************************************************************/
 
 /*! @file fla_sscal_ix1_avx2.c
  *  @brief SSCAL scales a vector by a constant in AVX2.
  *  */
 
 #include "FLAME.h"
-#include "fla_lapack_avx2_kernels.h"
 
-#if FLA_ENABLE_AMD_OPT
-
-int fla_sscal_ix1_avx2(aocl_int64_t *n, real *sa, real *sx)
+#ifdef FLA_ENABLE_AMD_OPT
+int fla_sscal_ix1_avx2(integer *n, real *alpha, real *x)
 {
     /* Local variables */
-    aocl_int64_t i__1, i;
-    __m256 xv256[2], pv256[2], alpha256;
-    __m128 xv128, pv128, alpha128;
-
+    integer i__1, i;
     i__1 = *n;
-
-    if(i__1 <= 0)
+    if (i__1 <= 0)
     {
         return 0;
     }
 
-    /* Load scaling factor alpha*/
-    alpha128 = _mm_set_ps1(*sa);
-    alpha256 = _mm256_broadcastss_ps(alpha128);
+    __m256 vd4_x[4], vd4_o[4], vd4_alpha;
+    __m128 vd2_x, vd2_o, vd2_alpha;
 
-    // Process 16 elements at a time
-    for(i = 0; i < (i__1 - 15); i += 16)
+    /* load scale factor in 256 bit register */
+    vd4_alpha = _mm256_broadcast_ss((float const *) alpha);
+
+    /* load scale factor in 128 bit register */
+    vd2_alpha = _mm_broadcast_ss((float const *) alpha);
+
+    for ( i = 0; (i + 63) < i__1; i += 64)
     {
-        /* load scomplex inputs */
-        xv256[0] = _mm256_loadu_ps((float const *)&sx[i]);
-        xv256[1] = _mm256_loadu_ps((float const *)&sx[i + 8]);
+        // Load the input values.
+        vd4_x[0] = _mm256_loadu_ps( (float const *) &x[i]);
+        vd4_x[1] = _mm256_loadu_ps( (float const *) &x[i + 8]);
+        vd4_x[2] = _mm256_loadu_ps( (float const *) &x[i + 16]);
+        vd4_x[3] = _mm256_loadu_ps( (float const *) &x[i + 24]);
 
-        /* performs the scaling */
-        pv256[0] = _mm256_mul_ps(alpha256, xv256[0]);
-        pv256[1] = _mm256_mul_ps(alpha256, xv256[1]);
+        // perform : x := alpha * x;
+        vd4_o[0] = _mm256_mul_ps( vd4_alpha, vd4_x[0] );
+        vd4_o[1] = _mm256_mul_ps( vd4_alpha, vd4_x[1] );
+        vd4_o[2] = _mm256_mul_ps( vd4_alpha, vd4_x[2] );
+        vd4_o[3] = _mm256_mul_ps( vd4_alpha, vd4_x[3] );
+        
+        // Store the output.
+        _mm256_storeu_ps( ((float *) &x[i]), vd4_o[0]);
+        _mm256_storeu_ps( ((float *) &x[i + 8]), vd4_o[1]);
+        _mm256_storeu_ps( ((float *) &x[i + 16]), vd4_o[2]);
+        _mm256_storeu_ps( ((float *) &x[i + 24]), vd4_o[3]);
 
-        /* store the results */
-        _mm256_storeu_ps((float *)&sx[i], pv256[0]);
-        _mm256_storeu_ps((float *)&sx[i + 8], pv256[1]);
+        // Load the input values.
+        vd4_x[0] = _mm256_loadu_ps( (float const *) &x[i + 32]);
+        vd4_x[1] = _mm256_loadu_ps( (float const *) &x[i + 40]);
+        vd4_x[2] = _mm256_loadu_ps( (float const *) &x[i + 48]);
+        vd4_x[3] = _mm256_loadu_ps( (float const *) &x[i + 56]);
+
+        // perform : x := alpha * x;
+        vd4_o[0] = _mm256_mul_ps( vd4_alpha, vd4_x[0] );
+        vd4_o[1] = _mm256_mul_ps( vd4_alpha, vd4_x[1] );
+        vd4_o[2] = _mm256_mul_ps( vd4_alpha, vd4_x[2] );
+        vd4_o[3] = _mm256_mul_ps( vd4_alpha, vd4_x[3] );
+
+        // Store the output.
+        _mm256_storeu_ps( ((float *) &x[i + 32]), vd4_o[0]);
+        _mm256_storeu_ps( ((float *) &x[i + 40]), vd4_o[1]);
+        _mm256_storeu_ps( ((float *) &x[i + 48]), vd4_o[2]);
+        _mm256_storeu_ps( ((float *) &x[i + 56]), vd4_o[3]);
+
     }
 
-    // Process 8 elements at a time
-    for(; i < (i__1 - 7); i += 8)
+    for ( ; (i + 31) < i__1; i += 32)
     {
-        /* load scomplex inputs */
-        xv256[0] = _mm256_loadu_ps((float const *)&sx[i]);
+        // Load the input values.
+        vd4_x[0] = _mm256_loadu_ps( (float const *) &x[i]);
+        vd4_x[1] = _mm256_loadu_ps( (float const *) &x[i + 8]);
+        vd4_x[2] = _mm256_loadu_ps( (float const *) &x[i + 16]);
+        vd4_x[3] = _mm256_loadu_ps( (float const *) &x[i + 24]);
 
-        /* performs the scaling */
-        pv256[0] = _mm256_mul_ps(alpha256, xv256[0]);
+        // perform : x := alpha * x;
+        vd4_o[0] = _mm256_mul_ps( vd4_alpha, vd4_x[0] );
+        vd4_o[1] = _mm256_mul_ps( vd4_alpha, vd4_x[1] );
+        vd4_o[2] = _mm256_mul_ps( vd4_alpha, vd4_x[2] );
+        vd4_o[3] = _mm256_mul_ps( vd4_alpha, vd4_x[3] );
 
-        /* store the results */
-        _mm256_storeu_ps((float *)&sx[i], pv256[0]);
+        // Store the output.
+        _mm256_storeu_ps( ((float *) &x[i]), vd4_o[0]);
+        _mm256_storeu_ps( ((float *) &x[i + 8]), vd4_o[1]);
+        _mm256_storeu_ps( ((float *) &x[i + 16]), vd4_o[2]);
+        _mm256_storeu_ps( ((float *) &x[i + 24]), vd4_o[3]);
+
     }
 
-    // Process 4 elements at a time
-    for(; i < (i__1 - 3); i += 4)
+    for ( ; (i + 15) < i__1; i += 16)
     {
-        /* load scomplex inputs */
-        xv128 = _mm_loadu_ps((float const *)&sx[i]);
-
+        /* load complex inputs */
+        vd4_x[0] = _mm256_loadu_ps((float const *) &x[i]);
+        vd4_x[1] = _mm256_loadu_ps((float const *) &x[i + 8]);
+        
         /* performs the scaling */
-        pv128 = _mm_mul_ps(alpha128, xv128);
-
+        vd4_o[0] = _mm256_mul_ps(vd4_x[0], vd4_alpha);
+        vd4_o[1] = _mm256_mul_ps(vd4_x[1], vd4_alpha);
+        
         /* store the results */
-        _mm_storeu_ps((float *)&sx[i], pv128);
+        _mm256_storeu_ps((float *) &x[i], vd4_o[0]);
+        _mm256_storeu_ps((float *) &x[i + 8], vd4_o[1]);
+    }
+
+    /* Code for increments equal to 1 only */
+    for ( ; (i + 7) < i__1; i += 8)
+    {
+        /* load complex inputs */
+        vd4_x[0] = _mm256_loadu_ps((float const *) &x[i]);
+        
+        /* performs the scaling */
+        vd4_o[0] = _mm256_mul_ps(vd4_x[0], vd4_alpha);
+        
+        /* store the results */
+        _mm256_storeu_ps((float *) &x[i], vd4_o[0]);
     }
 
     /* remainder iterations */
-    for(; i < i__1; i += 1)
+    for ( ; (i + 0) < i__1; i+= 1)
     {
-        /* load scomplex inputs */
-        xv128 = _mm_load_ss((float const *)&sx[i]);
-
+        /* load complex inputs */
+        vd2_x = _mm_load_ss((float const *) &x[i]);
+        
         /* performs the scaling */
-        pv128 = _mm_mul_ss(alpha128, xv128);
-
+        vd2_o = _mm_mul_ss(vd2_x, vd2_alpha);
+        
         /* store the results */
-        _mm_store_ss((float *)&sx[i], pv128);
+        _mm_store_ss((float *) &x[i], vd2_o);
     }
     return 0;
 }
-
 #endif

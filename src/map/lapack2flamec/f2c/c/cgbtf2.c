463fbe5188ec30e47c450fdbce7e844e62322f68
parsharm <parsharm@amd.com>
2023-05-24 16:06:09 +0530

    Libflame: AOCL FLA progress support for multithread Application.
    Previous Design:
    - AOCL FLA progress supports only single trhead application.
    
    New Design :
    - Now AOCL FLA progress supports for multithread Application.
    
    Fixed the complier warning for aocl_fla_progress.
    
    README is updated with details on testing the feature.
    
    Signed-off-by: Parag <parsharm@amd.com>
    AMD-Internal: CPUPL-3753
    Change-Id: I09468c3201408316f415bc41b99350fa5b2b6c23

--- src/map/lapack2flamec/f2c/c/cgbtf2.c
+++ src/map/lapack2flamec/f2c/c/cgbtf2.c
@@ -237,7 +237,7 @@ void cgbtf2_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, scomp
         return 0;
     }
     #if AOCL_FLA_PROGRESS_H
-        step_count =0;
+        progress_step_count =0;
          #ifndef FLA_ENABLE_WINDOWS_BUILD
                 if(!aocl_fla_progress_ptr)
                     aocl_fla_progress_ptr=aocl_fla_progress;
@@ -272,11 +272,11 @@ void cgbtf2_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, scomp
 	#if AOCL_FLA_PROGRESS_H
             if(aocl_fla_progress_ptr){
                 if(j%32==0 || j==i__1){
-                        step_count=j;
-                        AOCL_FLA_PROGRESS_FUNC_PTR("CGBTF2",6,&step_count,&thread_id,&total_threads);
+                        progress_step_count = j;
+                        AOCL_FLA_PROGRESS_FUNC_PTR("CGBTF2",6,&progress_step_count,&progress_thread_id,&progress_total_threads);
                 }
             }
-        #endif
+    #endif
 
         /* Set fill-in elements in column J+KV to zero. */
         if(j + kv <= *n)
--- src/map/lapack2flamec/f2c/c/zgbtrf.c
+++ src/map/lapack2flamec/f2c/c/zgbtrf.c
@@ -236,7 +236,7 @@ void zgbtrf_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, dcomp
         return 0;
     }
     #if AOCL_FLA_PROGRESS_H
-        step_count =0;
+        progress_step_count =0;
         #ifndef FLA_ENABLE_WINDOWS_BUILD
        	    if(!aocl_fla_progress_ptr)
 		aocl_fla_progress_ptr=aocl_fla_progress;
@@ -313,11 +313,11 @@ void zgbtrf_(aocl_int_t *m, aocl_int_t *n, aocl_int_t *kl, aocl_int_t *ku, dcomp
             jb = fla_min(i__3,i__4);
 	    #if AOCL_FLA_PROGRESS_H
                 if(aocl_fla_progress_ptr){
-                        step_count+=jb;
-                        AOCL_FLA_PROGRESS_FUNC_PTR("ZGBTRF",6,&step_count,&thread_id,&total_threads);
+                        progress_step_count+=jb;
+                        AOCL_FLA_PROGRESS_FUNC_PTR("ZGBTRF",6,&progress_step_count,&progress_thread_id,&progress_total_threads);
                 }
 
-            #endif
+        #endif
 
             /* The active part of the matrix is partitioned */
             /* A11 A12 A13 */

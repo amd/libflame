20cb9932848e9fc4426afbdc59ea85a123e61e83
Edward Smyth <Edward.Smyth@amd.com>
2023-06-05 20:23:31 +0530

    libFLAME: ACML LAPACK test failure: complex xPOTRI and ZPFTRI
    To match netlib LAPACK {c,z}lauu2.f behaviour, ensure diagonal is real
    for elements 1..N-1 in libFLAME equivalent function FLA_Ttmm.
    
    AMD-Internal: [CPUPL-3531]
    Change-Id: I3472db9951d38e0b0adfa805ee514b396c19ae5b

--- src/lapack/misc/ttmm/front/flamec/FLA_Ttmm.c
+++ src/lapack/misc/ttmm/front/flamec/FLA_Ttmm.c
@@ -48,11 +48,11 @@ FLA_Error FLA_Ttmm( FLA_Uplo uplo, FLA_Obj A )
     case FLA_COMPLEX:
     {
       scomplex *buff_A = ( scomplex * ) FLA_COMPLEX_PTR( A );
-      fla_dim_t ldim_A = FLA_Obj_col_stride( A );
+      integer ldim_A = FLA_Obj_col_stride( A );
 
       /* Force diagonal elements 1..N-1 to be real to match
          netlib LAPACK clauu2.f */                        
-      for (fla_dim_t i=0; i<m_A-1; i++)                          
+      for (integer i=0; i<m_A-1; i++)                          
       {                                                       
         buff_A[ldim_A *i + i].imag = 0.0F;                    
       }                                                       
@@ -63,11 +63,11 @@ FLA_Error FLA_Ttmm( FLA_Uplo uplo, FLA_Obj A )
     case FLA_DOUBLE_COMPLEX:
     {
       dcomplex *buff_A = ( dcomplex * ) FLA_DOUBLE_COMPLEX_PTR( A );
-      fla_dim_t ldim_A = FLA_Obj_col_stride( A );
+      integer ldim_A = FLA_Obj_col_stride( A );
 
       /* Force diagonal elements 1..N-1 to be real to match
          netlib LAPACK zlauu2.f */                        
-      for (fla_dim_t i=0; i<m_A-1; i++)                          
+      for (integer i=0; i<m_A-1; i++)                          
       {                                                       
         buff_A[ldim_A *i + i].imag = 0.0;                    
       }                                                       

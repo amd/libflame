49da478f3ce781cd4d0de4c31486f62c8af10adb
prangana <pradeep.rao@amd.com>
2023-07-05 05:45:06 -0700

    Option to link with AOCL-BLAS during build
    In order to enable closer integration with AOCL-BLAS, new configure
    option, "ENABLE_AOCL_BLAS", is created to link with AOCL-BLAS at
    build time. The location of AOCL-BLAS is to be set by user either by
    using environment variable or cmake option "AOCL_ROOT". This location
    must have "include" directory and a "lib" directory that contains the
    necesaary header files and AOCL-BLAS binary respectively. Currently,
    this feature is available for builds through Cmake only. The build
    documentation is updated with these details in BUILD.md.
    
    AMD-Internal: CPUPL-3826
    
    Change-Id: I5de5e8ac1491c9c2f8da6a1ff39a25b326357361

--- src/base/flamec/include/FLA_f2c.h
+++ src/base/flamec/include/FLA_f2c.h
@@ -164,7 +164,7 @@ typedef unsigned __int64 uint64_t;
 typedef int64_t integer;
 typedef uint64_t uinteger;
 #else
-typedef int integer;
+typedef int32_t integer;
 typedef unsigned long int uinteger;
 #endif
 
@@ -310,11 +310,7 @@ typedef short ftnlen;
 typedef short ftnint;
 #else
 typedef long int flag;
- #ifndef BLIS1_FROM_LIBFLAME
-   #define ftnlen integer
- #else
-   typedef long int ftnlen;
- #endif
+typedef integer ftnlen;
 typedef long int ftnint;
 #endif
 

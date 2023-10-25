fdfb3843f34e17e9c583b5575b37112b308e5b19
srigovin <sridhar.govindaswamy@amd.com>
2023-10-25 14:53:01 -0700

    libFLAME library linking error
    Linking error was observed with libFLAME library built without "--enable-amd-opt" option. Checks to exclude compilation of few optimized API functions were missing. Have made the necessary changes to solve the issue.
    
    CPUPL-4119
    Signed-off-by: Sridhar Govindaswamy <Sridhar.Govindaswamy@amd.com>
    Change-Id: Ifbabc9919dc7887592fe91d738c89738e2174ccf

--- src/map/lapack2flamec/f2c/flamec/geqr/dorg2r_fla.c
+++ src/map/lapack2flamec/f2c/flamec/geqr/dorg2r_fla.c
@@ -1,15 +1,10 @@
-/* dorg2r.f -- translated by f2c (version 20160102). You must link the resulting object file with
- libf2c: on Microsoft Windows system, link with libf2c.lib; on Linux or Unix systems, link with
- .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that
- order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in
- /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+/* ../netlib/dorg2r.f -- translated by f2c (version 20100827). You must link the resulting object file with libf2c: on Microsoft Windows system, link with libf2c.lib;
+ on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+
 /*
- *     Modifications Copyright (C) 2021-2026, Advanced Micro Devices, Inc. All rights reserved.
- */
-#include "FLAME.h"
-#if FLA_ENABLE_AOCL_BLAS
-#include "blis.h"
-#endif
+    Modifications Copyright (c) 2023 Advanced Micro Devices, Inc.  All rights reserved.
+*/
+
 #include "FLA_f2c.h" /* Table of constant values */
 static integer c__1 = 1;
 /* > \brief \b DORG2R generates all or part of the orthogonal matrix Q from a QR factorization determined by s geqrf (unblocked algorithm). */
@@ -128,7 +123,7 @@ int lapack_dorg2r(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal 
     /* Initialize global context data */
     aocl_fla_init();
 
-#ifdef FLA_ENABLE_AMD_OPT
+#if FLA_ENABLE_AMD_OPT
     if (global_context.is_avx2)
     {
        retval = dorg2r_fla_opt(m, n, k, a, lda, tau, work, info);
@@ -144,6 +139,7 @@ int lapack_dorg2r(aocl_int64_t *m, aocl_int64_t *n, aocl_int64_t *k, doublereal 
     return retval;
 }
 
+#if FLA_ENABLE_AMD_OPT
 int dorg2r_fla_opt(integer *m, integer *n, integer *k, doublereal * a, integer *lda, doublereal *tau, doublereal *work, integer *info)
 {
     /* System generated locals */
@@ -248,6 +244,7 @@ int dorg2r_fla_opt(integer *m, integer *n, integer *k, doublereal * a, integer *
     return 0;
     /* End of DORG2R */
 }
+#endif
 
 int dorg2r_fla_native(integer *m, integer *n, integer *k, doublereal * a, integer *lda, doublereal *tau, doublereal *work, integer *info)
 {

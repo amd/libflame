90b88805b993454d45c2b9caf80493ee0bc48d65
Venkatesha CH <Venkatesha.PrasadaCh@amd.com>
2024-01-12 02:05:57 -0800

    AOCL-LAPACK Test Suite:  Addition of test case for GELS & LARFG
    Added test code and validation for GELS and LARFG APIs
    
    Signed-off-by: Venkatesha CH <vprasada@amd.com>
    Change-Id: Ia827433263952ba73a8ca1a2d7cbace4c4294d92

--- test/main/src/test_larfg.c
+++ test/main/src/test_larfg.c
@@ -1,23 +1,14 @@
 /*
-    Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+    Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
 */
 
 #include "test_lapack.h"
-#if ENABLE_CPP_TEST
-#include <invoke_common.hh>
-#endif
-#include <invoke_lapacke.h>
 
-extern double perf;
-extern double time_min;
 /* Local prototypes */
-void fla_test_larfg_experiment(char *tst_api, test_params_t *params, integer datatype,
-                               integer p_cur, integer q_cur, integer pci, integer n_repeats,
-                               integer einfo);
-void prepare_larfg_run(integer datatype, integer n_A, integer incx, void *x, void *tau,
-                       integer interfacetype, test_params_t *params);
-void invoke_larfg(integer datatype, integer *n, void *x, integer *incx, integer *abs_incx,
-                  void *tau);
+void fla_test_larfg_experiment(test_params_t *params, integer datatype, integer p_cur, integer q_cur, integer pci,
+                               integer n_repeats, integer einfo, double *perf, double *t, double *residual);
+void prepare_larfg_run(integer datatype, integer n_A, integer incx, void *x, void *tau, integer n_repeats, double *time_min_);
+void invoke_larfg(integer datatype, integer *n, void *x, integer *incx, integer *abs_incx, void *tau);
 
 void fla_test_larfg(integer argc, char **argv, test_params_t *params)
 {
@@ -25,76 +16,83 @@ void fla_test_larfg(integer argc, char **argv, test_params_t *params)
     char *front_str = "LARFG";
     integer tests_not_run = 1, invalid_dtype = 0, einfo = 0;
 
-    if(argc == 1)
+    if (argc == 1)
     {
-        g_config_data = 1;
         fla_test_output_info("--- %s ---\n", op_str);
         fla_test_output_info("\n");
         fla_test_op_driver(front_str, SQUARE_INPUT, params, AUX, fla_test_larfg_experiment);
         tests_not_run = 0;
     }
-    if(argc == 9)
+    if (argc == 7)
     {
-        FLA_TEST_PARSE_LAST_ARG(argv[8]);
+        FLA_TEST_PARSE_LAST_ARG(argv[6]);
     }
 
-    if(argc >= 8 && argc <= 9)
+    if (argc >= 6 && argc <= 7)
     {
         /* Test with parameters from commandline */
         integer i, num_types, N;
         integer datatype, n_repeats;
+        double perf, time_min, residual;
         char stype, type_flag[4] = {0};
         char *endptr;
 
         /* Parse the arguments */
         num_types = strlen(argv[2]);
         N = strtoimax(argv[3], &endptr, CLI_DECIMAL_BASE);
-        params->aux_paramslist[0].alpha_real = strtod(argv[4], &endptr);
-        params->aux_paramslist[0].alpha_imag = strtod(argv[5], &endptr);
-        params->aux_paramslist[0].incx_larfg = strtoimax(argv[6], &endptr, CLI_DECIMAL_BASE);
-        n_repeats = strtoimax(argv[7], &endptr, CLI_DECIMAL_BASE);
-        params->n_repeats = n_repeats;
+        params->aux_paramslist[0].incx_larfg = strtoimax(argv[4], &endptr, CLI_DECIMAL_BASE);
+        n_repeats = strtoimax(argv[5], &endptr, CLI_DECIMAL_BASE);
 
-        if(n_repeats > 0)
+        if (n_repeats > 0)
         {
             params->aux_paramslist[0].aux_threshold = CLI_NORM_THRESH;
 
-            for(i = 0; i < num_types; i++)
+            for (i = 0; i < num_types; i++)
             {
                 stype = argv[2][i];
                 datatype = get_datatype(stype);
 
                 /* Check for invalide dataype */
-                if(datatype == INVALID_TYPE)
+                if (datatype == INVALID_TYPE)
                 {
                     invalid_dtype = 1;
                     continue;
                 }
 
                 /* Check for duplicate datatype presence */
-                if(type_flag[datatype - FLOAT] == 1)
+                if (type_flag[datatype - FLOAT] == 1)
                     continue;
                 type_flag[datatype - FLOAT] = 1;
 
                 /* Call the test code */
-                fla_test_larfg_experiment(front_str, params, datatype, N, N, 0, n_repeats, einfo);
+                fla_test_larfg_experiment(params, datatype,
+                                          N, N,
+                                          0,
+                                          n_repeats, einfo,
+                                          &perf, &time_min, &residual);
+                /* Print the results */
+                fla_test_print_status(front_str,
+                                      stype,
+                                      SQUARE_INPUT,
+                                      N, N,
+                                      residual, params->aux_paramslist[0].aux_threshold,
+                                      time_min, perf);
                 tests_not_run = 0;
             }
         }
     }
 
     /* Print error messages */
-    if(tests_not_run)
+    if (tests_not_run)
     {
         printf("\nIllegal arguments for larfg \n");
-        printf(
-            "./<EXE> LARFG <precisions - sdcz>  <N> <alpha_real> <alpha_imag> <incx> <repeats>\n");
+        printf("./<EXE> LARFG <precisions - sdcz>  <N> <incx> <repeats>\n");
     }
-    if(invalid_dtype)
+    if (invalid_dtype)
     {
         printf("\nInvalid datatypes specified, choose valid datatypes from 'sdcz'\n\n");
     }
-    if(g_ext_fptr != NULL)
+    if (g_ext_fptr != NULL)
     {
         fclose(g_ext_fptr);
         g_ext_fptr = NULL;
@@ -103,146 +101,99 @@ void fla_test_larfg(integer argc, char **argv, test_params_t *params)
     return;
 }
 
-void fla_test_larfg_experiment(char *tst_api, test_params_t *params, integer datatype,
-                               integer p_cur, integer q_cur, integer pci, integer n_repeats,
-                               integer einfo)
+void fla_test_larfg_experiment(test_params_t *params,
+                               integer datatype,
+                               integer p_cur,
+                               integer q_cur,
+                               integer pci,
+                               integer n_repeats,
+                               integer einfo,
+                               double *perf,
+                               double *t,
+                               double *residual)
 {
     integer n, incx, x_length, inc_x, info = 0;
     void *x, *x_test, *tau = NULL;
-    double alpha_real, alpha_imag;
-    double residual, err_thresh;
-    integer interfacetype = params->interfacetype;
-    void *filename = NULL;
+    double time_min = 1e9;
 
     incx = params->aux_paramslist[pci].incx_larfg;
-    alpha_real = params->aux_paramslist[pci].alpha_real;
-    alpha_imag = params->aux_paramslist[pci].alpha_imag;
-    err_thresh = params->aux_paramslist[pci].aux_threshold;
-
+    *residual = params->aux_paramslist[pci].aux_threshold;
     /* Determine the dimensions */
     n = p_cur;
 
+    if (incx < 1)
+    {
+        *residual = DBL_MIN;
+        return;
+    }
     inc_x = fla_i_abs(&incx);
     x_length = (1 + (n - 2) * inc_x) + inc_x;
     create_vector(datatype, &x, x_length);
     create_vector(datatype, &tau, 1);
 
     /* Initializing input values for vector */
-    if(!FLA_BRT_VERIFICATION_RUN)
-    {
-        init_vector(datatype, x, n, inc_x, g_ext_fptr, params->imatrix_char);
-    }
-    FLA_BRT_PROCESS_SINGLE_INPUT(datatype, x_length, 1, x, x_length, "dffd", n, alpha_real,
-                                 alpha_imag, inc_x)
-    if(FLA_OVERFLOW_UNDERFLOW_TEST)
-    {
-        scale_matrix_underflow_overflow_larfg(datatype, n, 1, x, incx, params->imatrix_char);
-    }
+    init_vector(datatype, x, n, inc_x, g_ext_fptr, params->imatrix_char);
 
     create_vector(datatype, &x_test, x_length);
     reset_vector(datatype, x_test, x_length, 1);
-
-    /* Assigning alpha value gto X vector in case of command line.
-     * In case of config the x[0] is treated as alpha */
-    if(!g_config_data)
-        assign_value(datatype, x, alpha_real, alpha_imag);
 
     /* Make a copy of inputs. This is required to validate the API functionality. */
     copy_vector(datatype, n, x, inc_x, x_test, inc_x);
 
     /* call to API */
-    prepare_larfg_run(datatype, n, incx, x_test, tau, interfacetype, params);
-
+    prepare_larfg_run(datatype, n, incx, x_test, tau, n_repeats, &time_min);
     /* execution time */
-    if(time_min == d_zero)
+    *t = time_min;
+    if (time_min == d_zero)
     {
         time_min = 1e-9;
+        *t = time_min;
     }
     /* Performance Computation */
-    perf = (double)(2.0 * n) / time_min / FLOPS_PER_UNIT_PERF;
-    if(datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
+    *perf = (double)(2.0 * n) / time_min / FLOPS_PER_UNIT_PERF;
+    if (datatype == COMPLEX || datatype == DOUBLE_COMPLEX)
     {
-        perf *= 4.0;
+        *perf *= 4.0;
     }
-
     /* Output Validation */
+    validate_larfg(datatype, n, incx, x_length, x, x_test, tau, residual);
+
     FLA_TEST_CHECK_EINFO(residual, info, einfo);
-    IF_FLA_BRT_VALIDATION(
-        n, n,
-        store_outputs_base(filename, params, 0, 2, datatype, x_length, x_test, datatype, 1, tau,
-                           "dffd", n, alpha_real, alpha_imag, inc_x),
-        validate_larfg(tst_api, datatype, n, incx, x_length, x, x_test, tau, residual, params),
-        check_reproducibility_base(filename, params, 0, 2, datatype, x_length, x_test, datatype, 1,
-                                   tau, "dffd", n, alpha_real, alpha_imag, inc_x))
-    else if(!FLA_EXTREME_CASE_TEST)
-    {
-        validate_larfg(tst_api, datatype, n, incx, x_length, x, x_test, tau, residual, params);
-    }
-    /* check for output matrix when inputs as extreme values */
-    else
-    {
-        if(!check_extreme_value(datatype, n, 1, x_test, incx, params->imatrix_char))
-        {
-            residual = DBL_MAX;
-        }
-        else
-        {
-            residual = err_thresh;
-        }
-        FLA_PRINT_TEST_STATUS(n, n, residual, err_thresh);
-    }
     /* Free up the buffers */
-    free_vector(x_test);
-free_buffers:
-    FLA_FREE_FILENAME(filename)
     free_vector(x);
+    free_vector(x_test);
     free_vector(tau);
 }
 
-void prepare_larfg_run(integer datatype, integer n_A, integer incx, void *x, void *tau,
-                       integer interfacetype, test_params_t *params)
+void prepare_larfg_run(integer datatype,
+                       integer n_A, integer incx,
+                       void *x, void *tau,
+                       integer n_repeats,
+                       double *time_min_)
 {
-    integer x_length;
-    double exe_time;
+    integer i, x_length;
+    double time_min = 1e9, exe_time;
     integer inc_x = fla_i_abs(&incx);
     x_length = (1 + (n_A - 2) * inc_x) + inc_x;
     void *x_save;
     create_vector(datatype, &x_save, x_length);
 
-    FLA_EXEC_LOOP_BEGIN
+    for (i = 0; i < n_repeats; ++i)
     {
-        /* Make a copy of the input vector x. Same input values will be passed in each itertaion. */
+    /* Make a copy of the input vector x. Same input values will be passed in each itertaion. */
         copy_vector(datatype, x_length, x, 1, x_save, 1);
+        exe_time = fla_test_clock();
 
-        /* Check if LAPACKE interface is enabled */
-        if((interfacetype == LAPACKE_ROW_TEST) || (interfacetype == LAPACKE_COLUMN_TEST))
-        {
-            exe_time = fla_test_clock();
+        /*  call  larfg API */
+        invoke_larfg(datatype, &n_A, x_save, &incx, &inc_x, tau);
 
-            invoke_lapacke_larfg(datatype, &n_A, x_save, &incx, &inc_x, tau);
+        exe_time = fla_test_clock() - exe_time;
 
-            exe_time = fla_test_clock() - exe_time;
-        }
-#if ENABLE_CPP_TEST
-        else if(interfacetype == LAPACK_CPP_TEST) /* Call CPP larfg API */
-        {
-            exe_time = fla_test_clock();
-            invoke_cpp_larfg(datatype, &n_A, x_save, &incx, &inc_x, tau);
-            exe_time = fla_test_clock() - exe_time;
-        }
-#endif
-        else
-        {
-            exe_time = fla_test_clock();
-
-            invoke_larfg(datatype, &n_A, x_save, &incx, &inc_x, tau);
-
-            exe_time = fla_test_clock() - exe_time;
-        }
-
-        /* Update ctx and loop conditions */
-        FLA_EXEC_LOOP_UPDATE_NO_INFO
+        /* Get the best execution time */
+        time_min = fla_min(time_min, exe_time);
     }
+
+    *time_min_ = time_min;
 
     /* Save the final result to x vector */
     copy_vector(datatype, x_length, x_save, 1, x, 1);
@@ -252,10 +203,9 @@ void prepare_larfg_run(integer datatype, integer n_A, integer incx, void *x, voi
 }
 
 /* larfg API call interface */
-void invoke_larfg(integer datatype, integer *n, void *x, integer *incx, integer *abs_incx,
-                  void *tau)
+void invoke_larfg(integer datatype, integer *n, void *x, integer *incx, integer *abs_incx, void *tau)
 {
-    switch(datatype)
+    switch (datatype)
     {
         case FLOAT:
         {

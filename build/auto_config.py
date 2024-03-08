2ad85bf5d70d0d8a619dcd05105306eb6b61627e
tprnaidu <tprnaidu@amd.com>
2024-03-08 11:18:20 +0530

    Update Zen5 core architecture detection with LF_ISA_CONFIG=auto
    1. Update core architecture detection on Zen5 when using LF_ISA_CONFIG=auto build flag.
    2. Realigned auto_config.py with few Python standards.
    
    AMD-Internal: CPUPL-4433
    Signed-off-by: tprnaidu <TPratap.Naidu@amd.com>
    
    Change-Id: I86fda26404f7f35aab2a7696695f91a1e0753379

--- build/auto_config.py
+++ build/auto_config.py
@@ -1,103 +1,123 @@
-"""Copyright (C) 2023, Advanced Micro Devices, Inc. All Rights Reserved"""
+"""
+Copyright (C) 2023-2024, Advanced Micro Devices, Inc. All Rights Reserved.
+Name: auto_config.py
+Purpose: To check and recognize the zen architecture family
+"""
 
+#Global Imports
 import platform
 import re
 import subprocess
 import sys
 
-def config_check():
-    # Execute wmic shell command with sub-process
-    global model, family, vendor, stepping
-    if 'Windows' in platform.system():
-        result = subprocess.Popen(
-            'wmic cpu get caption', shell=True,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        result = result[0].decode('utf-8')
-    # Replace the newline character with empty char
-        result = result.replace('\n', '')
 
-    # parse the string into list of string
-        parse_string = result.split(" ")
+def config_check():                                                             #Function to check the system info
 
-    # Strip the empty strings from list
-        parse_string = [data for data in parse_string if data.strip()]
+    try:
+        global model, family, vendor, stepping                                  #Global Variables
+        if 'Windows' in platform.system():
+            result = subprocess.Popen('wmic cpu get caption', shell=True,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE).communicate()     #Execute wmic shell command with subprocess
+            result = result[0].decode('utf-8')
 
-        vendor = parse_string[1]
-        family = int(parse_string[3])
-        model = int(parse_string[5])
-        stepping = int(parse_string[7])
+            result = result.replace('\n', '')                                   #Replace the newline character with empty char
+            parse_string = result.split(" ")                                    #Parse the string into list of string
+            parse_string = [data for data in parse_string if data.strip()]      #Strip the empty strings from list
 
-    elif 'Linux' in platform.system():
-        result = subprocess.Popen(
-            'lscpu', shell=True,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        stepping = int(re.findall(r'\WStepping:.*', result[0].decode(
-            'utf-8'), re.MULTILINE)[0].strip('\n').split(' ')[-1])
-        family = int(re.findall(r'\WCPU family:.*', result[0].decode(
-            'utf-8'), re.MULTILINE)[0].strip('\n').split(' ')[-1])
-        model = int(re.findall(r'\WModel:.*', result[0].decode('utf-8'),
-                               re.MULTILINE)[0].strip('\n').split(' ')[-1])
-        vendor = re.findall(r'\WModel name:.*', result[0].decode(
-            'utf-8'), re.MULTILINE)[0].strip('\n').split('Model name:')[-1]
-    # AMD family numbers
-    # Zen/Zen+/Zen2 (0x17) and Zen3/Zen4 (0x19) family numbers
-    zen_family = [23, 25]
-    # Bulldozer / Piledriver / Steamroller / Excavator family number
-    amd_family = 21
+            vendor = parse_string[1]
+            family = int(parse_string[3])
+            model = int(parse_string[5])
+            stepping = int(parse_string[7])
+        elif 'Linux' in platform.system():
+            result = subprocess.Popen('lscpu', shell=True,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE).communicate()     #Execute lscpu command with subprocess
 
-    # AMD CPUID model numbers
-    zen_model = [48, 255]
-    zen2_model = [0, 255]
-    zen3_model = [(0, 15), (32, 95)]
-    zen4_model = [(16, 31), (96, 175)]
-    excavator_model = [96, 127]
-    steamroller_model = [48, 63]
-    piledriver_model = [2, 16, 31]
-    bulldozer_model = [0, 1]
+            stepping = int(re.findall(r'\WStepping:.*', result[0].
+                                      decode('utf-8'), re.MULTILINE)[0].
+                           strip('\n').split(' ')[-1])
+            family = int(re.findall(r'\WCPU family:.*', result[0].
+                                    decode('utf-8'), re.MULTILINE)[0].
+                         strip('\n').split(' ')[-1])
+            model = int(re.findall(r'\WModel:.*', result[0].decode('utf-8'),
+                                   re.MULTILINE)[0].strip('\n').split(' ')[-1])
+            vendor = re.findall(r'\WModel name:.*', result[0].decode('utf-8'),
+                                re.MULTILINE)[0].strip('\n').split('Model '
+                                                                   'name:')[-1]
 
-    # Check the CPU configuration Intel64/AMD64
-    if vendor.count("Intel64"):
-        return
-    elif 'AMD' in vendor:  # .count("AMD64"):
-        # Check the AMD family name
-        if family == zen_family[0]:
-            if zen_model[0] <= model <= zen_model[1]:
-                family="zen2"
-            elif zen2_model[0] <= model <= zen2_model[1]:
-                family="zen"
+        """
+        AMD family numbers
+        Zen/Zen+/Zen2 (0x17), Zen3/Zen4 (0x19) and zen5 (0x1a) family numbers
+        """
+        zen_family = [23, 25, 26]
+
+        """
+        Bulldozer / Piledriver / Steamroller / Excavator family number
+        """
+        amd_family = 21
+
+        """
+        AMD CPUID model numbers
+        """
+        zen_model = [48, 255]
+        zen2_model = [0, 255]
+        zen3_model = [(0, 15), (32, 95)]
+        zen4_model = [(16, 31), (96, 175)]
+        zen5_model = [(0, 15), (16, 31)]
+        excavator_model = [96, 127]
+        steamroller_model = [48, 63]
+        piledriver_model = [2, 16, 31]
+        bulldozer_model = [0, 1]
+
+        if vendor.count("Intel64"):                                             #Check the CPU configuration Intel64/AMD64
+            return
+        elif 'AMD' in vendor:                                                   #.count("AMD64"):
+            """
+            Extracting AMD family name
+            """
+            if family == zen_family[0]:
+                if zen_model[0] <= model <= zen_model[1]:
+                    family="zen2"
+                elif zen2_model[0] <= model <= zen2_model[1]:
+                    family="zen"
+                else:
+                    print("Unknown model number")
+            elif family == zen_family[1]:
+                if (zen3_model[0][0] <= model <= zen3_model[0][1]) or (
+                        zen3_model[1][0] <= model <= zen3_model[1][1]):
+                    family="zen3"
+                elif (zen4_model[0][0] <= model <= zen4_model[0][1]) or (
+                        zen4_model[1][0] <= model <= zen4_model[1][1]):
+                    family="zen4"
+                else:
+                    print("Unknown model number zen4")
+            elif family == zen_family[2]:
+                if ((zen5_model[0][0]) <= model <= (zen5_model[0][1])) or \
+                    ((zen5_model[1][0]) <= model <= (zen5_model[1][1])):
+                    family="zen5"
+                else:
+                    print("Unknown model number for zen5")
+            elif family == amd_family:
+                if excavator_model[0] <= model <= excavator_model[1]:           #Check for specific models of excavator family
+                    family="excavator"
+                elif steamroller_model[0] <= model <= steamroller_model[1]:     #Check for specific models of steamroller family
+                    family="steamroller"
+                elif model == piledriver_model[0] or \
+                        (piledriver_model[1] <= model <= piledriver_model[2]):  #Check for specific models of piledriver family
+                    family="piledriver"
+                elif model == bulldozer_model[0] or \
+                        model == bulldozer_model[1]:
+                    family="bulldozer"
+                else:
+                    print("Unknown model number")
             else:
-                print("Unknown model number")
-        elif family == zen_family[1]:
-            if (zen3_model[0][0] <= model <= zen3_model[0][1]) or (
-                    zen3_model[1][0] <= model <= zen3_model[1][1]):
-                family="zen3"
-            elif (zen4_model[0][0] <= model <= zen4_model[0][1]) or (
-                    zen4_model[1][0] <= model <= zen4_model[1][1]):
-                family="zen4"
-            else:
-                print("Unknown model number zen4")
-        elif family == amd_family:
-            # check for specific models of excavator family
-            if excavator_model[0] <= model <= excavator_model[1]:
-                family="excavator"
-            # check for specific models of steamroller family
-            elif steamroller_model[0] <= model <= steamroller_model[1]:
-                family="steamroller"
-            # check for specific models of piledriver family
-            elif model == piledriver_model[0] or (
-                    piledriver_model[1] <= model <= piledriver_model[2]):
-                family="piledriver"
-            # check for specific models of bulldozer family
-            elif model == bulldozer_model[0] or model == bulldozer_model[1]:
-                family="bulldozer"
-            else:
-                print("Unknown model number")
+                print("Unknown family")
         else:
-            print("Unknown family")
-    else:
-        print("UNKNOWN CPU")
-    return family
+            print("UNKNOWN CPU")
+        return family
+    except Exception as e:
+        print("Exception due to %s" % e)
 
-# Function call for config family names
-config = config_check()
+config = config_check()                                                         #Function call for config family names
 print(config)

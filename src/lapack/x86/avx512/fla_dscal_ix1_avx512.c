3f3a9e078cdf26ed5b99a5d902f3eb8039a31cdf
Phani Kumar <phani.kumar@amd.com>
2024-03-25 10:10:50 +0530

    Perf improvement for DLARFG
     1. Removed extra context initilization.
     2. Improved perf by eight loops at a time through avx2 and AVX512 intrnsics.
     3. Changed AVX2 and AVX512 objects lib names to FLA_LAPACK_AVX2 and FLA_LAPACK_AVX512
    
    AMD Internal : [CPUPL-4598]
    
    Change-Id: I5908fcaeadb3aaa61521a72c2c9163a6d56c71cb

--- src/lapack/x86/avx512/fla_dscal_ix1_avx512.c
+++ src/lapack/x86/avx512/fla_dscal_ix1_avx512.c
@@ -1,17 +1,17 @@
 /******************************************************************************
- * Copyright (C) 2023-24, Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
  *******************************************************************************/
 
-/*! @file fla_dscal_ix1_avx2.c
+/*! @file fla_dscal_ix1_avx512.c
  *  @brief scales a vector by a constant
  *  */
 
 #include "FLAME.h"
-#include "fla_lapack_avx2_kernels.h"
+#include "fla_lapack_avx512_kernels.h"
 
 #if FLA_ENABLE_AMD_OPT
 
-int fla_dscal_ix1_avx2(integer *n, doublereal *da, doublereal *dx, integer *incx)
+int fla_dscal_ix1_avx512(integer *n, doublereal *da, doublereal *dx, integer *incx)
 {
     /* Parameter adjustments */
     --dx;
@@ -27,24 +27,22 @@ int fla_dscal_ix1_avx2(integer *n, doublereal *da, doublereal *dx, integer *incx
     d__1 = da;
 
     /* Load scaling factor alpha*/
+    __m512d alphav = _mm512_set1_pd(*d__1);
     __m128d alpha128 = _mm_set_pd1(*d__1);
-    __m256d alpha256 = _mm256_broadcastsd_pd(alpha128);
     /* Process 8 elements at a time */
     for(i = 1; i <= (i__1 - 7); i += 8)
     {
         /* Load the input values */
-        __m256d x0v = _mm256_loadu_pd((double const *)&dx[i]);
-        __m256d x1v = _mm256_loadu_pd((double const *)&dx[i + 4]);
+        __m512d x0v = _mm512_loadu_pd((double const *)&dx[i]);
         /* perform alpha * x  */
-        x0v = _mm256_mul_pd(alpha256, x0v);
-        x1v = _mm256_mul_pd(alpha256, x1v);
+        x0v = _mm512_mul_pd(alphav, x0v);
         /* Store the output */
-        _mm256_storeu_pd((double *)&dx[i], x0v);
-        _mm256_storeu_pd((double *)&dx[i + 4], x1v);
+        _mm512_storeu_pd((double *)&dx[i], x0v);
     }
     /* Remainder iterations */
     if((i__1 - i) >= 3)
     {
+        __m256d alpha256 = _mm256_broadcastsd_pd(alpha128);
         __m256d x0v = _mm256_loadu_pd((double const *)&dx[i]);
         x0v = _mm256_mul_pd(alpha256, x0v);
         _mm256_storeu_pd((double *)&dx[i], x0v);
@@ -65,6 +63,7 @@ int fla_dscal_ix1_avx2(integer *n, doublereal *da, doublereal *dx, integer *incx
         x1v128 = _mm_mul_pd(alpha128, x1v128);
         _mm_storel_pd((double *)&dx[i], x1v128);
     }
+
     return 0;
 }
 #endif

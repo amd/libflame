e8a53b931ad215824f96e32b89817a67e1380fc0
dnikku <Deepika.Nikku@amd.com>
2024-03-29 06:36:11 -0700

    AOCL LAPACK Testsuite: Added GELSD test case
    Added new test API to verify LAPACK GELSD API functionality
    
    AMD-Internal: CPUPL-4453
    Signed-off-by: dnikku <Deepika.Nikku@amd.com>
    Change-Id: Ie83b567a93079905bafc1a730ef1190da1b27768

--- test/main/src/test_routines.h
+++ test/main/src/test_routines.h
@@ -42,6 +42,7 @@ void fla_test_gtsv(integer argc, char **argv, test_params_t *params);
 void fla_test_gesvdx(integer argc, char **argv, test_params_t *params);
 void fla_test_gbtrf(integer argc, char **argv, test_params_t *params);
 void fla_test_gbtrs(integer argc, char **argv, test_params_t *params);
+void fla_test_gelsd(integer argc, char ** argv, test_params_t *params);
 
 #define LIN_ID 0
 #define EIG_ID 1
@@ -71,7 +72,8 @@ OPERATIONS API_test_functions[]
        {LIN_ID, "org2r", fla_test_org2r},     {LIN_ID, "ung2r", fla_test_org2r},
        {EIG_ID, "syevx", fla_test_syevx},     {EIG_ID, "heevx", fla_test_syevx},
        {LIN_ID, "gtsv", fla_test_gtsv},       {LIN_ID, "gels", fla_test_gels},
-       {AUX_ID, "larfg", fla_test_larfg},     {SVD_ID, "gesvdx", fla_test_gesvdx}};
+       {AUX_ID, "larfg", fla_test_larfg},     {SVD_ID, "gesvdx", fla_test_gesvdx},
+       {LIN_ID, "gelsd", fla_test_gelsd}};
 
 /* Add test API's group entry below */
 char *API_test_group[] = {"LIN", "EIG", "SVD", "AUX"};

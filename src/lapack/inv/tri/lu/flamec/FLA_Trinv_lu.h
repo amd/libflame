23e883187fe42978b854093411570b44365cf413
Phani Kumar <phani.kumar@amd.com>
2024-04-25 16:57:02 +0530

    DGETRI optimizations for small input sizes.
        1. Lapack code added for DTRTRI and DTRTI2 api's.
    	2. In lined gemv , dscal, dswap and dtrmv blas api’s for small input sizes.
    
    AMD Internal : [CPUPL-4604]
    
    Change-Id: I34f0d87dc55b21d2c7384ab7133a89026ded560f

--- src/lapack/inv/tri/lu/flamec/FLA_Trinv_lu.h
+++ src/lapack/inv/tri/lu/flamec/FLA_Trinv_lu.h
@@ -59,3 +59,5 @@ FLA_Error FLA_Trinv_lu_opc_var4( integer mn_A,
                                  scomplex* A, integer rs_A, integer cs_A );
 FLA_Error FLA_Trinv_lu_opz_var4( integer mn_A,
                                  dcomplex* A, integer rs_A, integer cs_A );
+void lapack_dtrtri(char *uplo, char *diag, integer *n, doublereal *a, integer *lda, integer *info);
+void lapack_dtrti2(char *uplo, char *diag, integer *n, doublereal *a, integer *lda, integer *info);

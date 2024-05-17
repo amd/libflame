a64c1c8dc1f436740c3ae37bcaeaecaf6f244f98
Vibhav Gupta <Vibhav.Gupta@amd.com>
2024-05-16 21:20:11 -0700

    AOCL-LAPACK Test Suite: Add LARF API
    Add test run and validation for the LARF API
    
    Signed-off-by: Vibhav Gupta <Vibhav.Gupta@amd.com>
    AMD-Internal: CPUPL-5047
    Change-Id: I92377fdc35f039e3e9a89fa16e4faa2229e85485

--- test/main/src/test_routines.h
+++ test/main/src/test_routines.h
@@ -45,6 +45,7 @@ void fla_test_gbtrs(integer argc, char **argv, test_params_t *params);
 void fla_test_gelsd(integer argc, char **argv, test_params_t *params);
 void fla_test_gelss(integer argc, char **argv, test_params_t *params);
 void fla_test_sytrf(integer argc, char **argv, test_params_t *params);
+void fla_test_larf(integer argc, char **argv, test_params_t *params);
 
 #define LIN_ID 0
 #define EIG_ID 1
@@ -76,7 +77,7 @@ OPERATIONS API_test_functions[]
        {LIN_ID, "gtsv", fla_test_gtsv},       {LIN_ID, "gels", fla_test_gels},
        {AUX_ID, "larfg", fla_test_larfg},     {SVD_ID, "gesvdx", fla_test_gesvdx},
        {LIN_ID, "gelsd", fla_test_gelsd},     {LIN_ID, "gelss", fla_test_gelss},
-       {LIN_ID, "sytrf", fla_test_sytrf}};
+       {LIN_ID, "sytrf", fla_test_sytrf},     {AUX_ID, "larf", fla_test_larf}};
 
 /* Add test API's group entry below */
 char *API_test_group[] = {"LIN", "EIG", "SVD", "AUX"};

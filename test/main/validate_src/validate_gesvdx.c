2b79c7f37c3c0c72e5ffd137a304fdfc78a39f05
Vidhya <Vidhya.Nagendran@amd.com>
2024-06-26 02:43:05 -0700

    AOCL-LAPACK Test Suite:  Overflow and Underflow for GESVDX
    1) Added overflow and underflow test for GESVDX API
    2) The range 'V' treated as 'A' only in case of overflow and underflow
    
    Signed-off-by: vinagend <Vidhya.Nagendran@amd.com>
    Change-Id: I47c5a274faf211a322ee8a03e32b25a69167aab5

--- test/main/validate_src/validate_gesvdx.c
+++ test/main/validate_src/validate_gesvdx.c
@@ -9,7 +9,8 @@
 void validate_gesvdx(char *jobu, char *jobvt, char range, integer m, integer n, void *A,
                      void *A_test, integer lda, void *vl, void *vu, integer il, integer iu,
                      integer ns, void *s, void *s_test, void *U, integer ldu, void *V, integer ldvt,
-                     integer datatype, double *residual, integer *info, FILE *g_ext_fptr)
+                     integer datatype, double *residual, integer *info, FILE *g_ext_fptr,
+                     void *scal, char imatrix)
 {
 
     if(m == 0 || n == 0)
@@ -34,21 +35,46 @@ void validate_gesvdx(char *jobu, char *jobvt, char range, integer m, integer n, 
             if((*jobu == 'V' && *jobvt == 'V') || (*jobu == 'v' && *jobvt == 'v'))
             {
                 /* Test 1: Compute (sigma_test - (U'A VT')) */
-                norm_sigma = fla_lapack_slange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        float *vector = (float *)sigma + i * ns;
+                        norm_sigma = fla_max(norm_sigma, snrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm_sigma = fla_lapack_slange("1", &ns, &ns, sigma, &min_m_n, work);
                 sgemm_("T", "N", &m, &n, &m, &s_one, U, &ldu, A, &lda, &s_zero, U_A, &m);
                 sgemm_("N", "T", &min_m_n, &min_m_n, &n, &s_one, U_A, &m, V, &ldvt, &s_n_one, sigma,
                        &min_m_n);
-                norm1 = fla_lapack_slange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        float *vector = (float *)sigma + i * ns;
+                        norm1 = fla_max(norm1, snrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm1 = fla_lapack_slange("F", &ns, &ns, sigma, &min_m_n, work);
                 resid1 = norm1 / (eps * norm_sigma * fla_max(m, n));
             }
             /* In case of g_ext_fptr the svd matrix is not called*/
             if(g_ext_fptr == NULL)
             {
+                if((imatrix == 'O' || imatrix == 'U') && (range != 'V' && range != 'v'))
+                {
+                    *(float *)scal = 1.00 / *(float *)scal;
+                    sscal_(&ns, scal, s, &i_one);
+                }
                 /* Test 2: To check functionality compute (s_test - s) */
-                norm_s = fla_lapack_slange("1", &ns, &i_one, s_test, &i_one, work);
+                norm_s = fla_lapack_slange("F", &ns, &i_one, s_test, &i_one, work);
                 saxpy_(&ns, &s_n_one, s, &i_one, s_test, &i_one);
-                norm2 = fla_lapack_slange("1", &ns, &i_one, s_test, &i_one, work);
-                resid2 = norm2 / (eps * norm_s * min_m_n);
+                norm2 = fla_lapack_slange("F", &ns, &i_one, s_test, &i_one, work);
+                resid2 = norm2 / (eps * norm_s * ns);
             }
 
             /* Test 3: Checking Orthogonal property: U' * U = I */
@@ -75,19 +101,45 @@ void validate_gesvdx(char *jobu, char *jobvt, char range, integer m, integer n, 
             if((*jobu == 'V' && *jobvt == 'V') || (*jobu == 'v' && *jobvt == 'v'))
             {
                 /* Test 1: Compute (sigma_test - (U'A VT')) */
-                norm_sigma = fla_lapack_dlange("1", &ns, &ns, sigma, &min_m_n, work);
+
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        double *vector = (double *)sigma + i * ns;
+                        norm_sigma = fla_max(norm_sigma, dnrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm_sigma = fla_lapack_dlange("F", &ns, &ns, sigma, &min_m_n, work);
                 dgemm_("T", "N", &m, &n, &m, &d_one, U, &ldu, A, &lda, &d_zero, U_A, &m);
                 dgemm_("N", "T", &min_m_n, &min_m_n, &n, &d_one, U_A, &m, V, &ldvt, &d_n_one, sigma,
                        &min_m_n);
-                norm1 = fla_lapack_dlange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        double *vector = (double *)sigma + i * ns;
+                        norm1 = fla_max(norm1, dnrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm1 = fla_lapack_dlange("F", &ns, &ns, sigma, &min_m_n, work);
                 resid1 = norm1 / (eps * norm_sigma * fla_max(m, n));
             }
             if(g_ext_fptr == NULL)
             {
                 /* Test 2: To check functionality compute (s_test - s) */
-                norm_s = fla_lapack_dlange("1", &ns, &i_one, s_test, &i_one, work);
+                if(imatrix == 'O' || imatrix == 'U')
+                {
+                    *(double *)scal = 1.00 / *(double *)scal;
+                    dscal_(&ns, scal, s, &i_one);
+                }
+                norm_s = fla_lapack_dlange("F", &ns, &i_one, s_test, &i_one, work);
                 daxpy_(&ns, &d_n_one, s, &i_one, s_test, &i_one);
-                norm2 = fla_lapack_dlange("1", &ns, &i_one, s_test, &i_one, work);
+                norm2 = fla_lapack_dlange("F", &ns, &i_one, s_test, &i_one, work);
                 resid2 = norm2 / (eps * norm_s * min_m_n);
             }
 
@@ -116,16 +168,42 @@ void validate_gesvdx(char *jobu, char *jobvt, char range, integer m, integer n, 
             if((*jobu == 'V' && *jobvt == 'V') || (*jobu == 'v' && *jobvt == 'v'))
             {
                 /* Test 1: Compute (sigma_test - (U'A VT')) */
-                norm_sigma = fla_lapack_clange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        scomplex *vector = (scomplex *)sigma + i * ns;
+                        norm_sigma = fla_max(norm_sigma, scnrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm_sigma = fla_lapack_clange("F", &ns, &ns, sigma, &min_m_n, work);
                 cgemm_("C", "N", &m, &n, &m, &c_one, U, &ldu, A, &lda, &c_zero, U_A, &m);
                 cgemm_("N", "C", &min_m_n, &min_m_n, &n, &c_one, U_A, &m, V, &ldvt, &c_n_one, sigma,
                        &min_m_n);
-                norm1 = fla_lapack_clange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        scomplex *vector = (scomplex *)sigma + i * ns;
+                        norm1 = fla_max(norm1, scnrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm1 = fla_lapack_clange("1", &ns, &ns, sigma, &min_m_n, work);
                 resid1 = norm1 / (eps * norm_sigma * fla_max(m, n));
             }
             if(g_ext_fptr == NULL)
             {
                 /* Test 2: To check functionality compute (s_test - s) */
+                if(imatrix == 'O' || imatrix == 'U')
+                {
+                    /* Scaledown the sigma during overflow */
+                    *(float *)scal = s_one / *(float *)scal;
+                    sscal_(&ns, scal, s, &i_one);
+                }
                 norm_s = fla_lapack_slange("1", &ns, &i_one, s, &i_one, work);
                 saxpy_(&ns, &s_n_one, s, &i_one, s_test, &i_one);
                 norm2 = fla_lapack_slange("1", &ns, &i_one, s_test, &i_one, work);
@@ -156,16 +234,42 @@ void validate_gesvdx(char *jobu, char *jobvt, char range, integer m, integer n, 
             if((*jobu == 'V' && *jobvt == 'V') || (*jobu == 'v' && *jobvt == 'v'))
             {
                 /* Test 1: Compute (sigma_test - (U'A VT')) */
-                norm_sigma = fla_lapack_zlange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        dcomplex *vector = (dcomplex *)sigma + i * ns;
+                        norm_sigma = fla_max(norm_sigma, dznrm2_(&ns, vector, &i_one));
+                    }
+                }
+                else
+                    norm_sigma = fla_lapack_zlange("F", &ns, &ns, sigma, &min_m_n, work);
                 zgemm_("C", "N", &m, &n, &m, &z_one, U, &ldu, A, &lda, &z_zero, U_A, &m);
                 zgemm_("N", "C", &min_m_n, &min_m_n, &n, &z_one, U_A, &m, V, &ldvt, &z_n_one, sigma,
                        &min_m_n);
-                norm1 = fla_lapack_zlange("1", &ns, &ns, sigma, &min_m_n, work);
+                if(imatrix == 'O')
+                {
+                    for(int i = 0; i < ns; i++)
+                    {
+                        /* To handle large size values nrm2 is used */
+                        dcomplex *vector = (dcomplex *)sigma + i * ns;
+                        norm1 = fla_max(norm1, dznrm2_(&ns, vector, &i_one));
+                    }
+                }
+                norm1 = fla_lapack_zlange("F", &ns, &ns, sigma, &min_m_n, work);
                 resid1 = norm1 / (eps * norm_sigma * fla_max(m, n));
             }
             if(g_ext_fptr == NULL)
             {
+
                 /* Test 2: To check functionality compute (s_test - s) */
+                if(imatrix == 'O' || imatrix == 'U')
+                {
+                    /* Scaledown the sigma during overflow and underflow */
+                    *(double *)scal = d_one / *(double *)scal;
+                    dscal_(&ns, scal, s, &i_one);
+                }
                 norm_s = fla_lapack_dlange("1", &ns, &i_one, s_test, &i_one, work);
                 daxpy_(&ns, &d_n_one, s, &i_one, s_test, &i_one);
                 norm2 = fla_lapack_dlange("1", &ns, &i_one, s_test, &i_one, work);

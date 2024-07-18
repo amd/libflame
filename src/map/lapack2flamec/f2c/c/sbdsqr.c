05d6f3258811a8b812007a203a51e703d52f1d66
samahmad <Sameer.Ahmad@amd.com>
2024-07-17 23:30:46 -0700

    libFLAME: Fixes for static analysis issues
    Fixed possible memory leak and uninitialized variables.
    
    Signed-off-by: samahmad <Sameer.Ahmad@amd.com>
    AMD-Internal: CPUPL-3431
    Change-Id: I1645058df6026055c545e0a6cf78b0f818b2fcda

--- src/map/lapack2flamec/f2c/c/sbdsqr.c
+++ src/map/lapack2flamec/f2c/c/sbdsqr.c
@@ -3,6 +3,11 @@
  on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a
  standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c
  -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+
+/*
+*     Modifications Copyright (c) 2024 Advanced Micro Devices, Inc.  All rights reserved.
+*/
+
 #include "FLA_f2c.h" /* Table of constant values */
 static doublereal c_b15 = -.125;
 static integer c__1 = 1;
@@ -346,6 +351,7 @@ void sbdsqr_(char *uplo, integer *n, integer *ncvt, integer *nru, integer *ncc, 
     --work;
     /* Function Body */
     *info = 0;
+    oldsn = 0.f;
     lower = lsame_(uplo, "L", 1, 1);
     if(!lsame_(uplo, "U", 1, 1) && !lower)
     {

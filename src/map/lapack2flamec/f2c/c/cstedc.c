bfd18b336c79d037bfecdac57a19e82f27b23450
Mohammad Younas Khan Pathan <mohammadyounaskhan.pathan@amd.com>
2024-08-02 00:19:36 -0700

    Fix for STEDC failure with size > 3000.
    Change-Id: Ibd91985653bded36707d87b28e654aca5295ee90

--- src/map/lapack2flamec/f2c/c/cstedc.c
+++ src/map/lapack2flamec/f2c/c/cstedc.c
@@ -3,6 +3,9 @@
  on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a
  standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c
  -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+
+/* Modifications Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved. */
+
 #include "FLA_f2c.h" /* Table of constant values */
 static integer c__9 = 9;
 static integer c__0 = 0;
@@ -287,6 +290,7 @@ void cstedc_(char *compz, integer *n, real *d__, real *e, complex *z__, integer 
     extern /* Subroutine */
         void
         ssteqr_(char *, integer *, real *, real *, real *, integer *, real *, integer *);
+    extern real sroundup_lwork(integer *);
     /* -- LAPACK computational routine (version 3.7.0) -- */
     /* -- LAPACK is a software package provided by Univ. of Tennessee, -- */
     /* -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..-- */
@@ -392,9 +396,9 @@ void cstedc_(char *compz, integer *n, real *d__, real *e, complex *z__, integer 
             lrwmin = (*n << 2) + 1 + (i__1 * i__1 << 1);
             liwmin = *n * 5 + 3;
         }
-        work[1].r = (real)lwmin;
+        work[1].r = sroundup_lwork(&lwmin);
         work[1].i = 0.f; // , expr subst
-        rwork[1] = (real)lrwmin;
+        rwork[1] = sroundup_lwork(&lrwmin);
         iwork[1] = liwmin;
         if(*lwork < lwmin && !lquery)
         {
@@ -578,9 +582,9 @@ void cstedc_(char *compz, integer *n, real *d__, real *e, complex *z__, integer 
         }
     }
 L70:
-    work[1].r = (real)lwmin;
+    work[1].r = sroundup_lwork(&lwmin);
     work[1].i = 0.f; // , expr subst
-    rwork[1] = (real)lrwmin;
+    rwork[1] = sroundup_lwork(&lrwmin);
     iwork[1] = liwmin;
     AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
     return;

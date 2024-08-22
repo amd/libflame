1effa431964be8f936f8aa0f3e212065dae0c7bc
varajago <varajago@amd.com>
2024-08-23 01:26:52 +0530

    Test-Suite: Fix for GTSV test failure
    Code change to fix issue in input initialization for
    gtsv testing.
    Change in file input reading changed to read RHS matrix.
    
    Signed-off-by: Vasanthakumar R <varajago@amd.com>
    AMD-Internal: CPUPL-5568
    Change-Id: Id191f9e618ae50d5a8ca93cc3bbbb7b4f7701738

--- test/main/validate_src/validate_gtsv.c
+++ test/main/validate_src/validate_gtsv.c
@@ -9,7 +9,8 @@
 #include "test_common.h"
 void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer ldb, void *X,
                    void *Xact, integer ldx, void *dl, void *d, void *du, void *dl_save,
-                   void *d_save, void *du_save, integer info, void *scal, char imatrix, double *residual)
+                   void *d_save, void *du_save, integer info, void *scal, char imatrix,
+                   double *residual)
 {
     if(n == 0 || nrhs == 0)
         return;
@@ -17,6 +18,7 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
     void *work = NULL;
     void *A, *ipiv;
     integer dl_size, du_size, inc, lda;
+    char NORM = '1';
 
     /* Set sizes and dimensions for vector and matrix */
     dl_size = n - 2, du_size = n - 1;
@@ -35,58 +37,26 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
             float norm_dl = 0, norm_d = 0, norm_du = 0, norm3 = 0, norm4 = 0, norm5 = 0, resid3;
             float *d_ptr, *du_ptr, *dl_ptr;
 
-            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
             eps = fla_lapack_slamch("P");
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    float *vector = (float *)B + i * ldb;
-                    norm_B = fla_max(norm_B, snrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm_B = fla_lapack_slange("F", &n, &nrhs, B, &ldb, work);
+
+            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm_B, imatrix, work);
             slagtm_("N", &n, &nrhs, &s_one, dl, d, du, X, &ldx, &s_n_one, B, &ldb);
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    float *vector = (float *)B + i * ldb;
-                    norm1 = fla_max(norm1, snrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm1 = fla_lapack_slange("F", &n, &nrhs, B, &ldb, work);
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm1, imatrix, work);
             resid1 = (norm1 / norm_B)/(eps * n);
 
             /* Test 2: Compute norm (Xact - X / (eps * norm_X *n)) */
-            if(imatrix == 'O')
+            if(Xact != NULL)
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    float *vector = (float *)X + i * ldx;
-                    norm_X = fla_max(norm_X, snrm2_(&n, vector, &i_one));
-                }
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm_X, imatrix, work);
+                matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm2, imatrix, work);
+                resid2 = (norm2 / norm_X) /(eps * n);
             }
             else
-                norm_X = fla_lapack_slange("F", &n, &nrhs, X, &ldx, work);
-            matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
-            if(imatrix == 'O')
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    float *vector = (float *)X + i * ldx;
-                    norm2 = fla_max(norm2, snrm2_(&n, vector, &i_one));
-                }
+                resid2 = (float)*residual;
             }
-            else
-                norm2 = fla_lapack_slange("F", &n, &nrhs, X, &ldx, work);
-            resid2 = (norm2 / norm_X) /(eps * n);
 
             /* Test3 : Compute norm for tridiagonal vectors dl, d, du by
              * Calculating the difference between tridiagonal vector (d, du, dl)
@@ -116,61 +86,29 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
         case DOUBLE:
         {
             double eps, norm_X = 0, norm_B = 0, norm1 = 0, norm2 = 0, resid1 = 0, resid2 = 0;
-            double norm_dl = 0, norm_d = 0, norm_du = 0, norm3 = 0, norm4 = 0, norm5 = 0, resid3 = 0;
+            double norm_dl = 0, norm_d = 0, norm_du = 0, norm3 = 0, norm4 = 0, norm5 = 0, resid3;
             double *d_ptr, *dl_ptr, *du_ptr;
 
-            /* Test1: Compute norm(AX-B / (eps * norm(b) * n)) */
             eps = fla_lapack_dlamch("P");
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    double *vector = (double *)B + i * ldb;
-                    norm_B = fla_max(norm_B, dnrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm_B = fla_lapack_dlange("F", &n, &nrhs, B, &ldb, work);
+
+            /* Test1: Compute norm(AX-B / (eps * norm(b) * n)) */
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm_B, imatrix, work);
             dlagtm_("N", &n, &nrhs, &d_one, dl, d, du, X, &ldx, &d_n_one, B, &ldb);
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    double *vector = (double *)B + i * ldb;
-                    norm1 = fla_max(norm1, dnrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm1 = fla_lapack_dlange("F", &n, &nrhs, B, &ldb, work);
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm1, imatrix, work);
             resid1 = (norm1 / norm_B) /(eps * n);
 
-            /* Test 2: Compute norm (xact - x / (eps * norm(x) *n)) */
-            if(imatrix == 'O')
+            if(Xact != NULL)
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    double *vector = (double *)X + i * ldx;
-                    norm_X = fla_max(norm_X, dnrm2_(&n, vector, &i_one));
-                }
+                /* Test 2: Compute norm (xact - x / (eps * norm(x) *n)) */
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm_X, imatrix, work);
+                matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm2, imatrix, work);
+                resid2 = (norm2 / norm_X) /(eps * n);
             }
             else
-                norm_X = fla_lapack_dlange("F", &n, &nrhs, X, &ldx, work);
-            matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
-            if(imatrix == 'O')
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    double *vector = (double *)X + i * ldx;
-                    norm2 = fla_max(norm2, dnrm2_(&n, vector, &i_one));
-                }
+                resid2 = (float)*residual;
             }
-            else
-                norm2 = fla_lapack_dlange("F", &n, &nrhs, X, &ldx, work);
-            resid2 = (norm2 / norm_X) /(eps * n);
 
             /* Test3 : Compute norm for tridiagonal vectors dl, d, du by
              * Calculating the difference between tridiagonal vector (d, du, dl)
@@ -202,48 +140,27 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
             float eps, norm_X = 0, norm_B = 0, norm1 = 0, norm2 = 0, resid1 = 0, resid2;
             float norm_dl = 0, norm_d = 0, norm_du = 0, norm3 = 0, norm4 = 0, norm5 = 0, resid3;
             scomplex *d_ptr, *dl_ptr, *du_ptr;
-            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
+
             eps = fla_lapack_slamch("P");
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    scomplex *vector = (scomplex *)B + i * ldb;
-                    norm_B = fla_max(norm_B, scnrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm_B = fla_lapack_clange("F", &n, &nrhs, B, &ldb, work);
+
+            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm_B, imatrix, work);
             clagtm_("N", &n, &nrhs, &s_one, dl, d, du, X, &ldx, &s_n_one, B, &ldb);
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    scomplex *vector = (scomplex *)B + i * ldb;
-                    norm1 = fla_max(norm1, scnrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm1 = fla_lapack_clange("F", &n, &nrhs, B, &ldb, work);
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm1, imatrix, work);
             resid1 = (norm1 / norm_B) /(eps * n);
 
-            /* Test 2: Compute norm (xact - x / (eps * norm_X *n)) */
-            norm_X = fla_lapack_clange("F", &n, &nrhs, X, &ldx, work);
-            matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
-            if(imatrix == 'O')
+            if(Xact != NULL)
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    scomplex *vector = (scomplex *)X + i * ldx;
-                    norm_X = fla_max(norm_X, scnrm2_(&n, vector, &i_one));
-                }
+                /* Test 2: Compute norm (xact - x / (eps * norm_X *n)) */
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm_X, imatrix, work);
+                matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm2, imatrix, work);
+                resid2 = (norm2 / norm_X) /(eps * n);
             }
             else
-                norm2 = fla_lapack_clange("F", &n, &nrhs, X, &ldx, work);
-            resid2 = (norm2 / norm_X) /(eps * n);
+            {
+                resid2 = (float)*residual;
+            }
 
             /* Test3 : Compute norm for tridiagonal vectors dl, d, du by
              * Calculating the difference between tridiagonal vector (d, du, dl)
@@ -268,7 +185,6 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
             resid3 = ((norm3 + norm4 + norm5) / (norm_dl + norm_d + norm_du)) / (eps * n);
 
             *residual = (double)fla_max(fla_max(resid1, resid2), resid3);
-
             break;
         }
         case DOUBLE_COMPLEX:
@@ -277,58 +193,26 @@ void validate_gtsv(integer datatype, integer n, integer nrhs, void *B, integer l
             double norm_dl = 0, norm_d = 0, norm_du = 0, norm3 = 0, norm4 = 0, norm5 = 0, resid3;
             dcomplex *d_ptr, *dl_ptr, *du_ptr;
 
-            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
             eps = fla_lapack_dlamch("P");
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    dcomplex *vector = (dcomplex *)B + i * ldb;
-                    norm_B = fla_max(norm_B, dznrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm_B = fla_lapack_zlange("F", &n, &nrhs, B, &ldb, work);
+
+            /* Test1: Compute norm(AX-B / (eps * norm_B * n)) */
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm_B, imatrix, work);
             zlagtm_("N", &n, &nrhs, &d_one, dl, d, du, X, &ldx, &d_n_one, B, &ldb);
-            if(imatrix == 'O')
-            {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    dcomplex *vector = (dcomplex *)B + i * ldb;
-                    norm1 = fla_max(norm1, dznrm2_(&n, vector, &i_one));
-                }
-            }
-            else
-                norm1 = fla_lapack_zlange("F", &n, &nrhs, B, &ldb, work);
+            compute_matrix_norm(datatype, NORM, n, nrhs, B, ldb, &norm1, imatrix, work);
             resid1 = (norm1 / norm_B) /(eps * n);
 
-            /* Test 2: Compute norm (xact - x / (eps * norm_X *n)) */
-            if(imatrix == 'O')
+            if(Xact != NULL)
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    dcomplex *vector = (dcomplex *)X + i * ldx;
-                    norm_X = fla_max(norm_X, dznrm2_(&n, vector, &i_one));
-                }
+                /* Test 2: Compute norm (xact - x / (eps * norm_X *n)) */
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm_X, imatrix, work);
+                matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
+                compute_matrix_norm(datatype, NORM, n, nrhs, X, ldx, &norm2, imatrix, work);
+                resid2 = (norm2 / norm_X) /(eps * n);
             }
             else
-                norm_X = fla_lapack_zlange("F", &n, &nrhs, X, &ldx, work);
-            matrix_difference(datatype, n, nrhs, X, ldx, Xact, ldx);
-            if(imatrix == 'O')
             {
-                for(int i = 0; i < nrhs; i++)
-                {
-                    /* To handle large size values nrm2 is used */
-                    dcomplex *vector = (dcomplex *)X + i * ldx;
-                    norm2 = fla_max(norm2, dznrm2_(&n, vector, &i_one));
-                }
+                resid2 = (float)*residual;
             }
-            else
-                norm2 = fla_lapack_zlange("F", &n, &nrhs, X, &ldx, work);
-            resid2 = (norm2 / norm_X) /(eps * n);
 
             /* Test3 : Compute norm for tridiagonal vectors dl, d, du by
              * Calculating the difference between tridiagonal vector (d, du, dl)

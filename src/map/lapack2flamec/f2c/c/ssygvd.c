c02414b52f950e65b403792ae5b2c6055b97eea5
samahmad <Sameer.Ahmad@amd.com>
2024-10-03 03:56:48 -0700

    sygvd/hegvd APIs return incorrect rounded down lwork and lrwork values.
    Using sroundup_lwork/droundup_lwork to properly convert
    integer to real/doublereal
    
    Change-Id: I9262434e8c5c1cff6b448c38d7a92ae5bb3420aa
    Signed-off-by: samahmad <Sameer.Ahmad@amd.com>
    AMD-Internal: CPUPL-5863

--- src/map/lapack2flamec/f2c/c/ssygvd.c
+++ src/map/lapack2flamec/f2c/c/ssygvd.c
@@ -266,6 +266,7 @@ void ssygvd_(integer *itype, char *jobz, char *uplo, integer *n, real *a, intege
     extern /* Subroutine */
         void
         ssygst_(integer *, char *, integer *, real *, integer *, real *, integer *, integer *);
+    extern real sroundup_lwork(integer *);
     /* -- LAPACK driver routine (version 3.4.0) -- */
     /* -- LAPACK is a software package provided by Univ. of Tennessee, -- */
     /* -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..-- */
@@ -346,7 +347,7 @@ void ssygvd_(integer *itype, char *jobz, char *uplo, integer *n, real *a, intege
     }
     if(*info == 0)
     {
-        work[1] = (real)lopt;
+        work[1] = sroundup_lwork(&lopt);
         iwork[1] = liopt;
         if(*lwork < lwmin && !lquery)
         {
@@ -429,7 +430,7 @@ void ssygvd_(integer *itype, char *jobz, char *uplo, integer *n, real *a, intege
                    lda);
         }
     }
-    work[1] = (real)lopt;
+    work[1] = sroundup_lwork(&lopt);
     iwork[1] = liopt;
     AOCL_DTL_TRACE_EXIT(AOCL_DTL_LEVEL_TRACE_5);
     return;

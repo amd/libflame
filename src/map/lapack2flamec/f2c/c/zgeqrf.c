37097fd6477a14200050a1cf2e5ad7511e6d8c2c
Vibhav Gupta <Vibhav.Gupta@amd.com>
2024-10-08 20:41:18 -0700

    AOCL-LAPACK: Set optimal block size based on input size for geqrf api
    Gains observed upto 15% for the following block size/input size
    combinations on lib-genoa-05 machine
    
    1. For float datatype, block sizes that work well are:-
       a) 60 for size greater than 3000 x 3000
       b) 48 for size greater than 1000 x 1000
       c) 24 for rest all cases
    2. For double datatype, block sizes that work well are:-
       a) 64 for size greater than 2000 x 2000
       b) 60 for size greater than 1000 x 1000
       c) 32 for rest all cases
    3. For complex datatype, block sizes that work well are:-
       a) 64 for size greater than 600 x 600
       b) 48 for size greater than 100 x 100
       c) 32 for rest all cases
    4. For double complex datatype, block sizes that work well are:-
       a) 64 for size greater than 400 x 400
       b) 48 for size greater than 100 x 100
       c) 32 for rest all cases
    
    Signed-off-by: Vibhav Gupta <Vibhav.Gupta@amd.com>
    AMD-Internal: CPUPL-5876
    
    Change-Id: Ide6acd9d41faafac6a84bdd18d38849789636fbe

--- src/map/lapack2flamec/f2c/c/zgeqrf.c
+++ src/map/lapack2flamec/f2c/c/zgeqrf.c
@@ -3,18 +3,16 @@
  .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that
  order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in
  /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+/******************************************************************************
+ * Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+ *******************************************************************************/
 #include "FLA_f2c.h" /* Table of constant values */
 #if !FLA_ENABLE_AMD_OPT
-static aocl_int64_t c__1 = 1;
+static integer c__1 = 1;
 #endif
-static aocl_int64_t c_n1 = -1;
-static aocl_int64_t c__3 = 3;
-static aocl_int64_t c__2 = 2;
-
-extern int fla_thread_get_num_threads();
-
-integer get_block_size_zgeqrf(aocl_int64_t *m, aocl_int64_t *n);
-
+static integer c_n1 = -1;
+static integer c__3 = 3;
+static integer c__2 = 2;
 /* > \brief \b ZGEQRF */
 /* =========== DOCUMENTATION =========== */
 /* Online html documentation available at */
@@ -211,7 +209,22 @@ void zgeqrf_(integer *m, integer *n, doublecomplex *a, integer *lda, doublecompl
     /* Function Body */
     k = fla_min(*m, *n);
     *info = 0;
+#if FLA_ENABLE_AMD_OPT
+    if(*m >= 400 && *n >= 400)
+    {
+        nb = 64;
+    }
+    else if(*m >= 100 && *n >= 100)
+    {
+        nb = 48;
+    }
+    else
+    {
+        nb = 32;
+    }
+#else
     nb = ilaenv_(&c__1, "ZGEQRF", " ", m, n, &c_n1, &c_n1);
+#endif
     lquery = *lwork == -1;
     if(*m < 0)
     {

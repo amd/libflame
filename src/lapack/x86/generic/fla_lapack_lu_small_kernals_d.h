f94fd32d5b25a4ed40c888bae64b55bf3de2d8a8
Phani Kumar <phani.kumar@amd.com>
2024-11-07 19:22:00 +0530

    Performance improvement in dgetri 3x3 input size
       1. Optimized the LAPACK_GETRI_SMALL_D_3x3 kernel by reducing the internal repetitive memory load/stores
    
    AMD Internal : [CPUPL-5865]
    
    Change-Id: Ib4e33cccdc4a78820014a676bcb9808f4c685797

--- src/lapack/x86/generic/fla_lapack_lu_small_kernals_d.h
+++ src/lapack/x86/generic/fla_lapack_lu_small_kernals_d.h
@@ -243,66 +243,76 @@
     i = i + 1;                                                           \
     FLA_LU_PIV_SMALL_D_3x3(i, n, buff_A, ldim_A, buff_p, info);
 
-#define LAPACK_GETRI_SMALL_D_3x3(n, a, a_dim1, ipiv, work)                               \
-    integer jp;                                                                          \
-    doublereal t_val, *apiv;                                                             \
-    /* trmv loop 1 */                                                                    \
-    a[1 + a_dim1] = 1. / a[1 + a_dim1];                                                  \
-    /* loop 2*/                                                                          \
-    a[2 + 2 * a_dim1] = 1. / a[2 + 2 * a_dim1];                                          \
-    a[1 + 2 * a_dim1] = a[1 + 2 * a_dim1] * a[1 + a_dim1];                               \
-    /*  loop 3 */                                                                        \
-    a[1 + 2 * a_dim1] = -a[2 + 2 * a_dim1] * a[1 + 2 * a_dim1];                          \
-    a[3 + 3 * a_dim1] = 1. / a[3 + 3 * a_dim1];                                          \
-    a[1 + 3 * a_dim1]                                                                    \
-        = (a[1 + 3 * a_dim1] * a[1 + a_dim1]) + (a[2 + 3 * a_dim1] * a[1 + 2 * a_dim1]); \
-    a[2 + 3 * a_dim1] = a[2 + 3 * a_dim1] * a[2 + 2 * a_dim1];                           \
-                                                                                         \
-    a[1 + 3 * a_dim1] = -a[3 + 3 * a_dim1] * a[1 + 3 * a_dim1];                          \
-    a[2 + 3 * a_dim1] = -a[3 + 3 * a_dim1] * a[2 + 3 * a_dim1];                          \
-    /* gemv inline */                                                                    \
-    a[1 + 2 * a_dim1] = a[1 + 2 * a_dim1] - a[3 + 2 * a_dim1] * a[1 + 3 * a_dim1];       \
-    a[2 + 2 * a_dim1] = a[2 + 2 * a_dim1] - a[3 + 2 * a_dim1] * a[2 + 3 * a_dim1];       \
-    a[3 + 2 * a_dim1] = -a[3 + 2 * a_dim1] * a[3 + 3 * a_dim1];                          \
-                                                                                         \
-    work[1] = a[3 + a_dim1];                                                             \
-                                                                                         \
-    a[1 + a_dim1] = a[1 + a_dim1] - a[2 + a_dim1] * a[1 + 2 * a_dim1];                   \
-    a[3 + a_dim1] = -a[2 + a_dim1] * a[3 + 2 * a_dim1];                                  \
-    a[2 + a_dim1] = -a[2 + a_dim1] * a[2 + 2 * a_dim1];                                  \
-                                                                                         \
-    a[1 + a_dim1] = a[1 + a_dim1] - work[1] * a[1 + 3 * a_dim1];                         \
-    a[2 + a_dim1] = a[2 + a_dim1] - work[1] * a[2 + 3 * a_dim1];                         \
-    a[3 + a_dim1] = a[3 + a_dim1] - work[1] * a[3 + 3 * a_dim1];                         \
-                                                                                         \
-    jp = ipiv[2];                                                                        \
-    if(jp != 2)                                                                          \
-    {                                                                                    \
-        /*  swap */                                                                      \
-        apiv = &a[jp * a_dim1 + 1];                                                      \
-        t_val = apiv[0];                                                                 \
-        apiv[0] = a[1 + 2 * a_dim1];                                                     \
-        a[1 + 2 * a_dim1] = t_val;                                                       \
-        t_val = apiv[1];                                                                 \
-        apiv[1] = a[2 + 2 * a_dim1];                                                     \
-        a[2 + 2 * a_dim1] = t_val;                                                       \
-        t_val = apiv[2];                                                                 \
-        apiv[2] = a[3 + 2 * a_dim1];                                                     \
-        a[3 + 2 * a_dim1] = t_val;                                                       \
-    }                                                                                    \
-    jp = ipiv[1];                                                                        \
-    if(jp != 1)                                                                          \
-    {                                                                                    \
-        /*  swap */                                                                      \
-        apiv = &a[jp * a_dim1 + 1];                                                      \
-        t_val = apiv[0];                                                                 \
-        apiv[0] = a[1 + a_dim1];                                                         \
-        a[1 + a_dim1] = t_val;                                                           \
-        t_val = apiv[1];                                                                 \
-        apiv[1] = a[2 + a_dim1];                                                         \
-        a[2 + a_dim1] = t_val;                                                           \
-        t_val = apiv[2];                                                                 \
-        apiv[2] = a[3 + a_dim1];                                                         \
-        a[3 + a_dim1] = t_val;                                                           \
+#define LAPACK_GETRI_SMALL_D_3x3(n, a, a_dim1, ipiv, work)                                  \
+    integer jp, a_dim2, a_dim3;                                                             \
+    doublereal t_val, *apiv, t_val11, t_val21, t_val31, t_val12, t_val22, t_val32, t_val13, \
+        t_val23, t_val33;                                                                   \
+    /* trmv loop 1 */                                                                       \
+    a_dim2 = 2 * a_dim1;                                                                    \
+    a_dim3 = 3 * a_dim1;                                                                    \
+    t_val11 = 1. / a[1 + a_dim1];                                                           \
+    t_val21 = a[2 + a_dim1];                                                                \
+    /* loop 2*/                                                                             \
+    t_val22 = 1. / a[2 + a_dim2];                                                           \
+    t_val12 = a[1 + a_dim2] * t_val11;                                                      \
+    /*  loop 3 */                                                                           \
+    t_val12 = -t_val22 * t_val12;                                                           \
+    t_val33 = 1. / a[3 + a_dim3];                                                           \
+    t_val23 = a[2 + a_dim3];                                                                \
+    t_val13 = a[1 + a_dim3];                                                                \
+    t_val13 = (t_val13 * t_val11) + (t_val23 * t_val12);                                    \
+    t_val23 = t_val23 * t_val22;                                                            \
+                                                                                            \
+    t_val13 = -t_val33 * t_val13;                                                           \
+    t_val23 = -t_val33 * t_val23;                                                           \
+    /* gemv inline */                                                                       \
+    t_val32 = a[3 + a_dim2];                                                                \
+    t_val12 = t_val12 - t_val32 * t_val13;                                                  \
+    t_val22 = t_val22 - t_val32 * t_val23;                                                  \
+    t_val32 = -t_val32 * t_val33;                                                           \
+    t_val31 = a[3 + a_dim1];                                                                \
+    work[1] = t_val31;                                                                      \
+                                                                                            \
+    t_val11 = t_val11 - t_val21 * t_val12;                                                  \
+    t_val31 = -t_val21 * t_val32;                                                           \
+    t_val21 = -t_val21 * t_val22;                                                           \
+    a[1 + a_dim2] = t_val12;                                                                \
+    a[2 + a_dim2] = t_val22;                                                                \
+    a[3 + a_dim2] = t_val32;                                                                \
+    a[1 + a_dim1] = t_val11 - work[1] * t_val13;                                            \
+    a[2 + a_dim1] = t_val21 - work[1] * t_val23;                                            \
+    a[3 + a_dim1] = t_val31 - work[1] * t_val33;                                            \
+    a[1 + a_dim3] = t_val13;                                                                \
+    a[2 + a_dim3] = t_val23;                                                                \
+    a[3 + a_dim3] = t_val33;                                                                \
+    jp = ipiv[2];                                                                           \
+    if(jp != 2)                                                                             \
+    {                                                                                       \
+        /*  swap */                                                                         \
+        apiv = &a[jp * a_dim1 + 1];                                                         \
+        t_val = apiv[0];                                                                    \
+        apiv[0] = a[1 + a_dim2];                                                            \
+        a[1 + a_dim2] = t_val;                                                              \
+        t_val = apiv[1];                                                                    \
+        apiv[1] = a[2 + a_dim2];                                                            \
+        a[2 + a_dim2] = t_val;                                                              \
+        t_val = apiv[2];                                                                    \
+        apiv[2] = a[3 + a_dim2];                                                            \
+        a[3 + a_dim2] = t_val;                                                              \
+    }                                                                                       \
+    jp = ipiv[1];                                                                           \
+    if(jp != 1)                                                                             \
+    {                                                                                       \
+        /*  swap */                                                                         \
+        apiv = &a[jp * a_dim1 + 1];                                                         \
+        t_val = apiv[0];                                                                    \
+        apiv[0] = a[1 + a_dim1];                                                            \
+        a[1 + a_dim1] = t_val;                                                              \
+        t_val = apiv[1];                                                                    \
+        apiv[1] = a[2 + a_dim1];                                                            \
+        a[2 + a_dim1] = t_val;                                                              \
+        t_val = apiv[2];                                                                    \
+        apiv[2] = a[3 + a_dim1];                                                            \
+        a[3 + a_dim1] = t_val;                                                              \
     }
 #endif

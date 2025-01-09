019e740f43c85ffb656a6ca941a684339a457c28
prangana <pradeep.rao@amd.com>
2025-01-09 07:07:06 -0800

    Use AOCL_ROOT variable to get dependent library paths
    Following changes
     - AOCL_ROOT can now be used to set both AOCL-BLAS and AOCL-Utils paths.
       Test suite would use the same too.
     - If LIBAOCLUTILS_LIBRARY_PATH is provided by user, the same
       will override AOCL_ROOT for AOCL-Utils library
     - If CMAKE_EXT_BLAS_LIBRARY_DEPENDENCY_PATH is provided by user,
       - The same will be used if ENABLE_AOCL_BLAS is not set
       - Ignored if ENABLE_AOCL_BLAS is set. Instead will use BLAS
         path from AOCL_ROOT
    
    AMD-Internal: [CPUPL-5530]
    
    Change-Id: I6fe7fd037b28f38e89eb0afbd33489ea6f27350d

--- cmake/find_dep_libs.cmake
+++ cmake/find_dep_libs.cmake
@@ -1,5 +1,5 @@
 # ########################################################################
-#Copyright(c) 2023 Advanced Micro Devices, Inc.
+#Copyright(c) 2023-2025 Advanced Micro Devices, Inc.
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files(the "Software"), to deal
@@ -48,11 +48,26 @@ function(aocl_libs)
       ENDIF(BUILD_SHARED_LIBS)
     ENDIF(FLA_OPENMP_MULTITHREADING)
 
+    # set aocl-utils library name
+    IF(BUILD_SHARED_LIBS)
+      SET(UTILS_LIB_NAME "libaoclutils")
+    ELSE(BUILD_SHARED_LIBS)
+      SET(BLAS_LIB_NAME "libaoclutils_static")
+    ENDIF(BUILD_SHARED_LIBS)
+
     find_library(AOCL_BLAS_LIB
     NAMES ${BLAS_LIB_NAME}
     HINTS ${AOCL_ROOT}/blis ${AOCL_ROOT}/amd-blis ${AOCL_ROOT}
     PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
     DOC "AOCL-BLAS library"
+    )
+
+    # add aocl-utils library
+    find_library(AOCL_UTILS_LIB
+    NAMES ${UTILS_LIB_NAME}
+    HINTS ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}
+    PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
+    DOC "AOCL-UTILS library"
     )
 
     #====Headers
@@ -61,6 +76,14 @@ function(aocl_libs)
     HINTS ${AOCL_ROOT}/amd-blis ${AOCL_ROOT}/blis ${AOCL_ROOT}
     PATH_SUFFIXES "include/${ILP_DIR}" "include_${ILP_DIR}" "include" "include/blis"
     DOC "AOCL-BLAS headers"
+    )
+
+    # add aocl-utils headers
+    find_path(AOCL_UTILS_INCLUDE_DIR
+    NAMES alci_c.h  alci.h  arch.h  enum.h  macros.h
+    HINTS ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}
+    PATH_SUFFIXES "include/${ILP_DIR}/alci" "include_${ILP_DIR}/alci" "include/alci"
+    DOC "AOCL-UTILS headers"
     )
 
   ELSE(WIN32)   
@@ -77,6 +100,8 @@ function(aocl_libs)
       SET(BLAS_LIB_NAME "blis")
     ENDIF(FLA_OPENMP_MULTITHREADING)
 
+    set(UTILS_LIB_NAME "aoclutils")
+
     find_library(AOCL_BLAS_LIB
     NAMES ${BLAS_LIB_NAME}
     HINTS ${AOCL_ROOT}/blis ${AOCL_ROOT}/amd-blis ${AOCL_ROOT}
@@ -84,6 +109,14 @@ function(aocl_libs)
     DOC "AOCL-BLAS library"
     )
     
+    # find aoclutils library
+    find_library(AOCL_UTILS_LIB
+    NAMES ${UTILS_LIB_NAME}
+    HINTS ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}
+    PATH_SUFFIXES "lib/${ILP_DIR}" "lib_${ILP_DIR}" "lib"
+    DOC "AOCL-UTILS library"
+    )
+
     #====Headers
     find_path(AOCL_BLAS_INCLUDE_DIR
     NAMES blis.h  blis.hh  cblas.h  cblas.hh
@@ -92,17 +125,37 @@ function(aocl_libs)
     DOC "AOCL-BLAS headers"
     )
 
+    # add aocl-utils headers
+    find_path(AOCL_UTILS_INCLUDE_DIR
+    NAMES alci/alci_c.h  alci/alci.h  alci/arch.h  alci/enum.h  alci/macros.h
+    HINTS ${AOCL_ROOT}/amd-utils ${AOCL_ROOT}
+    PATH_SUFFIXES "include/${ILP_DIR}" "include_${ILP_DIR}" "include"
+    DOC "AOCL-UTILS headers"
+    )
+
   ENDIF(WIN32)
 
   #===========
+  # Check if the library and headers are found
   if(AOCL_BLAS_LIB AND AOCL_BLAS_INCLUDE_DIR)
-    set(AOCL_BLAS_FOUND true PARENT_SCOPE)
-    set(BLAS_LIBRARY ${AOCL_BLAS_LIB})  
-  else()    
-    message (FATAL_ERROR "Error: could not find a suitable installation of AOCL-BLAS Library in \$AOCL_ROOT=${AOCL_ROOT}")
+    set(BLAS_LIBRARY ${AOCL_BLAS_LIB})
+
+    # Get the path and name of the library
+    get_filename_component(BLAS_LIB_PATH ${AOCL_BLAS_LIB} DIRECTORY)
+    get_filename_component(BLAS_LIB_NAME ${AOCL_BLAS_LIB} NAME)
   endif()
 
-  set(BLAS_LIBRARY ${BLAS_LIBRARY} PARENT_SCOPE)
+  # check if aocl-utils library and headers are found
+  if(AOCL_UTILS_LIB AND AOCL_UTILS_INCLUDE_DIR)
+    set(AOCL_UTILS_LIBRARY ${AOCL_UTILS_LIB})
+  endif()
+
+  # Set the variables to the parent scope
+set(BLAS_LIBRARY ${BLAS_LIBRARY} PARENT_SCOPE)
+set(BLAS_LIB_PATH ${BLAS_LIB_PATH} PARENT_SCOPE)
+set(BLAS_LIB_NAME ${BLAS_LIB_NAME} PARENT_SCOPE)
+set(AOCL_UTILS_LIBRARY ${AOCL_UTILS_LIBRARY} PARENT_SCOPE)
+set(AOCL_UTILS_INCLUDE_DIR ${AOCL_UTILS_INCLUDE_DIR} PARENT_SCOPE)
 
 endfunction(aocl_libs)
 
@@ -110,6 +163,8 @@ endfunction(aocl_libs)
 # clear to avoid endless appending on subsequent calls
 set(BLAS_LIBRARY)
 unset(BLAS_INCLUDE_DIR)
+set(AOCL_UTILS_LIBRARY)
+unset(AOCL_UTILS_INCLUDE_DIR)
 
 if(DEFINED ENV{AOCL_ROOT})            
     SET(AOCL_ROOT $ENV{AOCL_ROOT})
@@ -118,11 +173,11 @@ if(DEFINED ENV{AOCL_ROOT})
 			message(FATAL_ERROR "\n Invalid path to AOCL_ROOT \n")
 		endif()
 elseif(AOCL_ROOT)
-      SET(AOCL_ROOT ${AOCL_ROOT})
-      message(STATUS "AOCL_ROOT set from cmake option is ${AOCL_ROOT}")
-      if(NOT EXISTS ${AOCL_ROOT})
-        message(FATAL_ERROR "\n Invalid path to AOCL_ROOT \n")
-      endif()
+    SET(AOCL_ROOT ${AOCL_ROOT})
+    message(STATUS "AOCL_ROOT set from cmake option is ${AOCL_ROOT}")
+    if(NOT EXISTS ${AOCL_ROOT})
+      message(FATAL_ERROR "\n Invalid path to AOCL_ROOT \n")
+    endif()
 else()
       # if aocl-root is not set then check using pkg-config by default
       find_package(PkgConfig)
@@ -140,19 +195,39 @@ else()
           endif()
           message(STATUS "Found AOCL-BLAS library using pkg-config: ${BLAS_LIBRARY}")
         endif()
-      endif()
+
+        pkg_check_modules(AOCL_UTILS aocl-utils)
+        if (AOCL_UTILS_FOUND AND UNIX)
+          set (AOCL_UTILS_INCLUDE_DIR ${AOCL_UTILS_INCLUDE_DIRS})
+          get_filename_component(AOCL_UTILS_LIB_PATH ${AOCL_UTILS_LINK_LIBRARIES} DIRECTORY)
+          get_filename_component(AOCL_UTILS_LIB_NAME ${AOCL_UTILS_LINK_LIBRARIES} NAME_WE)
+
+          if (BUILD_SHARED_LIBS)
+            set (AOCL_UTILS_LIBRARY ${AOCL_UTILS_LIB_PATH}/${AOCL_UTILS_LIB_NAME}.so)
+          else()
+            set (AOCL_UTILS_LIBRARY ${AOCL_UTILS_LIB_PATH}/${AOCL_UTILS_LIB_NAME}.a)
+          endif()
+          message(STATUS "Found AOCL-UTILS library using pkg-config: ${AOCL_UTILS_LIBRARY}")
+        endif()
+      endif()      
 
       set(AOCL_ROOT ${CMAKE_INSTALL_PREFIX})
 endif()
 
-if (NOT AOCL_BLAS_INCLUDE_DIR OR NOT BLAS_LIBRARY)
+if (NOT AOCL_BLAS_INCLUDE_DIR OR NOT BLAS_LIBRARY
+    OR NOT AOCL_UTILS_INCLUDE_DIR OR NOT AOCL_UTILS_LIBRARY)
   aocl_libs()
 endif()
 
 set(BLAS_LIBRARY ${BLAS_LIBRARY})
 set(BLAS_INCLUDE_DIR ${AOCL_BLAS_INCLUDE_DIR})
+set(AOCL_UTILS_LIBRARY ${AOCL_UTILS_LIBRARY})
+set(AOCL_UTILS_INCLUDE_DIR ${AOCL_UTILS_INCLUDE_DIR})
 
-message(STATUS "AOCL-BLAS LIBRARY= ${BLAS_LIBRARY}")
-message(STATUS "AOCL-BLAS INCLUDE DIRS= ${BLAS_INCLUDE_DIR}")
+if(ENABLE_AOCL_BLAS)
+  message(STATUS "AOCL-BLAS LIBRARY= ${BLAS_LIBRARY}")
+  message(STATUS "AOCL-BLAS INCLUDE DIRS= ${BLAS_INCLUDE_DIR}")
+endif()
 
-mark_as_advanced(BLAS_LIBRARY BLAS_INCLUDE_DIR)
+# Hide these variables from the CMake GUI
+mark_as_advanced(BLAS_LIBRARY BLAS_INCLUDE_DIR BLIS_LIB_NAME BLIS_LIB_PATH AOCL_UTILS_LIBRARY AOCL_UTILS_INCLUDE_DIR)

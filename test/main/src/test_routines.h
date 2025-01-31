6ae37bb8e97630aec29a4229c2be9df68462ea35
samahmad <Sameer.Ahmad@amd.com>
2025-01-31 01:08:35 -0800

    AOCL LAPACK: Test Suite - Addition of getrfnp and getrfnpi APIs
    Following changes are made in this commit:
    
    1. Test cases for getrfnp and getrfnpi. These test cases use
       exisiting validate function of getrf to validate the result.
    2. OV/UV test cases are also added.
    3. Fix bug in getrfnpi API, where memory was being written to
       out of allocation bound when the given lda was greater than
       the column stride.
    
    Change-Id: Ia8252268b35b49be91a128b966439baa8ec3dc38
    Signed-off-by: samahmad <Sameer.Ahmad@amd.com>
    AMD-Internal: CPUPL-6375

--- test/main/src/test_routines.h
+++ test/main/src/test_routines.h
@@ -51,6 +51,8 @@ void fla_test_larf(integer argc, char **argv, test_params_t *params);
 void fla_test_sygvd(integer argc, char **argv, test_params_t *params);
 void fla_test_lange(integer argc, char **argv, test_params_t *params);
 void fla_test_gecon(integer argc, char **argv, test_params_t *params);
+void fla_test_getrfnp(integer argc, char **argv, test_params_t *params);
+void fla_test_getrfnpi(integer argc, char **argv, test_params_t *params);
 
 #define LIN_ID 0
 #define EIG_ID 1
@@ -85,7 +87,8 @@ OPERATIONS API_test_functions[]
        {LIN_ID, "sytrf", fla_test_sytrf},     {AUX_ID, "larf", fla_test_larf},
        {EIG_ID, "sygvd", fla_test_sygvd},     {AUX_ID, "hegvd", fla_test_sygvd},
        {LIN_ID, "hetrf_rook", fla_test_hetrf_rook}, {LIN_ID, "sytrf_rook", fla_test_sytrf_rook},
-       {AUX_ID, "lange", fla_test_lange},     {LIN_ID, "gecon", fla_test_gecon}};
+       {AUX_ID, "lange", fla_test_lange},     {LIN_ID, "gecon", fla_test_gecon},
+       {LIN_ID, "getrfnp", fla_test_getrfnp}, {LIN_ID, "getrfnpi", fla_test_getrfnpi}};
 
 /* Add test API's group entry below */
 char *API_test_group[] = {"LIN", "EIG", "SVD", "AUX"};

0f06c6227e1e4f982722280ad0e3f9c48f7519f4
Mohammad Younas Khan Pathan <mohammadyounaskhan.pathan@amd.com>
2025-03-19 06:16:43 +0530

    New SYTRD test case in main test suite
    Also added all ctests for SYTRD.
    
    AMD-Internal: [CPUPL-6425]
    Change-Id: Icd8d6cf947b6482951e91d59725c2f520db67fcc

--- test/main/validate_src/validate_sytrd.c
+++ test/main/validate_src/validate_sytrd.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+    Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
 */
 
 /*! @file validate_sytrd.c
@@ -10,9 +10,9 @@
 #include "test_prototype.h"
 
 void validate_sytrd(char *tst_api, integer datatype, char uplo, integer n, void *A_test, void *A_in,
-                    integer lda, void *D, void *E, void *tau, double err_thresh, void *params)
+                    integer lda, void *D, void *E, void *tau, double err_thresh)
 {
-    double residual = 0., resid1 = 0., resid2 = 0., resid3 = 0., resid4 = 0.;
+    double residual = 0., resid1 = 0., resid2 = 0.;
     void *A_save = NULL, *work = NULL, *Q_temp = NULL;
     void *Q = NULL, *T = NULL;
     integer lwork = -1;
@@ -61,71 +61,69 @@ void validate_sytrd(char *tst_api, integer datatype, char uplo, integer n, void 
     {
         case FLOAT:
         {
-            float norm, norm_A;
+            float norm, norm_A, eps;
+            eps = fla_lapack_slamch("P");
 
             /* Test 1: Generating symmetric matrix A using  T - Q**T * A * Q | / ( |T| n ulp ) */
             norm_A = fla_lapack_slange("1", &n, &n, A_save, &n, NULL);
             sgemm_("T", "N", &n, &n, &n, &s_one, Q, &n, A_in, &lda, &s_zero, Q_temp, &n);
             sgemm_("N", "N", &n, &n, &n, &s_one, Q_temp, &n, Q, &n, &s_n_one, A_save, &n);
             norm = fla_lapack_slange("1", &n, &n, A_save, &n, NULL);
-            resid1 = fla_compute_residual(datatype, 'P', norm, norm_A, n, params);
+            resid1 = norm / (eps * norm_A * (float)n);
 
             /* Test 2: Compute norm(I - Q*Q') / (N * EPS)*/
-            resid2 = check_orthogonality(datatype, Q, n, n, n, params);
+            resid2 = check_orthogonality(datatype, Q, n, n, n);
             break;
         }
         case DOUBLE:
         {
-            double norm, norm_A;
+            double norm, norm_A, eps;
+            eps = fla_lapack_dlamch("P");
 
             /* Test 1: Generating symmetric matrix A using  T - Q**T * A * Q | / ( |T| n ulp ) */
             norm_A = fla_lapack_dlange("1", &n, &n, A_save, &n, NULL);
             dgemm_("T", "N", &n, &n, &n, &d_one, Q, &n, A_in, &lda, &d_zero, Q_temp, &n);
             dgemm_("N", "N", &n, &n, &n, &d_one, Q_temp, &n, Q, &n, &d_n_one, A_save, &n);
             norm = fla_lapack_dlange("1", &n, &n, A_save, &n, NULL);
-            resid1 = fla_compute_residual(datatype, 'P', norm, norm_A, n, params);
+            resid1 = norm / (eps * norm_A * (float)n);
 
             /* Test 2: Compute norm(I - Q*Q') / (N * EPS)*/
-            resid2 = check_orthogonality(datatype, Q, n, n, n, params);
+            resid2 = check_orthogonality(datatype, Q, n, n, n);
             break;
         }
         case COMPLEX:
         {
-            float norm, norm_A;
+            float norm, norm_A, eps;
+            eps = fla_lapack_slamch("P");
 
             /* Test 1: Generating symmetric matrix A using  T - Q**T * A * Q | / ( |T| n ulp ) */
             norm_A = fla_lapack_clange("1", &n, &n, A_save, &n, work);
             cgemm_("C", "N", &n, &n, &n, &c_one, Q, &n, A_in, &lda, &c_zero, Q_temp, &n);
             cgemm_("N", "N", &n, &n, &n, &c_one, Q_temp, &n, Q, &n, &c_n_one, A_save, &n);
             norm = fla_lapack_clange("1", &n, &n, A_save, &n, work);
-            resid1 = fla_compute_residual(datatype, 'P', norm, norm_A, n, params);
+            resid1 = norm / (eps * norm_A * (float)n);
 
             /* Test 2: Compute norm(I - Q*Q') / (N * EPS)*/
-            resid2 = check_orthogonality(datatype, Q, n, n, n, params);
+            resid2 = check_orthogonality(datatype, Q, n, n, n);
             break;
         }
         case DOUBLE_COMPLEX:
         {
-            double norm, norm_A;
+            double norm, norm_A, eps;
+            eps = fla_lapack_dlamch("P");
 
             /* Test 1: Generating symmetric matrix A using  T - Q**T * A * Q | / ( |T| n ulp ) */
             norm_A = fla_lapack_zlange("1", &n, &n, A_save, &n, work);
             zgemm_("C", "N", &n, &n, &n, &z_one, Q, &n, A_in, &lda, &z_zero, Q_temp, &n);
             zgemm_("N", "N", &n, &n, &n, &z_one, Q_temp, &n, Q, &n, &z_n_one, A_save, &n);
             norm = fla_lapack_zlange("1", &n, &n, A_save, &n, work);
-            resid1 = fla_compute_residual(datatype, 'P', norm, norm_A, n, params);
+            resid1 = norm / (eps * norm_A * (float)n);
 
             /* Test 2: Compute norm(I - Q*Q') / (N * EPS)*/
-            resid2 = check_orthogonality(datatype, Q, n, n, n, params);
+            resid2 = check_orthogonality(datatype, Q, n, n, n);
             break;
         }
     }
-
-    /* Test 3: Ensure unused triangle was not modified */
-    resid3 = compare_matrix(datatype, same_char(uplo, 'U') ? "L" : "U", n, n, A_in, lda, A_test, lda);
-
-    /* Test 4: Check padding rows not modified */
-    resid4 = check_padding(datatype, n, n, A_test, lda);
 
     free_matrix(T);
     free_matrix(A_save);
@@ -134,11 +132,7 @@ void validate_sytrd(char *tst_api, integer datatype, char uplo, integer n, void 
     free_vector(work);
 
     residual = fla_test_max(resid1, resid2);
-    residual = fla_test_max(resid3, residual);
-    residual = fla_test_max(resid4, residual);
     FLA_PRINT_TEST_STATUS(n, n, residual, err_thresh);
     FLA_PRINT_SUBTEST_STATUS(resid1, err_thresh, "01");
     FLA_PRINT_SUBTEST_STATUS(resid2, err_thresh, "02");
-    FLA_PRINT_SUBTEST_STATUS(resid3, err_thresh, "03");
-    FLA_PRINT_SUBTEST_STATUS(resid4, err_thresh, "04");
 }

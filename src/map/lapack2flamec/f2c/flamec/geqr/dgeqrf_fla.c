1bf61cb5ad6687932f3c0cf490ec49d1d12b5ee8
Ahmad, Sameer <Sameer.Ahmad@amd.com>
2025-07-18 10:33:59 +0530

    AOCL LAPACK: DLARF, DLARFT, DLACPY and DGEQRF Optimizations
    The commit contains optimizations for the 4 APIs.
    
    DLARF: Added panel based blocking for large sizes to make the code
    mode cache friendly.
    
    DLARFT: Added blocking based algorithm to use level 3 BLAS APIs
    
    DLACPY: Added code to use memcpy function to copy matrices.
    
    DGEQRF: Added multithreading in LAPACK layer for large size matrices.
    The API should run with OMP_PROC_BIND=true for best performance.
    
    AMD-Internal: CPUPL-6866

--- src/map/lapack2flamec/f2c/flamec/geqr/dgeqrf_fla.c
+++ src/map/lapack2flamec/f2c/flamec/geqr/dgeqrf_fla.c
@@ -16,6 +16,19 @@ extern int fla_thread_get_num_threads();
 
 integer get_block_size_dgeqrf(integer *m, integer *n);
 
+#if FLA_ENABLE_AMD_OPT
+static void dgeqrf_mt_large(integer gm, integer gn, doublereal *a, integer lda, doublereal *tau,
+                            doublereal *work, integer nthreads, integer *info);
+static integer dgeqrf_mt_large_num_threads(integer gm, integer gn);
+static integer dgeqrf_mt_large_lwork(integer gm, integer gn, integer num_threads);
+void dgeqr2_fla(integer *, integer *, doublereal *, integer *, doublereal *, doublereal *,
+                integer *);
+void dlarfb_(char *, char *, char *, char *, integer *, integer *, integer *, doublereal *,
+             integer *, doublereal *, integer *, doublereal *, integer *, doublereal *, integer *);
+void dlarft_(char *, char *, integer *, integer *, doublereal *, integer *, doublereal *,
+             doublereal *, integer *);
+#endif
+
 /* > \brief \b DGEQRF */
 /* =========== DOCUMENTATION =========== */
 /* Online html documentation available at */
@@ -222,6 +235,15 @@ void dgeqrf_fla(integer *m, integer *n, doublereal *a, integer *lda, doublereal 
 #endif
 
     lwkopt = *n * nb;
+#if FLA_ENABLE_AMD_OPT
+    /* If the matrix is large, use the multithreaded version */
+    /* Need to tune this threshold */
+    integer nthreads = dgeqrf_mt_large_num_threads(*m, *n);
+    if(*m >= FLA_DGEQRF_MT_LARGE_M_THRESH && *n >= FLA_DGEQRF_MT_LARGE_N_THRESH)
+    {
+        lwkopt = dgeqrf_mt_large_lwork(*m, *n, nthreads);
+    }
+#endif
     work[1] = (doublereal)lwkopt;
     lquery = *lwork == -1;
     if(*m < 0)
@@ -269,12 +291,20 @@ void dgeqrf_fla(integer *m, integer *n, doublereal *a, integer *lda, doublereal 
 #endif
 
 /* Path for small sizes */
-#ifdef FLA_ENABLE_AMD_OPT
+#if FLA_ENABLE_AMD_OPT
     if(FLA_IS_MIN_ARCH_ID(FLA_ARCH_AVX2) && *m <= FLA_GEQRF_STHRESH && *n <= FLA_GEQRF_STHRESH)
     {
         fla_dgeqrf_small(m, n, &a[a_offset], lda, &tau[1], &work[1]);
         return;
     }
+#ifdef FLA_OPENMP_MULTITHREADING
+    else if(*m >= FLA_DGEQRF_MT_LARGE_M_THRESH && *n >= FLA_DGEQRF_MT_LARGE_N_THRESH
+            && *lwork >= lwkopt)
+    {
+        dgeqrf_mt_large(*m, *n, &a[a_offset], *lda, &tau[1], &work[1], nthreads, info);
+        return;
+    }
+#endif
 #endif
 
     if(nb > 1 && nb < k)
@@ -409,3 +439,161 @@ integer get_block_size_dgeqrf(integer *m, integer *n)
 }
 
 /* dgeqrf_ */
+
+#if FLA_ENABLE_AMD_OPT
+
+/* Returns the optimial number of threads to use for dgeqrf_mt_large */
+static integer dgeqrf_mt_large_num_threads(integer gm, integer gn)
+{
+    long long num_elems = (long long)gm * gn;
+    integer opt_nthreads;
+    if(num_elems <= FLA_DGEQRF_MT_THRESHOLD_8_THREADS)
+    {
+        opt_nthreads = 8;
+    }
+    else
+    {
+        opt_nthreads = 64;
+    }
+    return fla_min(fla_thread_get_num_threads(), opt_nthreads);
+}
+
+/* Returns the number of containers needed for given nb items of size item_size
+ * to fit in a container of size container_size */
+static inline size_t containers_needed_for_size(integer nb, size_t item_size, size_t container_size)
+{
+    size_t size_needed = item_size * nb;
+    return (size_needed - 1) / container_size + 1;
+}
+
+/* Returns the optimal lwork for multithreaded dgeqrf */
+static integer dgeqrf_mt_large_lwork(integer gm, integer gn, integer num_threads)
+{
+
+    integer nb = FLA_DGEQRF_MT_LARGE_PANEL_SIZE;
+
+    /* Number of panels */
+    integer nt = ((gn - 1) / nb) + 1;
+
+    /* Storage to keep triangular factors for each panel */
+    integer triangular_factor_req = (nb * nb) * nt;
+
+    integer per_thread_work_req = (nb * nb) * num_threads;
+
+    /* Dependency list to keep track of which panels are ready */
+    integer depend_list_req = containers_needed_for_size(nt, sizeof(uint8_t), sizeof(doublereal));
+
+    integer lwork = triangular_factor_req + per_thread_work_req + depend_list_req;
+
+    return lwork;
+}
+
+static void dgeqrf_large_mt_thread_fn(integer m, integer n, doublereal *a, integer lda,
+                                      doublereal *tau, doublereal *T_storage, uint8_t *a_dependency,
+                                      integer nb, integer k, doublereal *per_thread_work,
+                                      integer per_thread_work_size, integer *info)
+{
+    /* Get the current thread number */
+    integer thread_num = omp_get_thread_num();
+    integer iinfo;
+    /* Get the thread-local workspace */
+    doublereal *t_work = per_thread_work + thread_num * per_thread_work_size;
+
+    integer ldwork = nb;
+
+    /* Apply block reflector to all panels in left
+     * columns of the current block.
+     */
+    for(integer i = 0; i < fla_min(m, k); i += nb)
+    {
+        /* If the block reflector is not ready, wait for it */
+        while(a_dependency[i / nb] == 0)
+        {
+            /* Wait for all memory operation to be completed */
+            __asm__ __volatile__("lfence");
+            /* Use pause instruction for busy waiting */
+            __asm__ __volatile__("pause");
+        }
+
+        /* Apply the block reflector to the current panel */
+        integer mvai = m - i;
+        integer n_reflector = fla_min(mvai, nb);
+        dlarfb_("Left", "Transpose", "Forward", "Columnwise", &mvai, &n, &n_reflector,
+                &a[i + i * lda], &lda, T_storage + (nb * i), &nb, &a[k * lda + i], &lda, t_work,
+                &ldwork);
+    }
+
+    integer mvak = m - k;
+
+    /* If there are no more rows to process, return */
+    if(mvak <= 0)
+    {
+        return;
+    }
+
+    /* Factor the current panel */
+    dgeqr2_fla(&mvak, &n, &a[k * lda + k], &lda, &tau[k], t_work, &iinfo);
+
+    /* If the factorization failed, set the info and return */
+    if(iinfo != 0)
+    {
+        *info = iinfo;
+        return;
+    }
+
+    /* Generate the triangular factor of the block reflector */
+    integer k_reflector = fla_min(mvak, n);
+    dlarft_("Forward", "Columnwise", &mvak, &k_reflector, &a[k * lda + k], &lda, &tau[k],
+            T_storage + (nb * k), &nb);
+
+    /* Set the block reflector as ready */
+    a_dependency[k / nb] = 1;
+    /* Ensure all memory operations are completed */
+    __asm__ __volatile__("sfence");
+}
+
+static void dgeqrf_mt_large(integer gm, integer gn, doublereal *a, integer lda, doublereal *tau,
+                            doublereal *work, integer nthreads, integer *info)
+{
+
+    integer nb = FLA_DGEQRF_MT_LARGE_PANEL_SIZE;
+
+    integer iinfo = 0;
+
+    /* Number of panels */
+    integer nt = ((gn - 1) / nb) + 1;
+
+    /* Storage to keep triangular factors for each panel */
+    doublereal *T_storage = work;
+
+    /* Dependency list to keep track of which panels are ready */
+    uint8_t *a_dependency = (uint8_t *)(T_storage + (nb * nb) * nt);
+
+    /* Per-thread workspace */
+    doublereal *per_thread_work
+        = ((doublereal *)a_dependency)
+          + containers_needed_for_size(nt, sizeof(uint8_t), sizeof(doublereal));
+
+    /* Initialize the dependency list to 0 */
+    memset(a_dependency, 0, nt);
+
+    /* Work size for each thread */
+    integer per_thread_work_size = (nb * nb);
+
+/* Schedule threads to process panels
+ * Panels are assigned to threads in a round-robin manner
+ */
+#pragma omp parallel for num_threads(nthreads) schedule(static, 1) proc_bind(close)
+    for(integer i = 0; i < gn; i += nb)
+    {
+        /* Get the number of coulmns in current panel */
+        integer n_thread = fla_min(nb, gn - i);
+        dgeqrf_large_mt_thread_fn(gm, n_thread, a, lda, tau, T_storage, a_dependency, nb, i,
+                                  per_thread_work, per_thread_work_size, &iinfo);
+    }
+
+    /* Set the info */
+    *info = iinfo;
+}
+
+#endif

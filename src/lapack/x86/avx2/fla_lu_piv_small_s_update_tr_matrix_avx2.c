1beba8a9c88ad40a09b49b6259c84bc7a3a6c89d
Pathan, Mohammad Younas Khan <MohammadYounasKhan.Pathan@amd.com>
2026-02-27 21:18:15 +0530

    Fix for GETRF when values are less than safe min. (#242)
    Root cause: Due to very small precision values than safe min, computation on values becomes nan's.
    Solution: Added safemin (lamch) and checking value before computation of SGETRF, DGETRF APIs for avx2, avx512 kernels.
    
    AMD-Internal: CPUPL-8032

--- src/lapack/x86/avx2/fla_lu_piv_small_s_update_tr_matrix_avx2.c
+++ src/lapack/x86/avx2/fla_lu_piv_small_s_update_tr_matrix_avx2.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
  *******************************************************************************/
 
 #include "FLAME.h"
@@ -14,7 +14,7 @@
  * corresponding BLAS APIs to reduce function overheads.
  */
 void fla_lu_piv_small_s_update_tr_matrix_avx2(aocl_int64_t i_1, aocl_int64_t mi, aocl_int64_t ni, real *acur,
-                                              aocl_int64_t lda_t)
+                                              aocl_int64_t lda_t, real safemin)
 {
 
     aocl_int64_t j, b_off, y_off;
@@ -22,53 +22,108 @@ void fla_lu_piv_small_s_update_tr_matrix_avx2(aocl_int64_t i_1, aocl_int64_t mi,
     __m256 result[4], tempY[4], tempb[4], tempx, p_val8;
 
     p_val = *acur;
-    p_val = 1 / p_val;
-    p_val8 = _mm256_broadcast_ss(&p_val);
-    for(; i_1 < mi - 7; i_1 += 8)
-    {
-        tempx = _mm256_mul_ps(_mm256_loadu_ps(&acur[i_1]), p_val8);
-        _mm256_storeu_ps(&acur[i_1], tempx);
 
-        for(j = 1; j < ni - 3; j += 4)
+    if(f2c_abs(p_val) >= safemin)
+    {
+        p_val = 1 / p_val;
+        p_val8 = _mm256_broadcast_ss(&p_val);
+        for(; i_1 < mi - 7; i_1 += 8)
         {
-            b_off = j * lda_t;
-            y_off = i_1 + j * lda_t;
-            tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
-            tempb[1] = _mm256_broadcast_ss(&acur[b_off] + 1 * lda_t);
-            tempb[2] = _mm256_broadcast_ss(&acur[b_off] + 2 * lda_t);
-            tempb[3] = _mm256_broadcast_ss(&acur[b_off] + 3 * lda_t);
-            tempY[0] = _mm256_loadu_ps(&acur[y_off]);
-            tempY[1] = _mm256_loadu_ps(&acur[y_off] + 1 * lda_t);
-            tempY[2] = _mm256_loadu_ps(&acur[y_off] + 2 * lda_t);
-            tempY[3] = _mm256_loadu_ps(&acur[y_off] + 3 * lda_t);
-            // Y := Y - b * x
-            result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
-            result[1] = _mm256_fnmadd_ps(tempb[1], tempx, tempY[1]);
-            result[2] = _mm256_fnmadd_ps(tempb[2], tempx, tempY[2]);
-            result[3] = _mm256_fnmadd_ps(tempb[3], tempx, tempY[3]);
-            _mm256_storeu_ps(&acur[y_off], result[0]);
-            _mm256_storeu_ps((&acur[y_off] + 1 * lda_t), result[1]);
-            _mm256_storeu_ps((&acur[y_off] + 2 * lda_t), result[2]);
-            _mm256_storeu_ps((&acur[y_off] + 3 * lda_t), result[3]);
+            tempx = _mm256_mul_ps(_mm256_loadu_ps(&acur[i_1]), p_val8);
+            _mm256_storeu_ps(&acur[i_1], tempx);
+
+            for(j = 1; j < ni - 3; j += 4)
+            {
+                b_off = j * lda_t;
+                y_off = i_1 + b_off;
+                tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
+                tempb[1] = _mm256_broadcast_ss(&acur[b_off] + 1 * lda_t);
+                tempb[2] = _mm256_broadcast_ss(&acur[b_off] + 2 * lda_t);
+                tempb[3] = _mm256_broadcast_ss(&acur[b_off] + 3 * lda_t);
+                tempY[0] = _mm256_loadu_ps(&acur[y_off]);
+                tempY[1] = _mm256_loadu_ps(&acur[y_off] + 1 * lda_t);
+                tempY[2] = _mm256_loadu_ps(&acur[y_off] + 2 * lda_t);
+                tempY[3] = _mm256_loadu_ps(&acur[y_off] + 3 * lda_t);
+                // Y := Y - b * x
+                result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
+                result[1] = _mm256_fnmadd_ps(tempb[1], tempx, tempY[1]);
+                result[2] = _mm256_fnmadd_ps(tempb[2], tempx, tempY[2]);
+                result[3] = _mm256_fnmadd_ps(tempb[3], tempx, tempY[3]);
+                _mm256_storeu_ps(&acur[y_off], result[0]);
+                _mm256_storeu_ps((&acur[y_off] + 1 * lda_t), result[1]);
+                _mm256_storeu_ps((&acur[y_off] + 2 * lda_t), result[2]);
+                _mm256_storeu_ps((&acur[y_off] + 3 * lda_t), result[3]);
+            }
+            /* remaining inner loop update */
+            for(; j < ni; j++)
+            {
+                b_off = j * lda_t;
+                y_off = i_1 + b_off;
+                tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
+                tempY[0] = _mm256_loadu_ps(&acur[y_off]);
+                result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
+                _mm256_storeu_ps(&acur[y_off], result[0]);
+            }
         }
-        /* remining inner loop updation */
-        for(; j < ni; j++)
+        /* remaining outer loop update */
+        for(; i_1 < mi; i_1++)
         {
-            b_off = j * lda_t;
-            y_off = i_1 + j * lda_t;
-            tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
-            tempY[0] = _mm256_loadu_ps(&acur[y_off]);
-            result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
-            _mm256_storeu_ps(&acur[y_off], result[0]);
+            acur[i_1] = acur[i_1] * p_val;
+            for(j = 1; j < ni; j++)
+            {
+                acur[i_1 + j * lda_t] = acur[i_1 + j * lda_t] - acur[j * lda_t] * acur[i_1];
+            }
         }
     }
-    /* remining outer loop updation */
-    for(; i_1 < mi; i_1++)
+    else /* use division if value is less than safemin*/
     {
-        acur[i_1] = acur[i_1] * p_val;
-        for(j = 1; j < ni; j++)
+        p_val8 = _mm256_broadcast_ss(&p_val);
+        for(; i_1 < mi - 7; i_1 += 8)
         {
-            acur[i_1 + j * lda_t] = acur[i_1 + j * lda_t] - acur[j * lda_t] * acur[i_1];
+            tempx = _mm256_div_ps(_mm256_loadu_ps(&acur[i_1]), p_val8);
+            _mm256_storeu_ps(&acur[i_1], tempx);
+
+            for(j = 1; j < ni - 3; j += 4)
+            {
+                b_off = j * lda_t;
+                y_off = i_1 + b_off;
+                tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
+                tempb[1] = _mm256_broadcast_ss(&acur[b_off] + 1 * lda_t);
+                tempb[2] = _mm256_broadcast_ss(&acur[b_off] + 2 * lda_t);
+                tempb[3] = _mm256_broadcast_ss(&acur[b_off] + 3 * lda_t);
+                tempY[0] = _mm256_loadu_ps(&acur[y_off]);
+                tempY[1] = _mm256_loadu_ps(&acur[y_off] + 1 * lda_t);
+                tempY[2] = _mm256_loadu_ps(&acur[y_off] + 2 * lda_t);
+                tempY[3] = _mm256_loadu_ps(&acur[y_off] + 3 * lda_t);
+                // Y := Y - b * x
+                result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
+                result[1] = _mm256_fnmadd_ps(tempb[1], tempx, tempY[1]);
+                result[2] = _mm256_fnmadd_ps(tempb[2], tempx, tempY[2]);
+                result[3] = _mm256_fnmadd_ps(tempb[3], tempx, tempY[3]);
+                _mm256_storeu_ps(&acur[y_off], result[0]);
+                _mm256_storeu_ps((&acur[y_off] + 1 * lda_t), result[1]);
+                _mm256_storeu_ps((&acur[y_off] + 2 * lda_t), result[2]);
+                _mm256_storeu_ps((&acur[y_off] + 3 * lda_t), result[3]);
+            }
+            /* remaining inner loop update */
+            for(; j < ni; j++)
+            {
+                b_off = j * lda_t;
+                y_off = i_1 + b_off;
+                tempb[0] = _mm256_broadcast_ss(&acur[b_off]);
+                tempY[0] = _mm256_loadu_ps(&acur[y_off]);
+                result[0] = _mm256_fnmadd_ps(tempb[0], tempx, tempY[0]);
+                _mm256_storeu_ps(&acur[y_off], result[0]);
+            }
+        }
+        /* remaining outer loop update */
+        for(; i_1 < mi; i_1++)
+        {
+            acur[i_1] = acur[i_1] / p_val;
+            for(j = 1; j < ni; j++)
+            {
+                acur[i_1 + j * lda_t] = acur[i_1 + j * lda_t] - acur[j * lda_t] * acur[i_1];
+            }
         }
     }
 }

898025f5755cca0463976da9bdf9e08d864907b8
Saitharun, Katteboina <Katteboina.Saitharun@amd.com>
2026-04-16 17:41:27 +0530

    Fix missing ldb parameter in sygvd overflow/underflow test cases (#277)
    The sygvd CLI parser was updated to accept ldb as a separate parameter,
    but the test entries in overflow_underflow_test.cmake were not updated
    to include it. This caused argument misalignment where lwork was parsed
    as ldb, liwork as lwork, and so on, resulting in incorrect test runs.

--- test/main/overflow_underflow_test.cmake
+++ test/main/overflow_underflow_test.cmake
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
 ###############################################################################
 # Overflow and Underflow test cases for APIs.
 set(OVERFLOW_UNDERFLOW_VALUES_TEST_CASES
@@ -478,20 +478,20 @@ set(OVERFLOW_UNDERFLOW_VALUES_TEST_CASES
             "hseqr sdcz S I 75 1 75 100 100 -1 1 --imatrix=O"
             "hseqr sdcz E V 200 1 200 500 500 -1 1 --imatrix=O"
             "hseqr sdcz S V 200 1 200 500 500 -1 1 --imatrix=O"
-            "sygvd sdcz 1 V U 50 50 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 2 V U 150 150 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 3 V U 72 72 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 1 V U 500 500 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 2 V U 230 230 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 3 V U 8 8 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 1 V U 64 64 -1 -1 -1 1 --imatrix=O"
-            "sygvd sdcz 1 V U 50 50 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 2 V U 150 150 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 3 V U 72 72 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 1 V U 500 500 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 2 V U 230 230 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 3 V U 8 8 -1 -1 -1 1 --imatrix=U"
-            "sygvd sdcz 1 V U 64 64 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 1 V U 50 50 50 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 2 V U 150 150 150 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 3 V U 72 72 72 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 1 V U 500 500 500 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 2 V U 230 230 230 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 3 V U 8 8 8 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 1 V U 64 64 64 -1 -1 -1 1 --imatrix=O"
+            "sygvd sdcz 1 V U 50 50 50 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 2 V U 150 150 150 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 3 V U 72 72 72 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 1 V U 500 500 500 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 2 V U 230 230 230 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 3 V U 8 8 8 -1 -1 -1 1 --imatrix=U"
+            "sygvd sdcz 1 V U 64 64 64 -1 -1 -1 1 --imatrix=U"
             "lange sdcz M1IF 500 500 500 100 --imatrix-char=O"
             "lange sdcz M1IF 500 500 500 100 --imatrix-char=U"
             "gecon sdcz 1 15 20 1 --imatrix=U"
